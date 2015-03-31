e045762904196178c6656acd28a8d97e7d1714f9
Michał Gregorczyk <michalgr@fb.com>
2015-03-31 07:05:51 -0700

    Add TrivialPooledByteBuffer

--- imagepipeline/src/test/java/com/facebook/imagepipeline/bitmaps/DalvikBitmapFactoryTest.java
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/bitmaps/DalvikBitmapFactoryTest.java
@@ -26,6 +26,7 @@ import com.facebook.imagepipeline.memory.PooledByteBuffer;
 import com.facebook.imagepipeline.memory.SingleByteArrayPool;
 import com.facebook.imagepipeline.nativecode.Bitmaps;
 import com.facebook.imagepipeline.testing.MockBitmapFactory;
+import com.facebook.imagepipeline.testing.TrivialPooledByteBuffer;
 import com.facebook.testing.robolectric.v2.WithTestDefaultsRunner;
 
 import org.junit.Before;
@@ -202,58 +203,6 @@ public class DalvikBitmapFactoryTest {
       return options.inDither &&
           options.inPreferredConfig == Bitmaps.BITMAP_CONFIG &&
           options.inPurgeable;
-    }
-  }
-
-  /**
-   * A trivial implementation of {@link PooledByteBuffer}
-   */
-  private static class TrivialPooledByteBuffer implements PooledByteBuffer {
-    private byte[] mBuf;
-    private long mNativePtr;
-
-    public TrivialPooledByteBuffer(byte[] buf) {
-      this(buf, 0L);
-    }
-
-    public TrivialPooledByteBuffer(byte[] buf, long nativePtr) {
-      mBuf = buf;
-      mNativePtr = nativePtr;
-    }
-
-    @Override
-    public int size() {
-      return isClosed() ? -1 : mBuf.length;
-    }
-
-    @Override
-    public InputStream getStream() {
-      return new ByteArrayInputStream(mBuf);
-    }
-
-    @Override
-    public byte read(int offset) {
-      return mBuf[offset];
-    }
-
-    @Override
-    public void read(int offset, byte[] buffer, int bufferOffset, int length) {
-      System.arraycopy(mBuf, offset, buffer, bufferOffset, length);
-    }
-
-    @Override
-    public long getNativePtr() {
-      return mNativePtr;
-    }
-
-    @Override
-    public boolean isClosed() {
-      return mBuf == null;
-    }
-
-    @Override
-    public void close() {
-      mBuf = null;
     }
   }
 }
---
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/testing/TrivialPooledByteBuffer.java
@@ -0,0 +1,60 @@
+// Copyright 2004-present Facebook. All Rights Reserved.
+
+package com.facebook.imagepipeline.testing;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import com.facebook.imagepipeline.memory.PooledByteBuffer;
+
+/**
+ * A trivial implementation of {@link PooledByteBuffer}
+ */
+public class TrivialPooledByteBuffer implements PooledByteBuffer {
+  private byte[] mBuf;
+  private long mNativePtr;
+
+  public TrivialPooledByteBuffer(byte[] buf) {
+    this(buf, 0L);
+  }
+
+  public TrivialPooledByteBuffer(byte[] buf, long nativePtr) {
+    mBuf = buf;
+    mNativePtr = nativePtr;
+  }
+
+  @Override
+  public int size() {
+    return isClosed() ? -1 : mBuf.length;
+  }
+
+  @Override
+  public InputStream getStream() {
+    return new ByteArrayInputStream(mBuf);
+  }
+
+  @Override
+  public byte read(int offset) {
+    return mBuf[offset];
+  }
+
+  @Override
+  public void read(int offset, byte[] buffer, int bufferOffset, int length) {
+    System.arraycopy(mBuf, offset, buffer, bufferOffset, length);
+  }
+
+  @Override
+  public long getNativePtr() {
+    return mNativePtr;
+  }
+
+  @Override
+  public boolean isClosed() {
+    return mBuf == null;
+  }
+
+  @Override
+  public void close() {
+    mBuf = null;
+  }
+}
