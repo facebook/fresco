e1f7ad82a9c413604437279b519fb5e9e709608c
Michał Gregorczyk <michalgr@fb.com>
2015-04-02 05:00:30 -0700

    windows build

--- imagepipeline/src/main/jni/Application.mk
+++ imagepipeline/src/main/jni/Application.mk
@@ -5,7 +5,7 @@ APP_ABI := armeabi-v7a armeabi x86
 APP_PLATFORM := android-9
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-NDK_MODULE_PATH := $(APP_MK_DIR):$(APP_MK_DIR)../../../build/nativemerge
+NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../build/nativemerge
 
 APP_STL := gnustl_shared
 
