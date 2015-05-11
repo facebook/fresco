a30ce0c7c842bfe56387a1ebc419baed5f3bcfe9
Tyrone Nicholas <tnicholas@fb.com>
2015-05-11 14:59:22 -0700

    Move FakeClock into animations test library

--- imagepipeline/src/test/java/com/facebook/imagepipeline/animated/impl/AnimatedDrawableCachingBackendImplTest.java
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/animated/impl/AnimatedDrawableCachingBackendImplTest.java
@@ -30,7 +30,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 import static com.facebook.imagepipeline.animated.testing.TestAnimatedDrawableBackend.pixelValue;
