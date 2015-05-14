3bef4811fe90a6b62e670c71d0818d51c997a440
Ian Childs <ianc@fb.com>
2015-05-14 09:12:11 -0700

    Refactor WebPTransformProducer

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/WebpTranscodeProducer.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/WebpTranscodeProducer.java
@@ -9,9 +9,12 @@
 
 package com.facebook.imagepipeline.producers;
 
+import javax.annotation.Nullable;
+
 import java.io.InputStream;
 import java.util.concurrent.Executor;
 
+import com.facebook.common.internal.Preconditions;
 import com.facebook.common.references.CloseableReference;
 import com.facebook.common.util.TriState;
 import com.facebook.imageformat.ImageFormat;
@@ -21,35 +24,132 @@ import com.facebook.imagepipeline.memory.PooledByteBufferInputStream;
 import com.facebook.imagepipeline.memory.PooledByteBufferFactory;
 import com.facebook.imagepipeline.memory.PooledByteBufferOutputStream;
 import com.facebook.imagepipeline.nativecode.WebpTranscoder;
-import com.facebook.imagepipeline.request.ImageRequest;
 
 /**
- * Transcodes webps to jpegs.
+ * Transcodes WebP to JPEG / PNG.
  *
- * <p> If processed image is one of VP8, VP8X or VP8L non-animated webps then
- * it is transcoded to jpeg if Android's decoder does not support this format.
- * If image is of any other type, no transformation is applied.
+ * <p> If processed image is one of VP8, VP8X or VP8L non-animated WebPs then it is transcoded to
+ * jpeg if the decoder on the running version of Android does not support this format. This was the
+ * case prior to version 4.2.1.
+ * <p> If the image is not WebP, no transformation is applied.
  */
-public class WebpTranscodeProducer
-    extends ImageTransformProducer<CloseableReference<PooledByteBuffer>, Void> {
+public class WebpTranscodeProducer implements Producer<CloseableReference<PooledByteBuffer>> {
   private static final String PRODUCER_NAME = "WebpTranscodeProducer";
   private static final int DEFAULT_JPEG_QUALITY = 80;
+
+  private final Executor mExecutor;
+  private final PooledByteBufferFactory mPooledByteBufferFactory;
+  private final Producer<CloseableReference<PooledByteBuffer>> mNextProducer;
 
   public WebpTranscodeProducer(
       Executor executor,
       PooledByteBufferFactory pooledByteBufferFactory,
       Producer<CloseableReference<PooledByteBuffer>> nextProducer) {
-    super(executor, pooledByteBufferFactory, nextProducer);
+    mExecutor = Preconditions.checkNotNull(executor);
+    mPooledByteBufferFactory = Preconditions.checkNotNull(pooledByteBufferFactory);
+    mNextProducer = Preconditions.checkNotNull(nextProducer);
   }
 
   @Override
-  protected TriState shouldTransform(
-      final CloseableReference<PooledByteBuffer> imageRef,
-      final ImageRequest imageRequest,
-      boolean isLast) {
+  public void produceResults(
+      final Consumer<CloseableReference<PooledByteBuffer>> consumer,
+      final ProducerContext context) {
+    mNextProducer.produceResults(new WebpTranscodeConsumer(consumer, context), context);
+  }
+
+  private class WebpTranscodeConsumer extends DelegatingConsumer<
+      CloseableReference<PooledByteBuffer>,
+      CloseableReference<PooledByteBuffer>> {
+
+    private final ProducerContext mContext;
+    private TriState mShouldTranscodeWhenFinished;
+
+    public WebpTranscodeConsumer(
+        final Consumer<CloseableReference<PooledByteBuffer>> consumer,
+        final ProducerContext context) {
+      super(consumer);
+      mContext = context;
+      mShouldTranscodeWhenFinished = TriState.UNSET;
+    }
+
+    @Override
+    protected void onNewResultImpl(
+        @Nullable CloseableReference<PooledByteBuffer> newResult,
+        boolean isLast) {
+      // try to determine if the last result should be transformed
+      if (mShouldTranscodeWhenFinished == TriState.UNSET && newResult != null) {
+        mShouldTranscodeWhenFinished = shouldTranscode(newResult);
+      }
+
+      // just propagate result if it shouldn't be transformed
+      if (mShouldTranscodeWhenFinished == TriState.NO) {
+        getConsumer().onNewResult(newResult, isLast);
+        return;
+      }
+
+      if (isLast) {
+        if (mShouldTranscodeWhenFinished == TriState.YES && newResult != null) {
+          transcodeLastResult(newResult, getConsumer(), mContext);
+        } else {
+          getConsumer().onNewResult(newResult, isLast);
+        }
+      }
+    }
+  }
+
+  private void transcodeLastResult(
+      final CloseableReference<PooledByteBuffer> originalResult,
+      final Consumer<CloseableReference<PooledByteBuffer>> consumer,
+      final ProducerContext producerContext) {
+    Preconditions.checkNotNull(originalResult);
+    final CloseableReference<PooledByteBuffer> imageRefCopy = originalResult.clone();
+    final StatefulProducerRunnable<CloseableReference<PooledByteBuffer>> runnable =
+        new StatefulProducerRunnable<CloseableReference<PooledByteBuffer>>(
+            consumer,
+            producerContext.getListener(),
+            PRODUCER_NAME,
+            producerContext.getId()) {
+          @Override
+          protected CloseableReference<PooledByteBuffer> getResult() throws Exception {
+            PooledByteBufferOutputStream outputStream = mPooledByteBufferFactory.newOutputStream();
+            try {
+              doTranscode(imageRefCopy, outputStream);
+              return CloseableReference.of(outputStream.toByteBuffer());
+            } finally {
+              outputStream.close();
+            }
+          }
+
+          @Override
+          protected void disposeResult(CloseableReference<PooledByteBuffer> result) {
+            CloseableReference.closeSafely(result);
+          }
+
+          @Override
+          protected void onSuccess(CloseableReference<PooledByteBuffer> result) {
+            imageRefCopy.close();
+            super.onSuccess(result);
+          }
+
+          @Override
+          protected void onFailure(Exception e) {
+            imageRefCopy.close();
+            super.onFailure(e);
+          }
+
+          @Override
+          protected void onCancellation() {
+            imageRefCopy.close();
+            super.onCancellation();
+          }
+        };
+    mExecutor.execute(runnable);
+  }
+
+  private static TriState shouldTranscode(final CloseableReference<PooledByteBuffer> imageRef) {
+    Preconditions.checkNotNull(imageRef);
     InputStream imageInputStream = new PooledByteBufferInputStream(imageRef.get());
     ImageFormat imageFormat = ImageFormatChecker.getImageFormat_WrapIOException(imageInputStream);
-
     switch (imageFormat) {
       case WEBP_SIMPLE:
       case WEBP_LOSSLESS:
@@ -57,18 +157,18 @@ public class WebpTranscodeProducer
       case WEBP_EXTENDED_WITH_ALPHA:
         return TriState.valueOf(!WebpTranscoder.isWebpNativelySupported(imageFormat));
       case UNKNOWN:
-        return isLast ? TriState.NO : TriState.UNSET;
+        // the image format might be unknown because we haven't fetched the whole header yet,
+        // in which case the decision whether to transcode or not cannot be made yet
+        return TriState.UNSET;
       default:
+        // if the image format is known, but it is not WebP, then the image shouldn't be transcoded
         return TriState.NO;
     }
   }
 
-  @Override
-  protected void transform(
+  private static void doTranscode(
       final CloseableReference<PooledByteBuffer> imageRef,
-      final PooledByteBufferOutputStream outputStream,
-      ImageRequest imageRequest,
-      Void unused) throws Exception {
+      final PooledByteBufferOutputStream outputStream) throws Exception {
     InputStream imageInputStream = new PooledByteBufferInputStream(imageRef.get());
     ImageFormat imageFormat = ImageFormatChecker.getImageFormat_WrapIOException(imageInputStream);
     switch (imageFormat) {
@@ -85,39 +185,5 @@ public class WebpTranscodeProducer
       default:
         throw new IllegalArgumentException("Wrong image format");
     }
-  }
-
-  @Override
-  protected CloseableReference<PooledByteBuffer> getImageCopy(
-      CloseableReference<PooledByteBuffer> originalResult) {
-    return originalResult.clone();
-  }
-
-  @Override
-  protected Void getExtraInformation(
-      CloseableReference<PooledByteBuffer> originalResult) {
-    return null;
-  }
-
-  @Override
-  protected CloseableReference<PooledByteBuffer> createReturnValue(
-      PooledByteBuffer transformedBytes,
-      Void unused) {
-    return CloseableReference.of(transformedBytes);
-  }
-
-  @Override
-  protected void closeReturnValue(CloseableReference<PooledByteBuffer> returnValue) {
-    CloseableReference.closeSafely(returnValue);
-  }
-
-  @Override
-  protected String getProducerName() {
-    return PRODUCER_NAME;
-  }
-
-  @Override
-  protected boolean shouldAllowCancellation() {
-    return false;
   }
 }
