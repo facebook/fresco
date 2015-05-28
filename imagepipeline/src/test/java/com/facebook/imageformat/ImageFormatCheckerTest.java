540600e29492adea571535c880b88c91d6fcb85d
Lucas Moscovicz <lmoscovicz@fb.com>
2015-05-28 02:30:23 -0700

    Moved resource images to common folder

--- imagepipeline/src/test/java/com/facebook/imageformat/ImageFormatCheckerTest.java
+++ imagepipeline/src/test/java/com/facebook/imageformat/ImageFormatCheckerTest.java
@@ -31,47 +31,65 @@ public class ImageFormatCheckerTest {
 
   @Test
   public void testSimpleWebps() throws Exception {
-    singleImageTypeTest(getNames(2, "webps/%d_webp_plain.webp"), ImageFormat.WEBP_SIMPLE);
+    singleImageTypeTest(
+        getNames(2, "webps/%d_webp_plain.webp"),
+        ImageFormat.WEBP_SIMPLE);
   }
 
   @Test
   public void testLosslessWebps() throws Exception {
-    singleImageTypeTest(getNames(5, "webps/%d_webp_ll.webp"), ImageFormat.WEBP_LOSSLESS);
+    singleImageTypeTest(
+        getNames(5, "webps/%d_webp_ll.webp"),
+        ImageFormat.WEBP_LOSSLESS);
   }
 
   @Test
   public void testExtendedWebpsWithAlpha() throws Exception {
-    singleImageTypeTest(getNames(5, "webps/%d_webp_ea.webp"), ImageFormat.WEBP_EXTENDED_WITH_ALPHA);
+    singleImageTypeTest(
+        getNames(5, "webps/%d_webp_ea.webp"),
+        ImageFormat.WEBP_EXTENDED_WITH_ALPHA);
   }
 
   @Test
   public void testExtendedWebpsWithoutAlpha() throws Exception {
-    singleImageTypeTest(getName("webps/1_webp_e.webp"), ImageFormat.WEBP_EXTENDED);
+    singleImageTypeTest(
+        getName("webps/1_webp_e.webp"),
+        ImageFormat.WEBP_EXTENDED);
   }
 
   @Test
   public void testAnimatedWebps() throws Exception {
-    singleImageTypeTest(getName("webps/1_webp_anim.webp"), ImageFormat.WEBP_ANIMATED);
+    singleImageTypeTest(
+        getName("webps/1_webp_anim.webp"),
+        ImageFormat.WEBP_ANIMATED);
   }
 
   @Test
   public void testJpegs() throws Exception {
-    singleImageTypeTest(getNames(5, "jpegs/%d.jpeg"), ImageFormat.JPEG);
+    singleImageTypeTest(
+        getNames(5, "jpegs/%d.jpeg"),
+        ImageFormat.JPEG);
   }
 
   @Test
   public void testPngs() throws Exception {
-    singleImageTypeTest(getNames(5, "pngs/%d.png"), ImageFormat.PNG);
+    singleImageTypeTest(
+        getNames(5, "pngs/%d.png"),
+        ImageFormat.PNG);
   }
 
   @Test
   public void testGifs() throws Exception {
-    singleImageTypeTest(getNames(5, "gifs/%d.gif"), ImageFormat.GIF);
+    singleImageTypeTest(
+        getNames(5, "gifs/%d.gif"),
+        ImageFormat.GIF);
   }
 
   @Test
   public void testBmps() throws Exception {
-    singleImageTypeTest(getNames(5, "bmps/%d.bmp"), ImageFormat.BMP);
+    singleImageTypeTest(
+        getNames(5, "bmps/%d.bmp"),
+        ImageFormat.BMP);
   }
 
   private void singleImageTypeTest(
