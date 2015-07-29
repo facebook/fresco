6bad490bd8120023ebbbd1f7f891d6b8e7da29ba
Lucas Moscovicz <lmoscovicz@fb.com>
2015-07-29 03:29:56 -0700

    Added decodeDimensions to BitmapUtil

--- imagepipeline/src/main/java/com/facebook/imageutils/BitmapUtil.java
+++ imagepipeline/src/main/java/com/facebook/imageutils/BitmapUtil.java
@@ -11,7 +11,12 @@ package com.facebook.imageutils;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
+import android.util.Pair;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import javax.annotation.Nullable;
 
@@ -35,6 +40,32 @@ public final class BitmapUtil {
       // Estimate for earlier platforms.
       return bitmap.getWidth() * bitmap.getRowBytes();
     }
+  }
+
+  /**
+   * Decodes only the bounds of an image and returns its width and height or null if the size can't
+   * be determined
+   * @param bytes the input byte array of the image
+   * @return dimensions of the image
+   */
+  public static @Nullable Pair<Integer, Integer> decodeDimensions(byte[] bytes) {
+    // wrapping with ByteArrayInputStream is cheap and we don't have duplicate implementation
+    return decodeDimensions(new ByteArrayInputStream(bytes));
+  }
+
+  /**
+   * Decodes only the bounds of an image and returns its width and height or null if the size can't
+   * be determined
+   * @param is the InputStream containing the image data
+   * @return dimensions of the image
+   */
+  public static @Nullable Pair<Integer, Integer> decodeDimensions(InputStream is) {
+    BitmapFactory.Options options = new BitmapFactory.Options();
+    options.inJustDecodeBounds = true;
+
+    BitmapFactory.decodeStream(is, null, options);
+    return (options.outWidth == -1 || options.outHeight == -1) ?
+        null : new Pair(options.outWidth, options.outHeight);
   }
 
 }
--- imagepipeline/src/test/java/com/facebook/imageutils/BitmapUtilTest.java
+++ imagepipeline/src/test/java/com/facebook/imageutils/BitmapUtilTest.java
@@ -10,6 +10,7 @@ package com.facebook.imageutils;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.util.Pair;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -41,6 +42,87 @@ public class BitmapUtilTest {
     final Bitmap bitmap3 =
         BitmapFactory.decodeStream(BitmapUtilTest.class.getResourceAsStream("pngs/3.png"));
     assertEquals(172800, BitmapUtil.getSizeInBytes(bitmap3));
+  }
+
+  @Test
+  public void testDecodeDimensions_testPngs() {
+    assertEquals(
+        new Pair(240, 181),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("pngs/1.png")));
+    assertEquals(
+        new Pair(240, 246),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("pngs/2.png")));
+    assertEquals(
+        new Pair(240, 180),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("pngs/3.png")));
+  }
+
+  @Test
+  public void testDecodeDimensions_testJpegs() {
+    assertEquals(
+        new Pair(240, 181),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("jpegs/1.jpeg")));
+    assertEquals(
+        new Pair(240, 93),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("jpegs/2.jpeg")));
+    assertEquals(
+        new Pair(240, 240),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("jpegs/3.jpeg")));
+  }
+
+  @Test
+  public void testDecodeDimensions_testIncompleteJpegs() {
+    assertEquals(
+        new Pair(240, 181),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("jpegs/1cut.jpeg")));
+  }
+
+  @Test
+  public void testDecodeDimensions_testProgressiveJpegs() {
+    assertEquals(
+        new Pair(981, 657),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("jpegs/1prog.jpeg")));
+    assertEquals(
+        new Pair(800, 531),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("jpegs/2prog.jpeg")));
+  }
+
+  @Test
+  public void testDecodeDimensions_testStaticGifs() throws Exception {
+    assertEquals(
+        new Pair(240, 181),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("gifs/1.gif")));
+    assertEquals(
+        new Pair(240, 246),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("gifs/2.gif")));
+    assertEquals(
+        new Pair(240, 180),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("gifs/3.gif")));
+  }
+
+  @Test
+  public void testDecodeDimensions_testAnimatedGifs() {
+    assertEquals(
+        new Pair(500, 500),
+        BitmapUtil.decodeDimensions(
+            BitmapUtilTest.class.getResourceAsStream("animatedgifs/1.gif")));
+    assertEquals(
+        new Pair(550, 400),
+        BitmapUtil.decodeDimensions(
+            BitmapUtilTest.class.getResourceAsStream("animatedgifs/2.gif")));
+  }
+
+  @Test
+  public void testDecodeDimensions_testBmps() throws Exception {
+    assertEquals(
+        new Pair(240, 181),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("bmps/1.bmp")));
+    assertEquals(
+        new Pair(240, 246),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("bmps/2.bmp")));
+    assertEquals(
+        new Pair(240, 180),
+        BitmapUtil.decodeDimensions(BitmapUtilTest.class.getResourceAsStream("bmps/3.bmp")));
   }
 
 }
