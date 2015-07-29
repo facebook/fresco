87155bbfb2924d8f8bf397724c3909ee3658cc88
YongzhiWang <YongzhiWang@users.noreply.github.com>
2015-07-29 08:56:08 -0700

    Fix the null pointer exception

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ResizeAndRotateProducer.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ResizeAndRotateProducer.java
@@ -187,8 +187,15 @@ public class ResizeAndRotateProducer implements Producer<EncodedImage> {
         return null;
       }
       String originalSize = encodedImage.getWidth() + "x" + encodedImage.getHeight();
-      String requestedSize =
-          imageRequest.getResizeOptions().width + "x" + imageRequest.getResizeOptions().height;
+
+      String requestedSize;
+      if (imageRequest.getResizeOptions() != null) {
+        requestedSize =
+            imageRequest.getResizeOptions().width + "x" + imageRequest.getResizeOptions().height;
+      } else {
+        requestedSize = "Unspecified";
+      }
+
       String fraction = numerator > 0 ? numerator + "/8" : "";
       return ImmutableMap.of(
           ORIGINAL_SIZE_KEY, originalSize,
@@ -216,6 +223,11 @@ public class ResizeAndRotateProducer implements Producer<EncodedImage> {
       ResizeOptions resizeOptions,
       int width,
       int height) {
+
+    if (resizeOptions == null) {
+      return 1.0f;
+    }
+
     final float widthRatio = ((float) resizeOptions.width) / width;
     final float heightRatio = ((float) resizeOptions.height) / height;
     float ratio = Math.max(widthRatio, heightRatio);
