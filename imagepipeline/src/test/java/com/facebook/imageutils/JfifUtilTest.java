f51602b9ebd587d3bedab651fb35e11f0ed8a32a
Lucas Moscovicz <lmoscovicz@fb.com>
2015-07-30 03:43:21 -0700

    Remove getDimensions methods from utils

--- imagepipeline/src/test/java/com/facebook/imageutils/JfifUtilTest.java
+++ imagepipeline/src/test/java/com/facebook/imageutils/JfifUtilTest.java
@@ -123,36 +123,4 @@ public class JfifUtilTest {
     assertEquals(expectOri, JfifUtil.getOrientation(hexStringToByteArray(IMAGE_WITH_STRUCT_10)));
   }
 
-  @Test
-  public void testGetDimensions_VariousSOFWidthHeight() {
-    testGetDimensions_VariousSOFLocation(0, 0);
-    testGetDimensions_VariousSOFLocation(300, 400);
-    testGetDimensions_VariousSOFLocation(400, 300);
-    testGetDimensions_VariousSOFLocation(10, 4000);
-    testGetDimensions_VariousSOFLocation(4000, 10);
-  }
-
-  private void testGetDimensions_VariousSOFLocation(int width, int height) {
-    String testSOF1 = makeSOFSection(20, 3, width, height);
-    String testSOF2 = makeSOFSection(10, 1, width, height);
-    Rect expectRect = new Rect(0, 0, width, height);
-    final String IMAGE_WITH_STRUCT_1 = SOI + APP0 + DQT + DHT + DRI + testSOF1 + SOS + EOI;
-    final String IMAGE_WITH_STRUCT_2 = SOI + APP0 + DQT + DHT + testSOF1 + DRI + SOS + EOI;
-    final String IMAGE_WITH_STRUCT_3 = SOI + APP0 + DQT + testSOF1 + DHT + DRI + SOS + EOI;
-    final String IMAGE_WITH_STRUCT_4 = SOI + APP0 + testSOF2 + DQT + DHT + DRI + SOS + EOI;
-    final String IMAGE_WITH_STRUCT_5 = SOI + testSOF2 + APP0 + DQT + DHT + DRI + SOS + EOI;
-    assertEquals(expectRect, JfifUtil.getDimensions(hexStringToByteArray(IMAGE_WITH_STRUCT_1)));
-    assertEquals(expectRect, JfifUtil.getDimensions(hexStringToByteArray(IMAGE_WITH_STRUCT_2)));
-    assertEquals(expectRect, JfifUtil.getDimensions(hexStringToByteArray(IMAGE_WITH_STRUCT_3)));
-    assertEquals(expectRect, JfifUtil.getDimensions(hexStringToByteArray(IMAGE_WITH_STRUCT_4)));
-    assertEquals(expectRect, JfifUtil.getDimensions(hexStringToByteArray(IMAGE_WITH_STRUCT_5)));
-  }
-
-  @Test
-  public void testGetDimensions_NoSOF() {
-    final String NO_SOF_IMAGE_1 = SOI + APP0 + APP2 + DQT + DHT + DRI + SOS + EOI;
-    final String NO_SOF_IMAGE_2 = SOI + DQT + DHT + SOS + EOI;
-    assertEquals(null, JfifUtil.getDimensions(hexStringToByteArray(NO_SOF_IMAGE_1)));
-    assertEquals(null, JfifUtil.getDimensions(hexStringToByteArray(NO_SOF_IMAGE_2)));
-  }
 }
