3aa0b6ac6c615cfb704ba406ddc27a644f10884a
Lucas Moscovicz <lmoscovicz@fb.com>
2015-08-07 07:37:35 -0700

    Changed OrientedBitmapDrawable to extend ForwardingDrawable instead of BitmapDrawable

--- drawee/src/test/java/com/facebook/drawee/drawable/OrientedDrawableTest.java
+++ drawee/src/test/java/com/facebook/drawee/drawable/OrientedDrawableTest.java
@@ -8,10 +8,11 @@
  */
 package com.facebook.drawee.drawable;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,28 +23,36 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
- * Tests for {@link OrientedBitmapDrawable}
+ * Tests for {@link OrientedDrawable}
  */
 @RunWith(RobolectricTestRunner.class)
-public class OrientedBitmapDrawableTest {
+public class OrientedDrawableTest {
 
-  private Resources mResources;
-  private Bitmap mBitmap;
+  private Drawable mDrawable;
   private Canvas mCanvas;
+  private Rect mBounds;
 
   @Before
   public void setUp() {
-    mResources = Resources.getSystem();
-    mBitmap = mock(Bitmap.class);
+    mDrawable = mock(Drawable.class);
     mCanvas = mock(Canvas.class);
+    mBounds = mock(Rect.class);
+
+    // Change the bounds so that they will be different from the Drawable initial bounds and
+    // setBounds calls onBoundsChange.
+    mBounds.left = 100;
+    mBounds.top = 100;
+    mBounds.right = 500;
+    mBounds.bottom = 500;
   }
 
   @Test
   public void testCreation_invalidAngle() {
     try {
-      new OrientedBitmapDrawable(mResources, mBitmap, 20);
+      new OrientedDrawable(mDrawable, 20);
       fail();
     } catch (IllegalArgumentException e) {
       // Do nothing, expected.
@@ -51,51 +60,63 @@ public class OrientedBitmapDrawableTest {
   }
 
   @Test
-  public void testCreation_unknownAngle() {
-    OrientedBitmapDrawable drawable = new OrientedBitmapDrawable(mResources, mBitmap, -1);
-    drawable.draw(mCanvas);
-    assertTrue(drawable.mRotationMatrix.isIdentity());
-  }
-
-  @Test
   public void testCreation_zeroDegrees() {
-    OrientedBitmapDrawable drawable = new OrientedBitmapDrawable(mResources, mBitmap, 0);
+    OrientedDrawable drawable = new OrientedDrawable(mDrawable, 0);
+    drawable.setBounds(mBounds);
     drawable.draw(mCanvas);
     assertTrue(drawable.mRotationMatrix.isIdentity());
-
+    verify(mDrawable).setBounds(new Rect(mBounds));
   }
 
   @Test
   public void testCreation_nintyDegrees() {
-    OrientedBitmapDrawable drawable = new OrientedBitmapDrawable(mResources, mBitmap, 90);
+    OrientedDrawable drawable = new OrientedDrawable(mDrawable, 90);
+    drawable.setBounds(mBounds);
     drawable.draw(mCanvas);
 
     Matrix expectedMatrix = new Matrix();
     expectedMatrix.setRotate(90, drawable.getBounds().centerX(), drawable.getBounds().centerY());
     assertFalse(drawable.mRotationMatrix.isIdentity());
     AndroidGraphicsTestUtils.assertEquals(expectedMatrix, drawable.mRotationMatrix);
+    verifySetBounds(expectedMatrix);
   }
 
   @Test
   public void testCreation_hundredAndEightyDegrees() {
-    OrientedBitmapDrawable drawable = new OrientedBitmapDrawable(mResources, mBitmap, 180);
+    OrientedDrawable drawable = new OrientedDrawable(mDrawable, 180);
+    drawable.setBounds(mBounds);
     drawable.draw(mCanvas);
 
     Matrix expectedMatrix = new Matrix();
     expectedMatrix.setRotate(180, drawable.getBounds().centerX(), drawable.getBounds().centerY());
     assertFalse(drawable.mRotationMatrix.isIdentity());
     AndroidGraphicsTestUtils.assertEquals(expectedMatrix, drawable.mRotationMatrix);
+    verifySetBounds(expectedMatrix);
   }
 
   @Test
   public void testCreation_twoHundredAndSeventyDegrees() {
-    OrientedBitmapDrawable drawable = new OrientedBitmapDrawable(mResources, mBitmap, 270);
+    OrientedDrawable drawable = new OrientedDrawable(mDrawable, 270);
+    drawable.setBounds(mBounds);
     drawable.draw(mCanvas);
 
     Matrix expectedMatrix = new Matrix();
     expectedMatrix.setRotate(270, drawable.getBounds().centerX(), drawable.getBounds().centerY());
     assertFalse(drawable.mRotationMatrix.isIdentity());
     AndroidGraphicsTestUtils.assertEquals(expectedMatrix, drawable.mRotationMatrix);
+    verifySetBounds(expectedMatrix);
+  }
+
+  private void verifySetBounds(Matrix rotationMatrix) {
+    RectF expectedBounds = new RectF(mBounds);
+    Matrix inverse = new Matrix();
+    rotationMatrix.invert(inverse);
+    inverse.mapRect(expectedBounds);
+    verify(mDrawable).setBounds(
+        (int) expectedBounds.left,
+        (int) expectedBounds.top,
+        (int) expectedBounds.right,
+        (int) expectedBounds.bottom);
   }
 
 }
