d2a9a77a70216f5f5478450030132761df711da3
Yimin Chen <yimin@fb.com>
2015-12-01 18:38:52 -0800

    use try/finally to close cursor

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/LocalExifThumbnailProducer.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/LocalExifThumbnailProducer.java
@@ -146,12 +146,18 @@ public class LocalExifThumbnailProducer implements Producer<EncodedImage> {
   private String getRealPathFromUri(final Uri srcUri) {
     String result = null;
     if (UriUtil.isLocalContentUri(srcUri)) {
-      Cursor cursor = mContentResolver.query(srcUri, null, null, null, null);
-      if (cursor != null) {
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        result = cursor.getString(idx);
-        cursor.close();
+      Cursor cursor = null;
+      try {
+        cursor = mContentResolver.query(srcUri, null, null, null, null);
+        if (cursor != null) {
+          cursor.moveToFirst();
+          int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+          result = cursor.getString(idx);
+        }
+      } finally {
+        if (cursor != null) {
+          cursor.close();
+        }
       }
     } else if (UriUtil.isLocalFileUri(srcUri)) {
       result = srcUri.getPath();
