bdc33afdea14a296670f5537cd630b7fd991f6f2
Tyrone Nicholas <tnicholas@fb.com>
2015-12-15 08:40:56 -0800

    Add open-source -Stetho to the comparison app

--- samples/comparison/src/main/java/com/facebook/samples/comparison/configs/imagepipeline/ImagePipelineConfigFactory.java
+++ samples/comparison/src/main/java/com/facebook/samples/comparison/configs/imagepipeline/ImagePipelineConfigFactory.java
@@ -24,6 +24,7 @@ import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.facebook.imagepipeline.listener.RequestListener;
 import com.facebook.imagepipeline.listener.RequestLoggingListener;
 import com.facebook.samples.comparison.configs.ConfigConstants;
+import com.facebook.stetho.okhttp.StethoInterceptor;
 import com.squareup.okhttp.OkHttpClient;
 
 import java.util.HashSet;
@@ -58,6 +59,7 @@ public class ImagePipelineConfigFactory {
   public static ImagePipelineConfig getOkHttpImagePipelineConfig(Context context) {
     if (sOkHttpImagePipelineConfig == null) {
       OkHttpClient okHttpClient = new OkHttpClient();
+      okHttpClient.networkInterceptors().add(new StethoInterceptor());
       ImagePipelineConfig.Builder configBuilder =
         OkHttpImagePipelineConfigFactory.newBuilder(context, okHttpClient);
       configureCaches(configBuilder, context);
