32946996b8170c94849fe75afbfe3f59d2d2644f
Tyrone Nicholas <tnicholas@fb.com>
2016-02-15 04:11:24 -0800

    A new score-based eviction comparator
    Differential Revision: D2921750
    
    fb-gh-sync-id: aebe973462fcc6a46d2f2dd675163af850671430
    fbshipit-source-id: aebe973462fcc6a46d2f2dd675163af850671430

---
+++ imagepipeline-base/src/main/java/com/facebook/cache/disk/ScoreBasedEvictionComparatorSupplier.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.cache.disk;
+
+import com.facebook.common.internal.VisibleForTesting;
+
+/**
+ * Evicts cache items based on a mix of their size and timestamp.
+ */
+public class ScoreBasedEvictionComparatorSupplier implements EntryEvictionComparatorSupplier {
+
+  private final float mAgeWeight;
+  private final float mSizeWeight;
+
+  public ScoreBasedEvictionComparatorSupplier(float ageWeight, float sizeWeight) {
+    mAgeWeight = ageWeight;
+    mSizeWeight = sizeWeight;
+  }
+
+  @Override
+  public EntryEvictionComparator get() {
+    return new EntryEvictionComparator() {
+
+      long now = System.currentTimeMillis();
+
+      /**
+       * Return <0 if lhs should be evicted before rhs.
+       */
+      @Override
+      public int compare(DiskStorage.Entry lhs, DiskStorage.Entry rhs) {
+        float score1 = calculateScore(lhs, now);
+        float score2 = calculateScore(rhs, now);
+        return score1 < score2 ? 1 : ((score2 == score1) ? 0 : -1);
+      }
+    };
+  }
+
+  /**
+   * Calculates an eviction score.
+   *
+   * Entries with a higher eviction score should be evicted first.
+   */
+  @VisibleForTesting
+  float calculateScore(DiskStorage.Entry entry, long now) {
+    long ageMs = now - entry.getTimestamp();
+    long bytes = entry.getSize();
+    return mAgeWeight * ageMs + mSizeWeight * bytes;
+  }
+}
