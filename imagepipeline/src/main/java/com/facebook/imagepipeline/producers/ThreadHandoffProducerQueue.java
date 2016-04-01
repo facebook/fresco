fc7dee38445c682ca19975d6587f6b88ab7aa407
Satyarth Sampath <satyarth@wooplr.com>
2016-04-01 12:35:52 +0530

    changed tab indentation

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ThreadHandoffProducerQueue.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ThreadHandoffProducerQueue.java
@@ -16,44 +16,44 @@ import java.util.Deque;
 import java.util.concurrent.Executor;
 
 public class ThreadHandoffProducerQueue {
-    private boolean mQueueing = false;
-    private final Deque<Runnable> mRunnableList;
-    private final Executor mExecutor;
+  private boolean mQueueing = false;
+  private final Deque<Runnable> mRunnableList;
+  private final Executor mExecutor;
 
-    public ThreadHandoffProducerQueue(Executor executor) {
-        mExecutor = Preconditions.checkNotNull(executor);
-        mRunnableList = new ArrayDeque<>();
+  public ThreadHandoffProducerQueue(Executor executor) {
+    mExecutor = Preconditions.checkNotNull(executor);
+    mRunnableList = new ArrayDeque<>();
+  }
+
+
+  public synchronized void addToQueueOrExecute(Runnable runnable) {
+    if (mQueueing) {
+      mRunnableList.add(runnable);
+    } else {
+      mExecutor.execute(runnable);
     }
+  }
 
+  public synchronized void startQueueing() {
+    mQueueing = true;
+  }
 
-    public synchronized void addToQueueOrExecute(Runnable runnable) {
-        if (mQueueing) {
-            mRunnableList.add(runnable);
-        } else {
-            mExecutor.execute(runnable);
-        }
-    }
+  public synchronized void stopQueuing() {
+    mQueueing = false;
+    execInQueue();
+  }
 
-    public synchronized void startQueueing() {
-        mQueueing = true;
-    }
+  private void execInQueue() {
+    while (!mRunnableList.isEmpty())
+      mExecutor.execute(mRunnableList.pop());
+    mRunnableList.clear();
+  }
 
-    public synchronized void stopQueuing() {
-        mQueueing = false;
-        execInQueue();
-    }
+  public synchronized void remove(Runnable runnable) {
+    mRunnableList.remove(runnable);
+  }
 
-    private void execInQueue() {
-        while (!mRunnableList.isEmpty())
-            mExecutor.execute(mRunnableList.pop());
-        mRunnableList.clear();
-    }
-
-    public synchronized void remove(Runnable runnable) {
-        mRunnableList.remove(runnable);
-    }
-
-    public synchronized boolean isQueueing() {
-        return mQueueing;
-    }
+  public synchronized boolean isQueueing() {
+    return mQueueing;
+  }
 }
