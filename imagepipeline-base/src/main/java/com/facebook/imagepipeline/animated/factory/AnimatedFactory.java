b8ff9ec886f18e7f5f4bd575df81ff31396ede0d
Alexander Oprisnik <oprisnik@fb.com>
2016-04-26 16:36:16 -0700

    Removed unused imports for AnimatedFactory
    Reviewed By: kirwan
    
    Differential Revision: D3225096
    
    fb-gh-sync-id: a8d7524c2dd733d40a4326dc1e8fdee8b03fabf3
    fbshipit-source-id: a8d7524c2dd733d40a4326dc1e8fdee8b03fabf3

--- imagepipeline-base/src/main/java/com/facebook/imagepipeline/animated/factory/AnimatedFactory.java
+++ imagepipeline-base/src/main/java/com/facebook/imagepipeline/animated/factory/AnimatedFactory.java
@@ -6,17 +6,12 @@
  * LICENSE file in the root directory of this source tree. An additional grant
  * of patent rights can be found in the PATENTS file in the same directory.
  */
+
 package com.facebook.imagepipeline.animated.factory;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.util.concurrent.ScheduledExecutorService;
 
 import android.content.Context;
-import android.content.res.Resources;
-import com.facebook.common.time.MonotonicClock;
-import com.facebook.common.executors.SerialExecutorService;
-import com.facebook.imagepipeline.animated.factory.AnimatedDrawableFactory;
-import com.facebook.imagepipeline.animated.factory.AnimatedImageFactory;
 
 @NotThreadSafe
 public interface AnimatedFactory {
@@ -24,5 +19,4 @@ public interface AnimatedFactory {
   AnimatedDrawableFactory getAnimatedDrawableFactory(Context context);
 
   AnimatedImageFactory getAnimatedImageFactory();
-
 }
