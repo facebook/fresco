a82c2ca19acc82a7bdd20322f0813d6999665030
Konstantin Raev <bestander@fb.com>
2016-05-18 06:14:56 -0700

    Reverted fresco-okhttp2 bindings deleted in D3292375
    Reviewed By: kirwan
    
    Differential Revision: D3316389
    
    fbshipit-source-id: 5a2730b88479b8a4d2dbf9d97cc98ee565a244f2

---
+++ imagepipeline-backends/imagepipeline-okhttp/src/main/java/com/facebook/imagepipeline/backends/okhttp/OkHttpImagePipelineConfigFactory.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.imagepipeline.backends.okhttp;
+
+import android.content.Context;
+import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.squareup.okhttp.OkHttpClient;
+
+/**
+ * Factory for getting an {@link com.facebook.imagepipeline.core.ImagePipelineConfig} that uses
+ * {@link OkHttpNetworkFetcher}.
+ *
+ * @deprecated replaced with {@code
+ * com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory}.
+ */
+public class OkHttpImagePipelineConfigFactory {
+
+  public static ImagePipelineConfig.Builder newBuilder(Context context, OkHttpClient okHttpClient) {
+    return ImagePipelineConfig.newBuilder(context)
+        .setNetworkFetcher(new OkHttpNetworkFetcher(okHttpClient));
+  }
+}
