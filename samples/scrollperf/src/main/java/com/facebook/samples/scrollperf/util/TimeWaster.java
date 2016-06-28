79fac091903f8797b3a891c321df48abd37a0d37
Massimo Carli <maxcarli@fb.com>
2016-06-28 06:25:48 -0700

    Fixed Fibonacci algo
    Reviewed By: kirwan
    
    Differential Revision: D3488418
    
    fbshipit-source-id: 8f9b7e11bfd31da202db6d783b9b2086fd9a4ec2

--- samples/scrollperf/src/main/java/com/facebook/samples/scrollperf/util/TimeWaster.java
+++ samples/scrollperf/src/main/java/com/facebook/samples/scrollperf/util/TimeWaster.java
@@ -23,7 +23,7 @@ public final class TimeWaster {
    */
   public static long Fib(int n) {
     if (n < 2) {
-      return 1;
+      return n;
     } else {
       return Fib(n - 1) + Fib(n - 2);
     }
