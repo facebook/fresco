b0f994beba4032e61619788c1b5c83524c4b4a13
Alexander Oprisnik <oprisnik@fb.com>
2016-08-12 14:57:19 -0700

    Added warning message if Fresco is initialized more than once
    Summary:
    Fresco should only be initialized once, so we should have a warning if somebody re-initializes Fresco since this causes everything to be recreated and can result in memory leaks.
    We use finalizers to avoid most of these leaks but it's still something that should be avoided - especially if you're doing it unintentionally.
    
    In the future, we plan to crash here for debug builds.
    
    Reviewed By: kirwan
    
    Differential Revision: D3669241
    
    fbshipit-source-id: dd9600bdb8d1543cf8f193e76bb7f5b943206231

--- drawee-backends/drawee-pipeline/src/main/java/com/facebook/drawee/backends/pipeline/Fresco.java
+++ drawee-backends/drawee-pipeline/src/main/java/com/facebook/drawee/backends/pipeline/Fresco.java
@@ -9,8 +9,11 @@
 
 package com.facebook.drawee.backends.pipeline;
 
+import javax.annotation.Nullable;
+
 import android.content.Context;
 
+import com.facebook.common.logging.FLog;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.core.ImagePipeline;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
@@ -23,21 +26,38 @@ import com.facebook.imagepipeline.core.ImagePipelineFactory;
  * {#code Fresco.initialize(Context)}.
  */
 public class Fresco {
+
+  private static final Class<?> TAG = Fresco.class;
+
   private static PipelineDraweeControllerBuilderSupplier sDraweeControllerBuilderSupplier;
+  private static volatile boolean sIsInitialized = false;
 
   private Fresco() {}
 
   /** Initializes Fresco with the default config. */
   public static void initialize(Context context) {
-    context = context.getApplicationContext();
-    ImagePipelineFactory.initialize(context);
-    initializeDrawee(context);
+    initialize(context, null);
   }
 
   /** Initializes Fresco with the specified config. */
-  public static void initialize(Context context, ImagePipelineConfig imagePipelineConfig) {
+  public static void initialize(
+      Context context,
+      @Nullable ImagePipelineConfig imagePipelineConfig) {
+    if (sIsInitialized) {
+      FLog.w(
+          TAG,
+          "Fresco has already been initialized! `Fresco.initialize(...)` should only be called " +
+            "1 single time to avoid memory leaks!");
+    } else {
+      sIsInitialized = true;
+    }
+    // we should always use the application context to avoid memory leaks
     context = context.getApplicationContext();
-    ImagePipelineFactory.initialize(imagePipelineConfig);
+    if (imagePipelineConfig == null) {
+      ImagePipelineFactory.initialize(context);
+    } else {
+      ImagePipelineFactory.initialize(imagePipelineConfig);
+    }
     initializeDrawee(context);
   }
 
@@ -70,5 +90,10 @@ public class Fresco {
     sDraweeControllerBuilderSupplier = null;
     SimpleDraweeView.shutDown();
     ImagePipelineFactory.shutDown();
+  }
+
+  /** Returns true if Fresco has been initialized. */
+  public static boolean hasBeenInitialized() {
+    return sIsInitialized;
   }
 }
