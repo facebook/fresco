ad0732d073e65e404ae376448681b8d2102143b1
Eric Longuemare <e.longuemare@laposte.net>
2016-09-22 04:49:56 +0200

    issue-1472:Add statement to check if mCacheStats isInitialized in getSize() and getCount(), if not, call maybeUpdateFileCacheSize().

--- imagepipeline-base/src/main/java/com/facebook/cache/disk/DiskStorageCache.java
+++ imagepipeline-base/src/main/java/com/facebook/cache/disk/DiskStorageCache.java
@@ -592,10 +592,16 @@ public class DiskStorageCache implements FileCache, DiskTrimmable {
   }
 
   public long getSize() {
+    if (!mCacheStats.isInitialized()){
+      maybeUpdateFileCacheSize();
+    }
     return mCacheStats.getSize();
   }
 
   public long getCount() {
+    if (!mCacheStats.isInitialized()){
+      maybeUpdateFileCacheSize();
+    }
     return mCacheStats.getCount();
   }
 
