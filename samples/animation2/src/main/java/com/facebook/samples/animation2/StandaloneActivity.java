052d4dd183d40be699a881a7c2aedc25226e34ca
Alexander Oprisnik <oprisnik@fb.com>
2016-11-17 04:43:19 -0800

    Animations 2 sample app: wrap backend with inactivity check
    Summary: The sample app for animations2 now wraps the color backend with an inactivity check that displays a toast message when the backend is inactive (has not been drawn within the last 2 seconds).
    
    Reviewed By: kirwan
    
    Differential Revision: D4197284
    
    fbshipit-source-id: 0446dbfdcf626a200892574f7aaad88a4a8fedd0

--- samples/animation2/src/main/java/com/facebook/samples/animation2/StandaloneActivity.java
+++ samples/animation2/src/main/java/com/facebook/samples/animation2/StandaloneActivity.java
@@ -23,24 +23,23 @@ import android.os.Bundle;
 import android.support.annotation.IntRange;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.common.executors.UiThreadImmediateExecutorService;
+import com.facebook.common.time.RealtimeSinceBootClock;
 import com.facebook.fresco.animation.backend.AnimationBackend;
+import com.facebook.fresco.animation.backend.AnimationBackendDelegateWithInactivityCheck;
 import com.facebook.fresco.animation.drawable.AnimatedDrawable2;
 
 /**
  * Simple standalone activity that creates a new {@link AnimatedDrawable2} and a custom backend
  * that cycles through colors.
+ * Tap the view to start / stop the animation.
+ *
+ * When the animation is inactive for more than 2 seconds (no new frames drawn), a toast message
+ * will be displayed.
  */
 public class StandaloneActivity extends AppCompatActivity {
-
-  private static final int[] EXAMPLE_COLORS = {
-      Color.RED,
-      Color.GREEN,
-      Color.BLUE,
-      Color.CYAN,
-      Color.BLACK,
-      Color.GRAY,
-  };
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +49,16 @@ public class StandaloneActivity extends AppCompatActivity {
     // Get the animation container
     View animationContainer = findViewById(R.id.animation_container);
 
-    // Get the animation duration in ms for each color frame
-    int frameDurationMs = getResources().getInteger(android.R.integer.config_mediumAnimTime);
+    // Create the animation backend
+    // In addition, we wrap it with an activity check. Tap the view to stop the animation to see
+    // an inactivity toast message after 2 seconds.
+    // In order to remove the inactivity check, just remove the wrapper method and set it to
+    // the backend directly.
+    AnimationBackend animationBackend = wrapAnimationBackendWithInactivityCheck(
+        createColorAnimationBackend());
 
     // Create a new animated drawable with the example backend
-    final AnimatedDrawable2 animatedDrawable = new AnimatedDrawable2(
-        new ExampleColorBackend(EXAMPLE_COLORS, frameDurationMs));
+    final AnimatedDrawable2 animatedDrawable = new AnimatedDrawable2(animationBackend);
 
     // Set the animation as a background
     animationContainer.setBackgroundDrawable(animatedDrawable);
@@ -74,6 +77,55 @@ public class StandaloneActivity extends AppCompatActivity {
 
     // Start the animation
     animatedDrawable.start();
+  }
+
+  /**
+   * Creates a simple animation backend that cycles through a list of colors.
+   *
+   * @return the backend to use
+   */
+  private AnimationBackend createColorAnimationBackend() {
+    // Define the colors
+    int[] colors = {
+        Color.RED,
+        Color.GREEN,
+        Color.BLUE,
+        Color.CYAN,
+        Color.BLACK,
+        Color.GRAY,
+    };
+    // Get the animation duration in ms for each color frame
+    int frameDurationMs = getResources().getInteger(android.R.integer.config_mediumAnimTime);
+    // Create and return the backend
+    return new ExampleColorBackend(colors, frameDurationMs);
+  }
+
+  /**
+   * Wraps the given animation backend with an activity check.
+   * When no frame has been drawn for more than 2 seconds, an inactivity toast message will
+   * be displayed.
+   *
+   * @param animationBackend the backend to wrap
+   * @return the backend to use
+   */
+  private AnimationBackend wrapAnimationBackendWithInactivityCheck(
+      AnimationBackend animationBackend) {
+    AnimationBackendDelegateWithInactivityCheck.InactivityListener inactivityListener =
+        new AnimationBackendDelegateWithInactivityCheck.InactivityListener() {
+      @Override
+      public void onInactive() {
+        Toast.makeText(
+            StandaloneActivity.this,
+            "Animation backend inactive.",
+            Toast.LENGTH_SHORT)
+            .show();
+      }
+    };
+    return AnimationBackendDelegateWithInactivityCheck.createForBackend(
+        animationBackend,
+        inactivityListener,
+        RealtimeSinceBootClock.get(),
+        UiThreadImmediateExecutorService.getInstance());
   }
 
   private static class ExampleColorBackend implements AnimationBackend {
