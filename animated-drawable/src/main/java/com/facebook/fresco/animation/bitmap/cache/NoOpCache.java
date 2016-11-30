49111ae34c629d31b712b048d9b0f3a6bf775cef
Alexander Oprisnik <oprisnik@fb.com>
2016-11-29 18:11:57 -0800

    Added no-op cache
    Summary: Added a no-op cache that does not do any caching.
    
    Reviewed By: kirwan
    
    Differential Revision: D4239555
    
    fbshipit-source-id: 56f99402a2ceb9d6a1163b60c2fda2f0ae4f2eb5

---
+++ animated-drawable/src/main/java/com/facebook/fresco/animation/bitmap/cache/NoOpCache.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.fresco.animation.bitmap.cache;
+
+import javax.annotation.Nullable;
+
+import android.graphics.Bitmap;
+
+import com.facebook.common.references.CloseableReference;
+import com.facebook.fresco.animation.bitmap.BitmapAnimationBackend;
+import com.facebook.fresco.animation.bitmap.BitmapFrameCache;
+
+/**
+ * No-op bitmap cache that doesn't do anything.
+ */
+public class NoOpCache implements BitmapFrameCache {
+
+  @Nullable
+  @Override
+  public CloseableReference<Bitmap> getCachedFrame(int frameNumber) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public CloseableReference<Bitmap> getFallbackFrame(int frameNumber) {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  public CloseableReference<Bitmap> getBitmapToReuseForFrame(
+      int frameNumber,
+      int width,
+      int height) {
+    return null;
+  }
+
+  @Override
+  public int getSizeInBytes() {
+    return 0;
+  }
+
+  @Override
+  public void clear() {
+    // no-op
+  }
+
+  @Override
+  public void onFrameRendered(
+      int frameNumber,
+      CloseableReference<Bitmap> bitmap,
+      @BitmapAnimationBackend.FrameType int frameType) {
+    // no-op
+  }
+}
