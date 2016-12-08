e9e9360daf0a2e3de55b5c131199b36950111c47
Daniel Hugenroth <hugenroth@fb.com>
2016-12-08 10:48:29 -0800

    Add unit tests for debug overlay behaviour with regard to scale types
    Summary: This adds additional unit tests for the calculations in `DebugControllerOverlayDrawable`.
    
    Reviewed By: kirwan
    
    Differential Revision: D4292529
    
    fbshipit-source-id: 58aec056d41067ee076f79c4ffd33dc8a15adb68

---
+++ drawee/src/test/java/com/facebook/drawee/debug/DebugControllerOverlayDrawableInternalTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.drawee.debug;
+
+import com.facebook.drawee.drawable.ScalingUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import robolectric3.shadows.ShadowMatrix;
+
+/**
+ * Test cases for {@link DebugControllerOverlayDrawable} that are not included in the gradle build
+ * as they depend on a working `ShadowMatrix` implementation.
+ */
+@Config(shadows = {ShadowMatrix.class})
+@RunWith(RobolectricTestRunner.class)
+public class DebugControllerOverlayDrawableInternalTest {
+
+  DebugControllerOverlayDrawableTestHelper helper;
+
+  @Before
+  public void setUp() {
+    helper = new DebugControllerOverlayDrawableTestHelper();
+  }
+
+  @Test
+  public void testOverlayWhenScaleTypeFitCenter() {
+    helper.assertOverlayColorOk(100, 100, 100, 100, ScalingUtils.ScaleType.FIT_CENTER);
+    helper.assertOverlayColorOk(100, 100, 1000, 100, ScalingUtils.ScaleType.FIT_CENTER);
+    helper.assertOverlayColorOk(100, 100, 100, 1000, ScalingUtils.ScaleType.FIT_CENTER);
+
+    helper.assertOverlayColorNotOk(100, 100, 1000, 1000, ScalingUtils.ScaleType.FIT_CENTER);
+    helper.assertOverlayColorNotOk(100, 100, 10, 10, ScalingUtils.ScaleType.FIT_CENTER);
+  }
+
+  @Test
+  public void testOverlayWhenScaleTypeFitXY() {
+    helper.assertOverlayColorOk(100, 100, 100, 100, ScalingUtils.ScaleType.FIT_XY);
+
+    helper.assertOverlayColorNotOk(100, 100, 1000, 100, ScalingUtils.ScaleType.FIT_XY);
+    helper.assertOverlayColorNotOk(100, 100, 100, 1000, ScalingUtils.ScaleType.FIT_XY);
+    helper.assertOverlayColorNotOk(100, 100, 1000, 1000, ScalingUtils.ScaleType.FIT_XY);
+    helper.assertOverlayColorNotOk(100, 100, 10, 10, ScalingUtils.ScaleType.FIT_XY);
+  }
+
+  @Test
+  public void testOverlayWhenScaleTypeCenter() {
+    helper.assertOverlayColorOk(100, 100, 100, 100, ScalingUtils.ScaleType.CENTER);
+    helper.assertOverlayColorOk(100, 100, 1000, 100, ScalingUtils.ScaleType.CENTER);
+    helper.assertOverlayColorOk(100, 100, 100, 1000, ScalingUtils.ScaleType.CENTER);
+    helper.assertOverlayColorOk(100, 100, 1000, 1000, ScalingUtils.ScaleType.CENTER);
+
+    helper.assertOverlayColorNotOk(100, 100, 10, 10, ScalingUtils.ScaleType.CENTER);
+  }
+
+  @Test
+  public void testOverlayWhenScaleTypeCenterCrop() {
+    helper.assertOverlayColorOk(100, 100, 100, 100, ScalingUtils.ScaleType.CENTER_CROP);
+
+    helper.assertOverlayColorNotOk(100, 100, 1000, 100, ScalingUtils.ScaleType.CENTER_CROP);
+    helper.assertOverlayColorNotOk(100, 100, 100, 1000, ScalingUtils.ScaleType.CENTER_CROP);
+    helper.assertOverlayColorNotOk(100, 100, 1000, 1000, ScalingUtils.ScaleType.CENTER_CROP);
+    helper.assertOverlayColorNotOk(100, 100, 10, 10, ScalingUtils.ScaleType.CENTER_CROP);
+  }
+}
--- drawee/src/test/java/com/facebook/drawee/debug/DebugControllerOverlayDrawableTest.java
+++ drawee/src/test/java/com/facebook/drawee/debug/DebugControllerOverlayDrawableTest.java
@@ -8,14 +8,10 @@
  */
 package com.facebook.drawee.debug;
 
-import com.facebook.drawee.drawable.ScalingUtils.ScaleType;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
-
-import static org.junit.Assert.assertEquals;
 
 /**
  * Tests {@link DebugControllerOverlayDrawable}
@@ -23,128 +19,83 @@ import static org.junit.Assert.assertEquals;
 @RunWith(RobolectricTestRunner.class)
 public class DebugControllerOverlayDrawableTest {
 
-  public DebugControllerOverlayDrawable mOverlayDrawable;
+  DebugControllerOverlayDrawableTestHelper helper;
 
   @Before
   public void setUp() {
-    mOverlayDrawable = new DebugControllerOverlayDrawable();
+    helper = new DebugControllerOverlayDrawableTestHelper();
   }
 
   @Test
   public void testOverlayColorOkWhenSameSize() {
-    assertOverlayColorOk(100, 100, 100, 100, null);
-    assertOverlayColorOk(160, 90, 160, 90, null);
-    assertOverlayColorOk(10, 400, 10, 400, null);
+    helper.assertOverlayColorOk(100, 100, 100, 100, null);
+    helper.assertOverlayColorOk(160, 90, 160, 90, null);
+    helper.assertOverlayColorOk(10, 400, 10, 400, null);
   }
 
   @Test
   public void testOverlayColorOkWhenDifferentSize() {
-    assertOverlayColorOk(101, 50, 100, 50, null);
-    assertOverlayColorOk(99, 50, 100, 50, null);
-    assertOverlayColorOk(100, 52, 100, 50, null);
-    assertOverlayColorOk(100, 48, 100, 50, null);
+    helper.assertOverlayColorOk(101, 50, 100, 50, null);
+    helper.assertOverlayColorOk(99, 50, 100, 50, null);
+    helper.assertOverlayColorOk(100, 52, 100, 50, null);
+    helper.assertOverlayColorOk(100, 48, 100, 50, null);
 
-    assertOverlayColorOk(101, 49, 100, 50, null);
-    assertOverlayColorOk(98, 52, 100, 50, null);
+    helper.assertOverlayColorOk(101, 49, 100, 50, null);
+    helper.assertOverlayColorOk(98, 52, 100, 50, null);
 
-    assertOverlayColorOk(109, 50, 100, 50, null);
-    assertOverlayColorOk(91, 50, 100, 50, null);
-    assertOverlayColorOk(100, 54, 100, 50, null);
-    assertOverlayColorOk(100, 46, 100, 50, null);
+    helper.assertOverlayColorOk(109, 50, 100, 50, null);
+    helper.assertOverlayColorOk(91, 50, 100, 50, null);
+    helper.assertOverlayColorOk(100, 54, 100, 50, null);
+    helper.assertOverlayColorOk(100, 46, 100, 50, null);
 
-    assertOverlayColorOk(109, 54, 100, 50, null);
-    assertOverlayColorOk(109, 46, 100, 50, null);
-    assertOverlayColorOk(91, 54, 100, 50, null);
-    assertOverlayColorOk(91, 46, 100, 50, null);
+    helper.assertOverlayColorOk(109, 54, 100, 50, null);
+    helper.assertOverlayColorOk(109, 46, 100, 50, null);
+    helper.assertOverlayColorOk(91, 54, 100, 50, null);
+    helper.assertOverlayColorOk(91, 46, 100, 50, null);
   }
 
   @Test
   public void testOverlayColorAlmostOk() {
-    assertOverlayColorAlmostOk(110, 50, 100, 50, null);
-    assertOverlayColorAlmostOk(90, 50, 100, 50, null);
-    assertOverlayColorAlmostOk(100, 45, 100, 50, null);
-    assertOverlayColorAlmostOk(100, 55, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(110, 50, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(90, 50, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(100, 45, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(100, 55, 100, 50, null);
 
-    assertOverlayColorAlmostOk(110, 55, 100, 50, null);
-    assertOverlayColorAlmostOk(110, 45, 100, 50, null);
-    assertOverlayColorAlmostOk(90, 55, 100, 50, null);
-    assertOverlayColorAlmostOk(90, 45, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(110, 55, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(110, 45, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(90, 55, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(90, 45, 100, 50, null);
 
-    assertOverlayColorAlmostOk(149, 50, 100, 50, null);
-    assertOverlayColorAlmostOk(149, 74, 100, 50, null);
-    assertOverlayColorAlmostOk(51, 74, 100, 50, null);
-    assertOverlayColorAlmostOk(51, 26, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(149, 50, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(149, 74, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(51, 74, 100, 50, null);
+    helper.assertOverlayColorAlmostOk(51, 26, 100, 50, null);
   }
 
   @Test
   public void testOverlayColorNotOk() {
-    assertOverlayColorNotOk(150, 50, 100, 50, null);
-    assertOverlayColorNotOk(100, 75, 100, 50, null);
-    assertOverlayColorNotOk(100, 100, 100, 50, null);
-    assertOverlayColorNotOk(50, 50, 100, 50, null);
-    assertOverlayColorNotOk(100, 25, 100, 50, null);
+    helper.assertOverlayColorNotOk(150, 50, 100, 50, null);
+    helper.assertOverlayColorNotOk(100, 75, 100, 50, null);
+    helper.assertOverlayColorNotOk(100, 100, 100, 50, null);
+    helper.assertOverlayColorNotOk(50, 50, 100, 50, null);
+    helper.assertOverlayColorNotOk(100, 25, 100, 50, null);
 
-    assertOverlayColorNotOk(1000, 50, 100, 50, null);
-    assertOverlayColorNotOk(500, 50, 100, 50, null);
-    assertOverlayColorNotOk(50, 100, 100, 50, null);
-    assertOverlayColorNotOk(150, 25, 100, 50, null);
+    helper.assertOverlayColorNotOk(1000, 50, 100, 50, null);
+    helper.assertOverlayColorNotOk(500, 50, 100, 50, null);
+    helper.assertOverlayColorNotOk(50, 100, 100, 50, null);
+    helper.assertOverlayColorNotOk(150, 25, 100, 50, null);
 
-    assertOverlayColorNotOk(1000, 500, 100, 50, null);
-    assertOverlayColorNotOk(10, 5, 100, 50, null);
-    assertOverlayColorNotOk(1, 1, 100, 50, null);
+    helper.assertOverlayColorNotOk(1000, 500, 100, 50, null);
+    helper.assertOverlayColorNotOk(10, 5, 100, 50, null);
+    helper.assertOverlayColorNotOk(1, 1, 100, 50, null);
   }
 
   @Test
   public void testOverlayColorNotOkWhenZeroDimension() {
-    assertOverlayColorNotOk(0, 0, 0, 0, null);
-    assertOverlayColorNotOk(0, 100, 100, 100, null);
-    assertOverlayColorNotOk(100, 0, 100, 100, null);
-    assertOverlayColorNotOk(100, 100, 0, 100, null);
-    assertOverlayColorNotOk(100, 100, 100, 0, null);
-  }
-
-  private void assertOverlayColorOk(
-      int imageWidth,
-      int imageHeight,
-      int drawableWidth,
-      int drawableHeight,
-      ScaleType scaleType) {
-    mOverlayDrawable.setBounds(0, 0, drawableWidth, drawableHeight);
-    assertEquals(
-        DebugControllerOverlayDrawable.OVERLAY_COLOR_IMAGE_OK,
-        mOverlayDrawable.determineOverlayColor(
-            imageWidth,
-            imageHeight,
-            scaleType));
-  }
-
-  private void assertOverlayColorAlmostOk(
-      int imageWidth,
-      int imageHeight,
-      int drawableWidth,
-      int drawableHeight,
-      ScaleType scaleType) {
-    mOverlayDrawable.setBounds(0, 0, drawableWidth, drawableHeight);
-    assertEquals(
-        DebugControllerOverlayDrawable.OVERLAY_COLOR_IMAGE_ALMOST_OK,
-        mOverlayDrawable.determineOverlayColor(
-            imageWidth,
-            imageHeight,
-            scaleType));
-  }
-
-  private void assertOverlayColorNotOk(
-      int imageWidth,
-      int imageHeight,
-      int drawableWidth,
-      int drawableHeight,
-      ScaleType scaleType) {
-    mOverlayDrawable.setBounds(0, 0, drawableWidth, drawableHeight);
-    assertEquals(
-        DebugControllerOverlayDrawable.OVERLAY_COLOR_IMAGE_NOT_OK,
-        mOverlayDrawable.determineOverlayColor(
-            imageWidth,
-            imageHeight,
-            scaleType));
+    helper.assertOverlayColorNotOk(0, 0, 0, 0, null);
+    helper.assertOverlayColorNotOk(0, 100, 100, 100, null);
+    helper.assertOverlayColorNotOk(100, 0, 100, 100, null);
+    helper.assertOverlayColorNotOk(100, 100, 0, 100, null);
+    helper.assertOverlayColorNotOk(100, 100, 100, 0, null);
   }
 }
