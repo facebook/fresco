609df7df5b51e4bb488a7d6f07f92abcad887699
Daniel Hugenroth <hugenroth@fb.com>
2016-12-21 06:05:26 -0800

    Showcase sample app: make SVG and Keyframes examples use provided sample images and optional checkerboard background
    Summary: After this change the SVG sample will use a half-transparent version of the Fresco logo. Also, both the SVG and Keyframes examples allow using a checkerboard background for verifying transparency.
    
    Reviewed By: foghina
    
    Differential Revision: D4352133
    
    fbshipit-source-id: 34b46c143bedb0dbaefd4fa999a5871922376a75

--- samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/imageformat/keyframes/ImageFormatKeyframesFragment.java
+++ samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/imageformat/keyframes/ImageFormatKeyframesFragment.java
@@ -11,13 +11,16 @@
  */
 package com.facebook.fresco.samples.showcase.imageformat.keyframes;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v7.widget.SwitchCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.interfaces.DraweeController;
@@ -25,11 +28,17 @@ import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.fresco.samples.showcase.BaseShowcaseFragment;
 import com.facebook.fresco.samples.showcase.CustomImageFormatConfigurator;
 import com.facebook.fresco.samples.showcase.R;
+import com.facebook.fresco.samples.showcase.misc.CheckerBoardDrawable;
 
 /**
  * Fragment using a SimpleDraweeView to display a Keyframes animation
  */
 public class ImageFormatKeyframesFragment extends BaseShowcaseFragment {
+
+  public static final Uri URI_KEYFRAMES_ANIMATION =
+      Uri.parse("http://frescolib.org/static/sample-images/animation.keyframes");
+
+  private SimpleDraweeView mSimpleDraweeView;
 
   @Nullable
   @Override
@@ -51,16 +60,27 @@ public class ImageFormatKeyframesFragment extends BaseShowcaseFragment {
   public int getTitleId() {
     return R.string.format_keyframes_title;
   }
+
   @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
   private void initAnimation(View view) {
-    SimpleDraweeView simpleDraweeView = (SimpleDraweeView) view.findViewById(R.id.drawee_view);
-    simpleDraweeView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    mSimpleDraweeView = (SimpleDraweeView) view.findViewById(R.id.drawee_view);
+    mSimpleDraweeView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
     DraweeController controller = Fresco.newDraweeControllerBuilder()
-        .setOldController(simpleDraweeView.getController())
-        .setUri("http://frescolib.org/static/sample-images/animation.keyframes")
+        .setOldController(mSimpleDraweeView.getController())
+        .setUri(URI_KEYFRAMES_ANIMATION)
         .setAutoPlayAnimations(true)
         .build();
-    simpleDraweeView.setController(controller);
+    mSimpleDraweeView.setController(controller);
+
+    final SwitchCompat switchBackground = (SwitchCompat) view.findViewById(R.id.switch_background);
+    switchBackground.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        mSimpleDraweeView.getHierarchy().setBackgroundImage(isChecked
+            ? new CheckerBoardDrawable(getResources())
+            : null);
+      }
+    });
   }
 
 }
--- samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/imageformat/svg/ImageFormatSvgFragment.java
+++ samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/imageformat/svg/ImageFormatSvgFragment.java
@@ -11,6 +11,7 @@
  */
 package com.facebook.fresco.samples.showcase.imageformat.svg;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.SwitchCompat;
@@ -23,11 +24,17 @@ import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.fresco.samples.showcase.BaseShowcaseFragment;
 import com.facebook.fresco.samples.showcase.CustomImageFormatConfigurator;
 import com.facebook.fresco.samples.showcase.R;
+import com.facebook.fresco.samples.showcase.misc.CheckerBoardDrawable;
 
 /**
  * SVG example. It has a toggle to enable / disable SVG support and displays 1 image.
  */
 public class ImageFormatSvgFragment extends BaseShowcaseFragment {
+
+  private static final Uri URI_SVG_HALF_TRANSPARENT =
+      Uri.parse("http://frescolib.org/static/sample-images/fresco_logo_half_transparent.svg");
+
+  private SimpleDraweeView mSimpleDraweeView;
 
   @Nullable
   @Override
@@ -38,9 +45,18 @@ public class ImageFormatSvgFragment extends BaseShowcaseFragment {
 
   @Override
   public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-    SimpleDraweeView simpleDraweeView = (SimpleDraweeView) view.findViewById(R.id.drawee_view);
-    simpleDraweeView
-        .setImageURI("https://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg");
+    mSimpleDraweeView = (SimpleDraweeView) view.findViewById(R.id.drawee_view);
+    mSimpleDraweeView.setImageURI(URI_SVG_HALF_TRANSPARENT);
+
+    final SwitchCompat switchBackground = (SwitchCompat) view.findViewById(R.id.switch_background);
+    switchBackground.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        mSimpleDraweeView.getHierarchy().setBackgroundImage(isChecked
+            ? new CheckerBoardDrawable(getResources())
+            : null);
+      }
+    });
 
     SwitchCompat switchCompat = (SwitchCompat) view.findViewById(R.id.decoder_switch);
     switchCompat.setChecked(CustomImageFormatConfigurator.isSvgEnabled(getContext()));
