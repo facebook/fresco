2fc87f830e3ad3ee4c7826feabf8fbd0fcf7e22a
Alexander Oprisnik <oprisnik@fb.com>
2017-01-12 15:32:25 -0800

    Animations v2: extracted color sample backend
    Summary: The color sample is now in a separate class so that it's easier to re-use.
    
    Reviewed By: massimocarli
    
    Differential Revision: D4409508
    
    fbshipit-source-id: 0d949c287e678e1f84e93aca335c4b54ce15a69d

--- samples/animation2/src/main/java/com/facebook/samples/animation2/color/SimpleColorFragment.java
+++ samples/animation2/src/main/java/com/facebook/samples/animation2/color/SimpleColorFragment.java
@@ -11,14 +11,7 @@
  */
 package com.facebook.samples.animation2.color;
 
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.IntRange;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -32,9 +25,8 @@ import com.facebook.samples.animation2.R;
 import com.facebook.samples.animation2.utils.AnimationBackendUtils;
 
 /**
- * Simple standalone activity that creates a new {@link AnimatedDrawable2} and a custom backend
- * that cycles through colors.
- * Tap the view to start / stop the animation.
+ * Simple standalone activity that creates a new {@link AnimatedDrawable2} and a custom backend that
+ * cycles through colors. Tap the view to start / stop the animation.
  *
  * When the animation is inactive for more than 2 seconds (no new frames drawn), a toast message
  * will be displayed.
@@ -63,7 +55,7 @@ public class SimpleColorFragment extends Fragment {
     AnimationBackend animationBackend =
         AnimationBackendUtils.wrapAnimationBackendWithInactivityCheck(
             getContext(),
-            createColorAnimationBackend());
+            ExampleColorBackend.createSampleColorAnimationBackend(getResources()));
 
     // Create a new animated drawable with the example backend
     final AnimatedDrawable2 animatedDrawable = new AnimatedDrawable2(animationBackend);
@@ -85,100 +77,5 @@ public class SimpleColorFragment extends Fragment {
 
     // Start the animation
     animatedDrawable.start();
-  }
-
-  /**
-   * Creates a simple animation backend that cycles through a list of colors.
-   *
-   * @return the backend to use
-   */
-  private AnimationBackend createColorAnimationBackend() {
-    // Define the colors
-    int[] colors = {
-        Color.RED,
-        Color.GREEN,
-        Color.BLUE,
-        Color.CYAN,
-        Color.BLACK,
-        Color.GRAY,
-    };
-    // Get the animation duration in ms for each color frame
-    int frameDurationMs = getResources().getInteger(android.R.integer.config_mediumAnimTime);
-    // Create and return the backend
-    return new ExampleColorBackend(colors, frameDurationMs);
-  }
-
-  private static class ExampleColorBackend implements AnimationBackend {
-
-    private final Paint mPaint = new Paint();
-    private final int[] mColors;
-    private final int mFrameDurationMs;
-
-    private Rect mBounds;
-
-    private ExampleColorBackend(int[] colors, int frameDurationMs) {
-      mColors = colors;
-      mFrameDurationMs = frameDurationMs;
-    }
-
-    @Override
-    public int getFrameCount() {
-      return mColors.length;
-    }
-
-    @Override
-    public int getFrameDurationMs(int frameNumber) {
-      return mFrameDurationMs;
-    }
-
-    @Override
-    public int getLoopCount() {
-      return 3;
-    }
-
-    @Override
-    public boolean drawFrame(
-        Drawable parent, Canvas canvas, int frameNumber) {
-      if (mBounds == null) {
-        return false;
-      }
-      mPaint.setColor(mColors[frameNumber]);
-      canvas.drawRect(mBounds, mPaint);
-      return true;
-    }
-
-    @Override
-    public void setAlpha(@IntRange(from = 0, to = 255) int alpha) {
-      mPaint.setAlpha(alpha);
-    }
-
-    @Override
-    public void setColorFilter(@Nullable ColorFilter colorFilter) {
-      mPaint.setColorFilter(colorFilter);
-    }
-
-    @Override
-    public void setBounds(Rect bounds) {
-      mBounds = bounds;
-    }
-
-    @Override
-    public int getSizeInBytes() {
-      return mColors.length * 4;
-    }
-
-    @Override
-    public void clear() {
-    }
-
-    @Override
-    public int getIntrinsicWidth() {
-      return INTRINSIC_DIMENSION_UNSET;
-    }
-
-    @Override
-    public int getIntrinsicHeight() {
-      return INTRINSIC_DIMENSION_UNSET;
-    }
   }
 }
