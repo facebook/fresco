0947c1e788b7378450e45945c4f71f420948ed7f
Alexander Oprisnik <oprisnik@fb.com>
2017-01-12 15:32:27 -0800

    Added animations v2 media control example
    Summary: This adds a simple media control example for animations v2.
    
    Reviewed By: massimocarli
    
    Differential Revision: D4404478
    
    fbshipit-source-id: d16d4a39d76fbd3035addb4ab887e4b4202b4b85

---
+++ samples/animation2/src/main/java/com/facebook/samples/animation2/MediaControlFragment.java
@@ -0,0 +1,73 @@
+/*
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.facebook.samples.animation2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.ToggleButton;
+
+import com.facebook.fresco.animation.backend.AnimationBackend;
+import com.facebook.fresco.animation.drawable.AnimatedDrawable2;
+import com.facebook.samples.animation2.color.ExampleColorBackend;
+import com.facebook.samples.animation2.utils.AnimationBackendUtils;
+import com.facebook.samples.animation2.utils.AnimationControlsManager;
+
+/**
+ * Sample that displays an animated image and media controls to start / stop / seek.
+ */
+public class MediaControlFragment extends Fragment {
+
+  private AnimationControlsManager mAnimationControlsManager;
+  private AnimatedDrawable2 mAnimatedDrawable;
+
+  @Nullable
+  @Override
+  public View onCreateView(
+      LayoutInflater inflater,
+      @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.fragment_media_controls, container, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    // Get the animation container
+    ImageView imageView = (ImageView) view.findViewById(R.id.animation_container);
+
+    // Create the animation backend
+    // In addition, we wrap it with an activity check. Tap the view to stop the animation to see
+    // an inactivity toast message after 2 seconds.
+    // In order to remove the inactivity check, just remove the wrapper method and set it to
+    // the backend directly.
+    AnimationBackend animationBackend =
+        AnimationBackendUtils.wrapAnimationBackendWithInactivityCheck(
+            getContext(),
+            ExampleColorBackend.createSampleColorAnimationBackend(getResources()));
+
+    // Create a new animated drawable, assign it to the image view and start the animation.
+    mAnimatedDrawable = new AnimatedDrawable2(animationBackend);
+
+    imageView.setImageDrawable(mAnimatedDrawable);
+
+    mAnimationControlsManager = new AnimationControlsManager(
+        mAnimatedDrawable,
+        (SeekBar) view.findViewById(R.id.seekbar),
+        (ToggleButton) view.findViewById(R.id.playpause),
+        view.findViewById(R.id.reset));
+  }
+}
