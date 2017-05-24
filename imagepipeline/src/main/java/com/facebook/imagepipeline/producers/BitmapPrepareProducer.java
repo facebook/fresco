b9f6fdbc9b02971c7ef99bd4ceec7a48f8c196c7
Daniel Hugenroth <hugenroth@fb.com>
2017-05-23 19:17:38 -0700

    Bitmap.prepareToDraw: Add BitmapPrepareProducer
    Summary:
    This change adds a `BitmapPrepareProducer` that optimistically uploads a `CloseableStaticBitmap` to the GPU. Requests that are pre-fetching are ignored and just passed through.
    
    By calling `Bitmap#prepareToDraw` the `RenderThread` can upload the `Bitmap` asynchronously when it's idle. This has often moves the upload out of `DrawFrame` and therefore improves the critical path.
    
    This came up from issue https://github.com/facebook/fresco/issues/1756 that was risen from the Android UI Toolkit Graphics team.
    
    I've verified the change using `systrace` on my Google Pixel phone. Screenshots in the comments
    
    Reviewed By: erikandre
    
    Differential Revision: D5106592
    
    fbshipit-source-id: b2c9873c7b120f8bf4ab56c1eac9819c787b0757

---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/BitmapPrepareProducer.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.imagepipeline.producers;
+
+import android.graphics.Bitmap;
+
+import com.facebook.common.internal.Preconditions;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.image.CloseableStaticBitmap;
+
+/**
+ * This producer issues to a call to {@link android.graphics.Bitmap#prepareToDraw()} to allow the
+ * RendererThread upload the bitmap to GPU asynchronously before it is used. This has no affect on
+ * Android versions before N.
+ *
+ * Controlled via {@link com.facebook.imagepipeline.core.ImagePipelineExperiments#mUseBitmapPrepareToDraw}
+ */
+public class BitmapPrepareProducer implements Producer<CloseableReference<CloseableImage>> {
+
+  public static final String PRODUCER_NAME = "BitmapPrepareProducer";
+
+  private final Producer<CloseableReference<CloseableImage>> mInputProducer;
+
+  public BitmapPrepareProducer(final Producer<CloseableReference<CloseableImage>> inputProducer) {
+    mInputProducer = Preconditions.checkNotNull(inputProducer);
+  }
+
+  @Override
+  public void produceResults(
+      final Consumer<CloseableReference<CloseableImage>> consumer,
+      final ProducerContext producerContext) {
+    if (producerContext.isPrefetch()) {
+      // do not prepare bitmaps that are pre-fetcheds
+      mInputProducer.produceResults(consumer, producerContext);
+    } else {
+      mInputProducer.produceResults(new BitmapPrepareConsumer(consumer), producerContext);
+    }
+  }
+
+  private static class BitmapPrepareConsumer extends
+      DelegatingConsumer<CloseableReference<CloseableImage>, CloseableReference<CloseableImage>> {
+
+    BitmapPrepareConsumer(Consumer<CloseableReference<CloseableImage>> consumer) {
+      super(consumer);
+    }
+
+    @Override
+    protected void onNewResultImpl(
+        CloseableReference<CloseableImage> newResult,
+        @Status int status) {
+      internalPrepareBitmap(newResult);
+      getConsumer().onNewResult(newResult, status);
+    }
+
+    private static void internalPrepareBitmap(CloseableReference<CloseableImage> newResult) {
+      if (newResult == null || !newResult.isValid()) {
+        return;
+      }
+
+      final CloseableImage closeableImage = newResult.get();
+      if (closeableImage == null || closeableImage.isClosed()) {
+        return;
+      }
+
+      if (closeableImage instanceof CloseableStaticBitmap) {
+        final CloseableStaticBitmap staticBitmap = (CloseableStaticBitmap) closeableImage;
+        final Bitmap bitmap = staticBitmap.getUnderlyingBitmap();
+        if (bitmap == null) {
+          return;
+        }
+
+        bitmap.prepareToDraw();
+      }
+    }
+  }
+}
---
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/producers/BitmapPrepareProducerTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+package com.facebook.imagepipeline.producers;
+
+import android.graphics.Bitmap;
+
+import com.facebook.common.references.CloseableReference;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.image.CloseableStaticBitmap;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class BitmapPrepareProducerTest {
+
+  @Mock public Producer<CloseableReference<CloseableImage>> mInputProducer;
+  @Mock public Consumer<CloseableReference<CloseableImage>> mConsumer;
+  @Mock public ProducerContext mProducerContext;
+
+  private CloseableReference<CloseableImage> mImageReference;
+  @Mock private CloseableStaticBitmap mCloseableStaticBitmap;
+  @Mock private Bitmap mBitmap;
+
+  private BitmapPrepareProducer mBitmapPrepareProducer;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    mBitmapPrepareProducer = new BitmapPrepareProducer(mInputProducer);
+
+    mImageReference = CloseableReference.of((CloseableImage) mCloseableStaticBitmap);
+    when(mCloseableStaticBitmap.getUnderlyingBitmap()).thenReturn(mBitmap);
+
+    doAnswer(new Answer() {
+      @Override
+      public Object answer(InvocationOnMock invocation) throws Throwable {
+        Object[] args = invocation.getArguments();
+        Consumer<CloseableReference<CloseableImage>> consumer =
+            (Consumer<CloseableReference<CloseableImage>>) args[0];
+        consumer.onNewResult(mImageReference, 0);
+        return null;
+      }
+    }).when(mInputProducer).produceResults(any(Consumer.class), any(ProducerContext.class));
+  }
+
+  @Test
+  public void testProduceResults_whenCalled_thenInputProducerCalled() {
+    mBitmapPrepareProducer.produceResults(mConsumer, mProducerContext);
+
+    verify(mInputProducer, times(1)).produceResults(any(Consumer.class), eq(mProducerContext));
+  }
+
+  @Test
+  public void testProduceResults_whenPrefetch_thenPassThrough() {
+    when(mProducerContext.isPrefetch()).thenReturn(true);
+
+    mBitmapPrepareProducer.produceResults(mConsumer, mProducerContext);
+
+    // note: the given consumer is used and not the BitmapPrepareConsumer
+    verify(mInputProducer, times(1)).produceResults(eq(mConsumer), eq(mProducerContext));
+  }
+
+  @Test
+  public void testProduceResults_whenNotPrefetch_thenBitmapPrepareToDrawCalled() {
+    when(mProducerContext.isPrefetch()).thenReturn(false);
+
+    mBitmapPrepareProducer.produceResults(mConsumer, mProducerContext);
+
+    verify(mBitmap, times(1)).prepareToDraw();
+  }
+
+  @Test
+  public void testProduceResults_whenPrefetch_thenBitmapPrepareToDrawCalled() {
+    when(mProducerContext.isPrefetch()).thenReturn(true);
+
+    mBitmapPrepareProducer.produceResults(mConsumer, mProducerContext);
+
+    verify(mBitmap, never()).prepareToDraw();
+  }
+}
