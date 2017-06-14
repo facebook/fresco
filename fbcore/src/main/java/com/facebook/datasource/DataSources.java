f013c8bbad39b2ec1fcb9289384f331a6a3a8a13
Daniel Hugenroth <hugenroth@fb.com>
2017-06-14 03:56:13 -0700

    Add DataSources#waitForFinalResult 2/w
    Summary:
    : A helper method to allow for synchronous fetches of decoded and encoded images.
    
    Sample usage:
    
    ```
    DataSource<CloseableReference<PooledByteBuffer>> dataSource = mImagePipeline.fetchEncodedImage(imageRequest, callerContext);
    try {
        final CloseableReference<PooledByteBuffer> result = DataSources.waitForFinalResult(dataSource);
        //
        // do something with result
        //
    } finally {
        dataSource.close();
    }
    ```
    
    Reviewed By: oprisnik
    
    Differential Revision: D5228893
    
    fbshipit-source-id: 1c3bfae6f4384d9fc36b7a7c607889d9615e0d5d

--- fbcore/src/main/java/com/facebook/datasource/DataSources.java
+++ fbcore/src/main/java/com/facebook/datasource/DataSources.java
@@ -9,6 +9,10 @@
 
 package com.facebook.datasource;
 
+import javax.annotation.Nullable;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
 
 import com.facebook.common.internal.Supplier;
 
@@ -39,5 +43,86 @@ public class DataSources {
         return DataSources.immediateFailedDataSource(failure);
       }
     };
+  }
+
+  /**
+   * This methods blocks the calling thread until the {@link DataSource} has a final result, has
+   * been cancelled or has failed.
+   *
+   * @param dataSource The {@link DataSource} to wait for. The caller MUST close the data source
+   * after this method returned!
+   * @param <T> The type parameter for the {@link DataSource}
+   *
+   * @return The final result of the {@link DataSource}. Intermediate results are ignored. Might be
+   * <code>null</code> if the data source has been cancelled.
+   *
+   * @throws Throwable if the {@link DataSource} has failed
+   */
+  @Nullable
+  public static <T> T waitForFinalResult(DataSource<T> dataSource) throws Throwable {
+    final CountDownLatch latch = new CountDownLatch(1);
+    final ValueHolder<T> resultHolder = new ValueHolder<>();
+    final ValueHolder<Throwable> pendingException = new ValueHolder<>();
+
+    dataSource.subscribe(
+        new DataSubscriber<T>() {
+          @Override
+          public void onNewResult(DataSource<T> dataSource) {
+            // only return the final result
+            if (!dataSource.isFinished()) {
+              return;
+            }
+
+            try {
+              resultHolder.value = dataSource.getResult();
+            } finally {
+              latch.countDown();
+            }
+          }
+
+          @Override
+          public void onFailure(DataSource<T> dataSource) {
+            try {
+              pendingException.value = dataSource.getFailureCause();
+            } finally {
+              latch.countDown();
+            }
+          }
+
+          @Override
+          public void onCancellation(DataSource<T> dataSource) {
+            // will make the outer method return null
+            latch.countDown();
+          }
+
+          @Override
+          public void onProgressUpdate(DataSource<T> dataSource) {
+            // intentionally left blank
+          }
+        },
+        new Executor() {
+          @Override
+          public void execute(Runnable command) {
+            // it's fair to run these short methods on the datasource thread to avoid
+            // context switching
+            command.run();
+          }
+        });
+
+    // wait for countdown() by the DataSubscriber
+    latch.await();
+
+    // if the data source failed, throw its exception
+    if (pendingException.value != null) {
+      throw pendingException.value;
+    }
+
+    return resultHolder.value;
+  }
+
+  private static class ValueHolder<T> {
+
+    @Nullable
+    public T value = null;
   }
 }
--- fbcore/src/test/java/com/facebook/datasource/DataSourcesTest.java
+++ fbcore/src/test/java/com/facebook/datasource/DataSourcesTest.java
@@ -9,22 +9,51 @@
 
 package com.facebook.datasource;
 
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+
+import org.junit.Rule;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareOnlyThisForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 import org.robolectric.RobolectricTestRunner;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(RobolectricTestRunner.class)
+@Config(manifest= Config.NONE)
+@PrepareOnlyThisForTest({DataSources.class})
+@PowerMockIgnore({ "org.mockito.*", "org.robolectric.*", "android.*" })
 public class DataSourcesTest {
+
+  @Rule
+  public PowerMockRule rule = new PowerMockRule();
+
+  private CountDownLatch mCountDownLatch;
   private Exception mException;
+  private DataSource<Object> mDataSource;
+
+  private final Object mFinalResult = "final";
+  private final Object mIntermediateResult = "intermediate";
 
   @Before
-  public void setUp() {
+  public void setUp() throws Exception {
     mException = mock(Exception.class);
+    mDataSource = mock(DataSource.class);
+
+    PowerMockito.mockStatic(CountDownLatch.class);
+    mCountDownLatch = mock(CountDownLatch.class);
+    PowerMockito.whenNew(CountDownLatch.class).withAnyArguments().thenReturn(mCountDownLatch);
   }
 
   @Test
@@ -35,5 +64,95 @@ public class DataSourcesTest {
     assertEquals(mException, dataSource.getFailureCause());
     assertFalse(dataSource.hasResult());
     assertFalse(dataSource.isClosed());
+  }
+
+  @Test
+  public void testWaitForFinalResult_whenFinalResult_thenReturnFinalResult() throws Throwable {
+    when(mDataSource.isFinished()).thenReturn(true);
+    when(mDataSource.getResult()).thenReturn(mFinalResult);
+
+    doAnswer(new Answer<Void>() {
+      @Override
+      public Void answer(InvocationOnMock invocation) throws Throwable {
+        final Object[] args = invocation.getArguments();
+        DataSubscriber dataSubscriber = (DataSubscriber) args[0];
+        dataSubscriber.onNewResult(mDataSource);
+        return null;
+      }
+    }).when(mDataSource).subscribe(any(DataSubscriber.class), any(Executor.class));
+
+    final Object actual = DataSources.waitForFinalResult(mDataSource);
+    assertEquals(mFinalResult, actual);
+
+    verify(mCountDownLatch, times(1)).await();
+    verify(mCountDownLatch, times(1)).countDown();
+  }
+
+  @Test
+  public void testWaitForFinalResult_whenOnlyIntermediateResult_thenNoUpdate() throws Throwable {
+    when(mDataSource.isFinished()).thenReturn(false);
+    when(mDataSource.getResult()).thenReturn(mIntermediateResult);
+
+    doAnswer(new Answer<Void>() {
+      @Override
+      public Void answer(InvocationOnMock invocation) throws Throwable {
+        final Object[] args = invocation.getArguments();
+        DataSubscriber dataSubscriber = (DataSubscriber) args[0];
+        dataSubscriber.onNewResult(mDataSource);
+        return null;
+      }
+    }).when(mDataSource).subscribe(any(DataSubscriber.class), any(Executor.class));
+
+    // the mocked one falls through, but the real one waits with the countdown latch for isFinished
+    final Object actual = DataSources.waitForFinalResult(mDataSource);
+    assertEquals(null, actual);
+
+    verify(mCountDownLatch, times(1)).await();
+    verify(mCountDownLatch, never()).countDown();
+  }
+
+  @Test
+  public void testWaitForFinalResult_whenCancelled_thenReturnNull() throws Throwable {
+    doAnswer(new Answer<Void>() {
+      @Override
+      public Void answer(InvocationOnMock invocation) throws Throwable {
+        final Object[] args = invocation.getArguments();
+        DataSubscriber dataSubscriber = (DataSubscriber) args[0];
+        dataSubscriber.onCancellation(mDataSource);
+        return null;
+      }
+    }).when(mDataSource).subscribe(any(DataSubscriber.class), any(Executor.class));
+
+    final Object actual = DataSources.waitForFinalResult(mDataSource);
+    assertEquals(null, actual);
+
+    verify(mCountDownLatch, times(1)).await();
+    verify(mCountDownLatch, times(1)).countDown();
+  }
+
+  @Test
+  public void testWaitForFinalResult_whenFailed_thenThrow() throws Throwable {
+    final Exception expectedException = new IOException("failure failure");
+    when(mDataSource.getFailureCause()).thenReturn(expectedException);
+
+    doAnswer(new Answer<Void>() {
+      @Override
+      public Void answer(InvocationOnMock invocation) throws Throwable {
+        final Object[] args = invocation.getArguments();
+        DataSubscriber dataSubscriber = (DataSubscriber) args[0];
+        dataSubscriber.onFailure(mDataSource);
+        return null;
+      }
+    }).when(mDataSource).subscribe(any(DataSubscriber.class), any(Executor.class));
+
+    try {
+      DataSources.waitForFinalResult(mDataSource);
+      fail("expected exception");
+    } catch (Exception exception) {
+      assertEquals(expectedException, exception);
+    }
+
+    verify(mCountDownLatch, times(1)).await();
+    verify(mCountDownLatch, times(1)).countDown();
   }
 }
