0df51c5591ee433715dc88cb6c00bb2e17002d78
Daniel Hugenroth <hugenroth@fb.com>
2017-07-10 11:04:36 -0700

    ImageUriProvider: Update SettingsFragment
    Reviewed By: oprisnik
    
    Differential Revision: D5389035
    
    fbshipit-source-id: f14cf1cacb7bb7c9666b0f4fe55e6661d26544ea

--- samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/settings/SettingsFragment.java
+++ samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/settings/SettingsFragment.java
@@ -23,11 +23,13 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.fresco.samples.showcase.R;
 import com.facebook.fresco.samples.showcase.ShowcaseFragment;
+import com.facebook.fresco.samples.showcase.misc.ImageUriProvider;
 
 /**
  * The Fragment for settings
@@ -39,18 +41,26 @@ public class SettingsFragment extends PreferenceFragmentCompat
 
   public static final String KEY_CLEAR_DISK_CACHE = "clear_disk_cache";
   public static final String KEY_DEBUG_OVERLAY = "debug_overlay";
+  public static final String KEY_URI_OVERRIDE = "uri_override";
 
   public static final String KEY_DETAILS_ANDROID_VERSION = "android_version";
   public static final String KEY_DETAILS_CPU_ARCHITECTURE = "cpu_architecture";
   public static final String KEY_DETAILS_DEVICE_NAME = "device_name";
 
-  /**
-   * The Dialog for asking the restart for the application
-   */
+  private UriOverrideDialog mSetUriOverrideDialog;
   private ShowRestartMessageDialog mShowRestartMessageDialog;
+
+  private ImageUriProvider mImageUriProvider;
 
   @Override
   public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    if (!isAdded()) {
+      return;
+    }
+    switch (key) {
+      case KEY_URI_OVERRIDE:
+        populateUriOverride();
+    }
   }
 
   @Override
@@ -59,6 +69,9 @@ public class SettingsFragment extends PreferenceFragmentCompat
       case KEY_CLEAR_DISK_CACHE:
         onClearDiskCachePreferenceClicked();
         return true;
+      case KEY_URI_OVERRIDE:
+        onUriOverrideClicked();
+        return true;
       default:
         return super.onPreferenceTreeClick(preference);
     }
@@ -66,14 +79,23 @@ public class SettingsFragment extends PreferenceFragmentCompat
 
   @Override
   public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    mImageUriProvider = ImageUriProvider.getInstance(getContext());
     addPreferencesFromResource(R.xml.preferences);
     getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    populateUriOverride();
     populateVersionAndDeviceDetails();
   }
 
   private void onClearDiskCachePreferenceClicked() {
     Fresco.getImagePipeline().clearCaches();
     showToastText(getString(R.string.preference_cache_clear_cache_message_success));
+  }
+
+  private void onUriOverrideClicked() {
+    if (mSetUriOverrideDialog == null) {
+      mSetUriOverrideDialog = new UriOverrideDialog();
+    }
+    mSetUriOverrideDialog.show(getFragmentManager(), "uri_override");
   }
 
   /**
@@ -84,6 +106,19 @@ public class SettingsFragment extends PreferenceFragmentCompat
       mShowRestartMessageDialog = new ShowRestartMessageDialog();
     }
     return mShowRestartMessageDialog;
+  }
+
+  private void populateUriOverride() {
+    final String uri = mImageUriProvider.getUriOverride();
+    final Preference preferenceUriOverride = findPreference(KEY_URI_OVERRIDE);
+    if (uri == null || uri.isEmpty()) {
+      preferenceUriOverride.setSummary(R.string.preference_uri_override_summary_none);
+    } else {
+      preferenceUriOverride.setSummary(
+          getString(
+              R.string.preference_uri_override_summary_given,
+              uri));
+    }
   }
 
   private void populateVersionAndDeviceDetails() {
@@ -133,6 +168,57 @@ public class SettingsFragment extends PreferenceFragmentCompat
   @Override
   public int getTitleId() {
     return R.string.action_settings;
+  }
+
+  /**
+   * Dialog asking for a new URI override
+   */
+  public static class UriOverrideDialog extends DialogFragment
+      implements DialogInterface.OnClickListener {
+
+    private ImageUriProvider mImageUriProvider;
+    private EditText mEditText;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+      mImageUriProvider = ImageUriProvider.getInstance(getContext());
+      mEditText = new EditText(getContext());
+
+      final String previousUri = mImageUriProvider.getUriOverride();
+      if (previousUri != null && !previousUri.isEmpty()) {
+        mEditText.setText(mImageUriProvider.getUriOverride());
+      }
+      mEditText.setHint(R.string.preference_uri_override_dialog_edittext_hint);
+
+      final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+      builder.setMessage(R.string.preference_uri_override_dialog_message)
+          .setView(mEditText)
+          .setPositiveButton(R.string.preference_uri_override_dialog_button_set, this)
+          .setNeutralButton(R.string.preference_uri_override_dialog_button_cancel, null)
+          .setNegativeButton(R.string.preference_uri_override_dialog_button_remove, this);
+      return builder.create();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+      switch (which) {
+        case DialogInterface.BUTTON_POSITIVE:
+          try {
+            mImageUriProvider.setUriOverride(mEditText.getText().toString());
+          } catch (IllegalArgumentException e) {
+            Toast.makeText(
+                getContext(),
+                getString(
+                    R.string.preference_uri_override_dialog_edittext_error_bad_uri,
+                    e.getMessage()),
+                Toast.LENGTH_SHORT).show();
+          }
+          break;
+        case DialogInterface.BUTTON_NEGATIVE:
+          mImageUriProvider.setUriOverride(null);
+          break;
+      }
+    }
   }
 
   /**
