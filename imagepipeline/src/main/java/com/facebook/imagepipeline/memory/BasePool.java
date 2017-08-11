8fd95b0ba6742c73f7849a02e24e38de23410ae2
Eric Hwang <ehwang@fb.com>
2017-08-10 17:04:42 -0700

    Prevent bucket creation when releasing values (fatal exceptions)
    Reviewed By: lambdapioneer
    
    Differential Revision: D5606687
    
    fbshipit-source-id: 88e487bb22b8db9aad1e1531c3a02d1151624e96

--- imagepipeline/src/main/java/com/facebook/imagepipeline/memory/BasePool.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/memory/BasePool.java
@@ -308,7 +308,7 @@ public abstract class BasePool<V> implements Pool<V> {
     final int bucketedSize = getBucketedSizeForValue(value);
     final int sizeInBytes = getSizeInBytes(bucketedSize);
     synchronized (this) {
-      final Bucket<V> bucket = getBucket(bucketedSize);
+      final Bucket<V> bucket = getBucketIfPresent(bucketedSize);
       if (!mInUseValues.remove(value)) {
         // This value was not 'known' to the pool (i.e.) allocated via the pool.
         // Something is going wrong, so let's free the value and report soft error.
@@ -595,6 +595,15 @@ public abstract class BasePool<V> implements Pool<V> {
     }
   }
 
+  /**
+   * Gets the freelist for the specified bucket if it exists.
+   *
+   * @param bucketedSize the bucket size
+   * @return the freelist for the bucket
+   */
+  private synchronized Bucket<V> getBucketIfPresent(int bucketedSize) {
+    return mBuckets.get(bucketedSize);
+  }
 
   /**
    * Gets the freelist for the specified bucket. Create the freelist if there isn't one
