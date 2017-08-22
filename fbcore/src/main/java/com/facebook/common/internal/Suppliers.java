a3192a6e4f9023ab0e7268b15e4bcf9bcad0b0f9
Alexander Oprisnik <oprisnik@fb.com>
2017-08-22 09:56:14 -0700

    Added default boolean suppliers
    Summary: This adds boolean suppliers that can be used for default values.
    
    Reviewed By: erikandre
    
    Differential Revision: D5671342
    
    fbshipit-source-id: a4ba9f9a92d482f31e6f0b3c11892a2f0e0e5386

--- fbcore/src/main/java/com/facebook/common/internal/Suppliers.java
+++ fbcore/src/main/java/com/facebook/common/internal/Suppliers.java
@@ -9,9 +9,7 @@
 
 package com.facebook.common.internal;
 
-/**
- * Wrapper for creating a Supplier.
- */
+/** Wrapper for creating a Supplier and default Suppliers for convenience. */
 public class Suppliers {
   /**
    * Returns a Supplier which always returns {@code instance}.
@@ -26,4 +24,22 @@ public class Suppliers {
       }
     };
   }
+
+  /** Boolean supplier that always returns true. */
+  public static final Supplier<Boolean> BOOLEAN_TRUE =
+      new Supplier<Boolean>() {
+        @Override
+        public Boolean get() {
+          return true;
+        }
+      };
+
+  /** Boolean supplier that always returns false. */
+  public static final Supplier<Boolean> BOOLEAN_FALSE =
+      new Supplier<Boolean>() {
+        @Override
+        public Boolean get() {
+          return false;
+        }
+      };
 }
