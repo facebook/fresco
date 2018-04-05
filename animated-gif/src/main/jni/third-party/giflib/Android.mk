5ff502cb8235403e288bddbce4cd5cf3160ac76d
Alexander Oprisnik <oprisnik@fb.com>
2018-04-05 11:18:01 -0700

    Update GifLib to 5.1.4
    Summary:
    This updates GifLib from 5.1.1 to 5.1.4.
    Launchpad publishes newer versions as `.bz2`, so I updated the unpack logic to handle that case as well.
    
    Reviewed By: lambdapioneer
    
    Differential Revision: D7513936
    
    fbshipit-source-id: 00857eb0854146c3d953e7fb1a39b4de3795b641

--- animated-gif/src/main/jni/third-party/giflib/Android.mk
+++ animated-gif/src/main/jni/third-party/giflib/Android.mk
@@ -5,6 +5,7 @@ LOCAL_CFLAGS    := -DHAVE_CONFIG_H
 LOCAL_MODULE    := gif
 LOCAL_SRC_FILES := \
 	dgif_lib.c \
-	gifalloc.c 
+	gifalloc.c \
+	openbsd-reallocarray.c
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
 include $(BUILD_STATIC_LIBRARY)
