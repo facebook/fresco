9c044c732b45831ae53f5229d5461ae8287add7b
Alexander Oprisnik <oprisnik@fb.com>
2018-06-13 10:56:45 -0700

    Added data source supplier creation with external request listener
    Summary: This allows to create a data source supplier with an additional request listener.
    
    Reviewed By: foghina
    
    Differential Revision: D8395100
    
    fbshipit-source-id: cf5afd4f0673eb925467cdf0f8e61b522b96c952

--- imagepipeline/src/main/java/com/facebook/imagepipeline/core/ImagePipeline.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/core/ImagePipeline.java
@@ -131,6 +131,34 @@ public class ImagePipeline {
    * DataSource representing the pending results of the task.
    *
    * @param imageRequest the request to submit (what to execute).
+   * @param callerContext the caller context of the caller of data source supplier
+   * @param requestLevel which level to look down until for the image
+   * @param requestListener additional image request listener independent of ImageRequest listeners
+   * @return a DataSource representing pending results and completion of the request
+   */
+  public Supplier<DataSource<CloseableReference<CloseableImage>>> getDataSourceSupplier(
+      final ImageRequest imageRequest,
+      final Object callerContext,
+      final ImageRequest.RequestLevel requestLevel,
+      final @Nullable RequestListener requestListener) {
+    return new Supplier<DataSource<CloseableReference<CloseableImage>>>() {
+      @Override
+      public DataSource<CloseableReference<CloseableImage>> get() {
+        return fetchDecodedImage(imageRequest, callerContext, requestLevel, requestListener);
+      }
+
+      @Override
+      public String toString() {
+        return Objects.toStringHelper(this).add("uri", imageRequest.getSourceUri()).toString();
+      }
+    };
+  }
+
+  /**
+   * Returns a DataSource supplier that will on get submit the request for execution and return a
+   * DataSource representing the pending results of the task.
+   *
+   * @param imageRequest the request to submit (what to execute).
    * @return a DataSource representing pending results and completion of the request
    */
   public Supplier<DataSource<CloseableReference<PooledByteBuffer>>>
