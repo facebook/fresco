c2768da5207505a570a283625195e4ee669b5edf
Alexander Oprisnik <oprisnik@fb.com>
2018-06-25 09:27:17 -0700

    Added ImagePerfImageOriginListenerTest
    Summary: Added tests for `ImagePerfImageOriginListener`.
    
    Reviewed By: erikandre
    
    Differential Revision: D8614982
    
    fbshipit-source-id: e6437d335d765eea127bf98aa64d3f5bd465558e

---
+++ drawee-backends/drawee-pipeline/src/test/java/com/facebook/drawee/backends/pipeline/info/internal/ImagePerfImageOriginListenerTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.drawee.backends.pipeline.info.internal;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import com.facebook.drawee.backends.pipeline.info.ImageLoadStatus;
+import com.facebook.drawee.backends.pipeline.info.ImageOrigin;
+import com.facebook.drawee.backends.pipeline.info.ImagePerfMonitor;
+import com.facebook.drawee.backends.pipeline.info.ImagePerfState;
+import org.junit.Before;
+import org.junit.Test;
+
+/** Tests {@link ImagePerfImageOriginListener} */
+public class ImagePerfImageOriginListenerTest {
+
+  private static final String CONTROLLER_ID = "abc";
+
+  private ImagePerfMonitor mImagePerfMonitor;
+  private ImagePerfState mImagePerfState;
+
+  private ImagePerfImageOriginListener mListener;
+
+  @Before
+  public void setUp() {
+    mImagePerfMonitor = mock(ImagePerfMonitor.class);
+    mImagePerfState = mock(ImagePerfState.class);
+    mListener = new ImagePerfImageOriginListener(mImagePerfState, mImagePerfMonitor);
+  }
+
+  @Test
+  public void testOnImageLoaded() {
+
+    mListener.onImageLoaded(CONTROLLER_ID, ImageOrigin.NETWORK, true);
+
+    verify(mImagePerfState).setImageOrigin(eq(ImageOrigin.NETWORK));
+    verify(mImagePerfState).setSuccessful(eq(true));
+    verify(mImagePerfMonitor)
+        .notifyListeners(eq(mImagePerfState), eq(ImageLoadStatus.ORIGIN_AVAILABLE));
+    verifyNoMoreInteractions(mImagePerfMonitor);
+  }
+}
