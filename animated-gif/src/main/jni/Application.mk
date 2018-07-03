facebook/fresco

commit 1b86b49f60861b56d7bd89550e16f7f8e5b69383

Fix NDK settings (#2134)
Summary:
Thanks for submitting a PR! Please read these instructions carefully:

- [x] Explain the **motivation** for making this change.
- [x] Provide a **test plan** demonstrating that the code is solid.
- [x] Match the **code formatting** of the rest of the codebase.
- [x] Target the `master` branch

What existing problem does the pull request solve?

Fix build failed occurred by ndk settings ([Application.mk](https://developer.android.com/ndk/guides/application_mk))
Remove armeabi support (Remove from NDK r17)

```
> Task :imagepipeline:ndk_build_bitmaps FAILED
A problem was found with the configuration of task ':imagepipeline:ndk_build_bitmaps'. Registering invalid inputs and outputs via TaskInputs and TaskOutputs methods has been deprecated and is scheduled to be removed in Gradle 5.0.
 - File '.../fresco/imagepipeline/src/main/jni/bitmaps' specified for property '$1' is not a file.
Android NDK: APP_PLATFORM not set. Defaulting to minimum supported version android-14.
Android NDK: WARNING: APP_STL gnustl_static is deprecated and will be removed in the next release. Please switch to either c++_static or c++_shared. See https://developer.android.com/ndk/guides/cpp-support.html for more information.
Android NDK: The armeabi ABI is no longer supported. Use armeabi-v7a.
.../Android/sdk/ndk-bundle/build/core/setup-app.mk:79: *** Android NDK: Aborting    Android NDK: NDK Application 'local' targets unknown ABI(s): armeabi
.  Stop.
Android NDK: Please fix the APP_ABI definition in ../Application.mk
make: Entering directory `.../fresco/imagepipeline/src/main/jni/bitmaps'
make: Leaving directory `.../fresco/imagepipeline/src/main/jni/bitmaps'
```

Relative issue: #24 #163 #286 #513 #591 #618 #938 #989 #1114 #1205 #1226 #1427 #1945

A good test plan has the exact commands you ran and their output, provides screenshots or videos if the pull request changes UI or updates the website. See [What is a Test Plan?][1] to learn more.

animated-gif/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := armeabi-v7a armeabi arm64-v8a x86 x86_64
+APP_ABI := all
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../nativedeps/merge

imagepipeline/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := armeabi-v7a armeabi arm64-v8a x86 x86_64
+APP_ABI := all
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../nativedeps/merge

static-webp/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := armeabi-v7a armeabi arm64-v8a x86 x86_64
+APP_ABI := all
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../nativedeps/merge

circle.yml
@@ -24,9 +24,9 @@ jobs:
       - run:
           name: Setup NDK
           command: |
-            wget --quiet https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip
-            unzip -q android-ndk-r16b-linux-x86_64.zip -d /home/circleci
-            echo "ndk.dir=/home/circleci/android-ndk-r16b" >> /home/circleci/fresco/local.properties
+            wget --quiet https://dl.google.com/android/repository/android-ndk-r17b-linux-x86_64.zip
+            unzip -q android-ndk-r17b-linux-x86_64.zip -d /home/circleci
+            echo "ndk.dir=/home/circleci/android-ndk-r17b" >> /home/circleci/fresco/local.properties
       - run:
           name: "NDK Fix: https://github.com/android-ndk/ndk/issues/188"
           command: sudo apt-get install file

gradle.properties
@@ -24,8 +24,8 @@ GROUP=com.facebook.fresco
 
 # Deps for gradle
 BUILD_TOOLS_VERSION=27.0.3
-COMPILE_SDK_VERSION=26
-ANDROID_GRADLE_PLUGIN_VERSION=3.1.2
+COMPILE_SDK_VERSION=27
+ANDROID_GRADLE_PLUGIN_VERSION=3.1.3
 
 # Deps for libraries
 ANNOTATION_API_VERSION=1.2
@@ -41,7 +41,7 @@ NINEOLDANDROID_VERSION=2.4.0
 POWERMOCK_VERSION=1.6.6
 ROBOLECTRIC_VERSION=3.0
 SOLOADER_VERSION=0.5.0
-SUPPORT_LIB_VERSION=27.1.0
+SUPPORT_LIB_VERSION=27.1.1
 VOLLEY_VERSION=1.0.0
 
 # Deps for native libraries
@@ -54,7 +54,7 @@ LIBWEBP_VERSION=0.6.1
 # SDK versions for the samples
 MIN_SDK_VERSION=9
 SAMPLES_MIN_SDK_VERSION=14
-TARGET_SDK_VERSION=26
+TARGET_SDK_VERSION=27
 
 # Deps for comparison app
 GLIDE_VERSION=3.8.0

run_comparison.py
@@ -15,8 +15,8 @@ This script builds and runs the comparison app, switching from one library to th
 taking measurements as it goes, and outputs the results neatly.
 
 Due to a bug, you must specify the CPU when running the script.
-Use -c armeabi-v7a for most phones. Use -c armeabi for ARM v5-6 phones, or
--c arm64 for 64-bit ARM devices. Some emulators and tablets will need -c x86.
+Use -c armeabi-v7a for most phones. Use -c arm64 for 64-bit ARM devices. 
+Some emulators and tablets will need -c x86.
 
 To select a subset of the libraries, use the -s option with a
 space-separated list. Available options are fresco, fresco-okhttp,
@@ -67,7 +67,6 @@ TEST_SOURCES = (
 
 ABIS = (
     'arm64-v8a',
-    'armeabi',
     'armeabi-v7a',
     'x86',
     'x86_64'

samples/animation2/build.gradle
@@ -42,7 +42,7 @@ android {
         abi {
             enable true
             reset()
-            include 'x86', 'x86_64', 'arm64-v8a', 'armeabi-v7a', 'armeabi'
+            include 'x86', 'x86_64', 'arm64-v8a', 'armeabi-v7a'
             universalApk false
         }
     }

samples/comparison/build.gradle
@@ -54,7 +54,7 @@ android {
         abi {
             enable true
             reset()
-            include 'x86', 'x86_64', 'arm64-v8a', 'armeabi-v7a', 'armeabi'
+            include 'x86', 'x86_64', 'arm64-v8a', 'armeabi-v7a'
             universalApk false
         }
     }

commit f480722e2eb59b92e08112933ba9861f00298e15

Fix NDK setting for ndk ver < 17
Summary: Changes in github 1b86b49f60861b56d7bd89550e16f7f8e5b69383  introduced the following error for ndk < 17: `fresco/static-webp/src/main/jni/static-webp/../../../../nativedeps/merge/libwebp-0.6.1/src/dsp/yuv.c:104: undefined reference to 'WebPInitSamplersMIPS32'`

Reviewed By: oprisnik

Differential Revision: D8723530

fbshipit-source-id: e9167b7e5221493ddb09546a0482f79e3b75c2dc

animated-gif/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := all
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../nativedeps/merge

imagepipeline/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := all
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../nativedeps/merge

static-webp/src/main/jni/Application.mk
@@ -1,7 +1,7 @@
 # Copyright 2004-present Facebook. All Rights Reserved.
 APP_BUILD_SCRIPT := Android.mk
 
-APP_ABI := all
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 
 APP_MK_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 NDK_MODULE_PATH := $(APP_MK_DIR)$(HOST_DIRSEP)$(APP_MK_DIR)../../../nativedeps/merge