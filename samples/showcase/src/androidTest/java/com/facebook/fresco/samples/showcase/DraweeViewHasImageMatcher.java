5e5875e98910aa514dde7172e4771c972c34f64a
Zuzanna Mroczek <zuza@fb.com>
2018-07-13 07:53:19 -0700

    Add DraweeViewHasImageMatcher class for E2E tests
    Reviewed By: oprisnik
    
    Differential Revision: D8834535
    
    fbshipit-source-id: c66a0f887b949242b6758cf8dbaa1578e9010709

---
+++ samples/showcase/src/androidTest/java/com/facebook/fresco/samples/showcase/DraweeViewHasImageMatcher.java
@@ -0,0 +1,34 @@
+/*
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.facebook.fresco.samples.showcase;
+
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.view.View;
+import com.facebook.drawee.view.SimpleDraweeView;
+import org.hamcrest.Description;
+
+class DraweeViewHasImageMatcher {
+
+  static BoundedMatcher<View, SimpleDraweeView> hasImage() {
+    return new BoundedMatcher<View, SimpleDraweeView>(SimpleDraweeView.class) {
+      @Override
+      public void describeTo(Description description) {
+        description.appendText("has image");
+      }
+
+      @Override
+      public boolean matchesSafely(SimpleDraweeView draweeView) {
+        return draweeView.getHierarchy().hasImage();
+      }
+    };
+  }
+}
