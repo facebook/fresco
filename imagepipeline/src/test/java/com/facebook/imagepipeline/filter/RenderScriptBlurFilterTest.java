ca6886e66254f17c9f360e45888961a9d2b8ce7d
Mikael Morales Gonzalez <moralesmikael@fb.com>
2018-08-28 04:58:02 -0700

    Java implementation of blurring algorithms
    Reviewed By: lambdapioneer
    
    Differential Revision: D9420597
    
    fbshipit-source-id: d9f2b9728c42bdbb94a203270bad24b2e659d181

---
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/filter/RenderScriptBlurFilterTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.facebook.imagepipeline.filter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class RenderScriptBlurFilterTest {
+  @Mock private Bitmap srcBitmap;
+  @Mock private Bitmap destBitmap;
+  @Mock private Context mContext;
+
+  @Before
+  public void setup() {
+    MockitoAnnotations.initMocks(this);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void invalidNegativeRadiusBlur() {
+    RenderScriptBlurFilter.blurBitmap(destBitmap, srcBitmap, mContext, -1);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void invalidZeroRadiusBlur() {
+    RenderScriptBlurFilter.blurBitmap(destBitmap, srcBitmap, mContext, 0);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void invalidBigRadiusBlur() {
+    RenderScriptBlurFilter.blurBitmap(
+        destBitmap, srcBitmap, mContext, RenderScriptBlurFilter.BLUR_MAX_RADIUS + 1);
+  }
+}
