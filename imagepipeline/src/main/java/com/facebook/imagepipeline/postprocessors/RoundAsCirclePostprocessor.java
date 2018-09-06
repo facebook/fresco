fd9584e6f2043d51f3d8feeae8ce6044056c028e
Mikael Morales Gonzalez <moralesmikael@fb.com>
2018-09-06 09:09:38 -0700

    Update existing native rounding post processor
    Reviewed By: oprisnik
    
    Differential Revision: D9607950
    
    fbshipit-source-id: 080aa9cd339761a48e534c2fd4d3677005063e78

--- imagepipeline/src/main/java/com/facebook/imagepipeline/postprocessors/RoundAsCirclePostprocessor.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/postprocessors/RoundAsCirclePostprocessor.java
@@ -15,19 +15,33 @@ import javax.annotation.Nullable;
 
 /** Postprocessor that rounds a given image as a circle. */
 public class RoundAsCirclePostprocessor extends BasePostprocessor {
+  private static final boolean ENABLE_ANTI_ALIASING = true;
 
   private @Nullable CacheKey mCacheKey;
+  private final boolean mEnableAntiAliasing;
+
+  public RoundAsCirclePostprocessor() {
+    this(ENABLE_ANTI_ALIASING);
+  }
+
+  public RoundAsCirclePostprocessor(boolean enableAntiAliasing) {
+    mEnableAntiAliasing = enableAntiAliasing;
+  }
 
   @Override
   public void process(Bitmap bitmap) {
-    NativeRoundingFilter.toCircle(bitmap);
+    NativeRoundingFilter.toCircle(bitmap, mEnableAntiAliasing);
   }
 
   @Nullable
   @Override
   public CacheKey getPostprocessorCacheKey() {
     if (mCacheKey == null) {
-      mCacheKey = new SimpleCacheKey("RoundAsCirclePostprocessor");
+      if (mEnableAntiAliasing) {
+        mCacheKey = new SimpleCacheKey("RoundAsCirclePostprocessor#AntiAliased");
+      } else {
+        mCacheKey = new SimpleCacheKey("RoundAsCirclePostprocessor");
+      }
     }
     return mCacheKey;
   }
