5d419bcc450bd0e4fbc27546e94ac55ad20a3e60
Héctor Ramos <hramos@fb.com>
2018-09-11 15:27:47 -0700

    Update copyright headers to yearless format
    Reviewed By: yungsters
    
    Differential Revision: D9727774
    
    fbshipit-source-id: df4fc1e4390733fe774b1a160dd41b4a3d83302a

--- imagepipeline/src/test/java/com/facebook/imagepipeline/memory/BucketMapTest.java
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/memory/BucketMapTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  * All rights reserved.
  *
  * This source code is licensed under the BSD-style license found in the
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 
 /*
- * Copyright (c) 2015-present, Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
