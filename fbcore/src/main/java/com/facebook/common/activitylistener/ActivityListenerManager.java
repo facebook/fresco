7abfc021b3bc45c38fb35dc6101cf8d53352b384
Karan Bhatia <bhatia@fb.com>
2018-09-24 12:44:39 -0700

    Break apart register method to allow access to found ListenableActivity
    Reviewed By: denley
    
    Differential Revision: D9952301
    
    fbshipit-source-id: 98aafe4d1b62de39acf127a7b86dbb15fa445418

--- fbcore/src/main/java/com/facebook/common/activitylistener/ActivityListenerManager.java
+++ fbcore/src/main/java/com/facebook/common/activitylistener/ActivityListenerManager.java
@@ -12,11 +12,12 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import com.facebook.common.internal.Preconditions;
 import java.lang.ref.WeakReference;
+import javax.annotation.Nullable;
 
 /**
  * Registers ActivityListener with ActivityListener.
  *
- * <p> A WeakReference is used to wrap an ActivityVisibilityListener. When it is nullified
+ * <p>A WeakReference is used to wrap an ActivityVisibilityListener. When it is nullified
  * ActivityListener is automatically removed from the listened ListenableActivity.
  */
 public class ActivityListenerManager {
@@ -25,17 +26,22 @@ public class ActivityListenerManager {
    * If given context is an instance of ListenableActivity then creates new instance of
    * WeakReferenceActivityListenerAdapter and adds it to activity's listeners
    */
-  public static void register(
-      ActivityListener activityListener,
-      Context context) {
+  public static void register(ActivityListener activityListener, Context context) {
+    ListenableActivity activity = getListenableActivity(context);
+    if (activity != null) {
+      Listener listener = new Listener(activityListener);
+      activity.addActivityListener(listener);
+    }
+  }
+
+  public static @Nullable ListenableActivity getListenableActivity(Context context) {
     if (!(context instanceof ListenableActivity) && context instanceof ContextWrapper) {
       context = ((ContextWrapper) context).getBaseContext();
     }
     if (context instanceof ListenableActivity) {
-      ListenableActivity listenableActivity = (ListenableActivity) context;
-      Listener listener = new Listener(activityListener);
-      listenableActivity.addActivityListener(listener);
+      return (ListenableActivity) context;
     }
+    return null;
   }
 
   private static class Listener extends BaseActivityListener {
