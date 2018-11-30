440720690b7d072281cc8e60612f83540bda2e22
Alexander Oprisnik <oprisnik@fb.com>
2018-11-30 08:27:00 -0800

    Add @Nullable annotation for cache key generation helper method
    Summary: The method already checks if the `ImageRequest` is null.
    
    Reviewed By: erikandre
    
    Differential Revision: D13237000
    
    fbshipit-source-id: 289463e83812dd056329234805ef0120af0c4f5b

--- imagepipeline/src/main/java/com/facebook/imagepipeline/core/ImagePipeline.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/core/ImagePipeline.java
@@ -625,11 +625,9 @@ public class ImagePipeline {
             });
     return dataSource;
   }
-/**
- * @return {@link CacheKey} for doing bitmap cache lookups in the pipeline.
- */
+  /** @return {@link CacheKey} for doing bitmap cache lookups in the pipeline. */
   @Nullable
-  public CacheKey getCacheKey(ImageRequest imageRequest, Object callerContext) {
+  public CacheKey getCacheKey(@Nullable ImageRequest imageRequest, Object callerContext) {
     if (FrescoSystrace.isTracing()) {
       FrescoSystrace.beginSection("ImagePipeline#getCacheKey");
     }
