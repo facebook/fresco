06287e70c3dd2bb0235d56d67b59599a05ac2590
Carmi Grushko <carmi@fb.com>
2019-04-03 13:24:06 -0700

    Display Image URI in Fresco's debug overlay
    Reviewed By: oprisnik
    
    Differential Revision: D14727025
    
    fbshipit-source-id: 8902da561e92fa3f9202d23d838e35856f7f3f70

--- drawee-backends/drawee-pipeline/src/main/java/com/facebook/drawee/backends/pipeline/PipelineDraweeController.java
+++ drawee-backends/drawee-pipeline/src/main/java/com/facebook/drawee/backends/pipeline/PipelineDraweeController.java
@@ -330,25 +330,32 @@ public class PipelineDraweeController
     }
 
     if (getControllerOverlay() instanceof DebugControllerOverlayDrawable) {
-      DebugControllerOverlayDrawable debugOverlay =
-          (DebugControllerOverlayDrawable) getControllerOverlay();
-      debugOverlay.setControllerId(getId());
+      updateDebugOverlay(image, (DebugControllerOverlayDrawable) getControllerOverlay());
+    }
+  }
 
-      final DraweeHierarchy draweeHierarchy = getHierarchy();
-      ScaleType scaleType = null;
-      if (draweeHierarchy != null) {
-        final ScaleTypeDrawable scaleTypeDrawable =
-            ScalingUtils.getActiveScaleTypeDrawable(draweeHierarchy.getTopLevelDrawable());
-        scaleType = scaleTypeDrawable != null ? scaleTypeDrawable.getScaleType() : null;
-      }
-      debugOverlay.setScaleType(scaleType);
-      debugOverlay.setOrigin(mDebugOverlayImageOriginListener.getImageOrigin());
-      if (image != null) {
-        debugOverlay.setDimensions(image.getWidth(), image.getHeight());
-        debugOverlay.setImageSize(image.getSizeInBytes());
-      } else {
-        debugOverlay.reset();
-      }
+  /**
+   * updateDebugOverlay updates the debug overlay. Subclasses of {@link PipelineDraweeController}
+   * can override this method (and call <code>super</code>) to provide additional debug information.
+   */
+  protected void updateDebugOverlay(
+      @Nullable CloseableImage image, DebugControllerOverlayDrawable debugOverlay) {
+    debugOverlay.setControllerId(getId());
+
+    final DraweeHierarchy draweeHierarchy = getHierarchy();
+    ScaleType scaleType = null;
+    if (draweeHierarchy != null) {
+      final ScaleTypeDrawable scaleTypeDrawable =
+          ScalingUtils.getActiveScaleTypeDrawable(draweeHierarchy.getTopLevelDrawable());
+      scaleType = scaleTypeDrawable != null ? scaleTypeDrawable.getScaleType() : null;
+    }
+    debugOverlay.setScaleType(scaleType);
+    debugOverlay.setOrigin(mDebugOverlayImageOriginListener.getImageOrigin());
+    if (image != null) {
+      debugOverlay.setDimensions(image.getWidth(), image.getHeight());
+      debugOverlay.setImageSize(image.getSizeInBytes());
+    } else {
+      debugOverlay.reset();
     }
   }
 
