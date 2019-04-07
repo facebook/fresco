470c0c0b9be90b009f7a5ec3f8c7086246c395db
Carmi Grushko <carmi@fb.com>
2019-04-07 09:50:14 -0700

    Instrument `HttpUrlConnectionNetworkFetcher`: request queue, fetch and total time
    Reviewed By: yanivsb
    
    Differential Revision: D14821660
    
    fbshipit-source-id: 14bbeec5be2a86c2ac3dc4c855ebc6d04f25939e

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/HttpUrlConnectionNetworkFetcher.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/HttpUrlConnectionNetworkFetcher.java
@@ -9,13 +9,17 @@ package com.facebook.imagepipeline.producers;
 
 import android.net.Uri;
 import com.facebook.common.internal.VisibleForTesting;
+import com.facebook.common.time.MonotonicClock;
+import com.facebook.common.time.RealtimeSinceBootClock;
 import com.facebook.common.util.UriUtil;
 import com.facebook.imagepipeline.image.EncodedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -23,10 +27,28 @@ import java.util.concurrent.Future;
 /**
  * Network fetcher that uses the simplest Android stack.
  *
- * <p> Apps requiring more sophisticated networking should implement their own
- * {@link NetworkFetcher}.
+ * <p>Apps requiring more sophisticated networking should implement their own {@link
+ * NetworkFetcher}.
  */
-public class HttpUrlConnectionNetworkFetcher extends BaseNetworkFetcher<FetchState> {
+public class HttpUrlConnectionNetworkFetcher
+    extends BaseNetworkFetcher<HttpUrlConnectionNetworkFetcher.HttpUrlConnectionNetworkFetchState> {
+
+  public static class HttpUrlConnectionNetworkFetchState extends FetchState {
+
+    private long submitTime;
+    private long responseTime;
+    private long fetchCompleteTime;
+
+    public HttpUrlConnectionNetworkFetchState(
+        Consumer<EncodedImage> consumer, ProducerContext producerContext) {
+      super(consumer, producerContext);
+    }
+  }
+
+  private static final String QUEUE_TIME = "queue_time";
+  private static final String FETCH_TIME = "fetch_time";
+  private static final String TOTAL_TIME = "total_time";
+  private static final String IMAGE_SIZE = "image_size";
 
   private static final int NUM_NETWORK_THREADS = 3;
   private static final int MAX_REDIRECTS = 5;
@@ -39,28 +61,32 @@ public class HttpUrlConnectionNetworkFetcher extends BaseNetworkFetcher<FetchSta
   private int mHttpConnectionTimeout;
 
   private final ExecutorService mExecutorService;
+  private final MonotonicClock mMonotonicClock;
 
   public HttpUrlConnectionNetworkFetcher() {
-    this(Executors.newFixedThreadPool(NUM_NETWORK_THREADS));
+    this(RealtimeSinceBootClock.get());
   }
 
   public HttpUrlConnectionNetworkFetcher(int httpConnectionTimeout) {
-    this(Executors.newFixedThreadPool(NUM_NETWORK_THREADS));
+    this(RealtimeSinceBootClock.get());
     mHttpConnectionTimeout = httpConnectionTimeout;
   }
 
   @VisibleForTesting
-  HttpUrlConnectionNetworkFetcher(ExecutorService executorService) {
-    mExecutorService = executorService;
+  HttpUrlConnectionNetworkFetcher(MonotonicClock monotonicClock) {
+    mExecutorService = Executors.newFixedThreadPool(NUM_NETWORK_THREADS);
+    mMonotonicClock = monotonicClock;
   }
 
   @Override
-  public FetchState createFetchState(Consumer<EncodedImage> consumer, ProducerContext context) {
-    return new FetchState(consumer, context);
+  public HttpUrlConnectionNetworkFetchState createFetchState(
+      Consumer<EncodedImage> consumer, ProducerContext context) {
+    return new HttpUrlConnectionNetworkFetchState(consumer, context);
   }
 
   @Override
-  public void fetch(final FetchState fetchState, final Callback callback) {
+  public void fetch(final HttpUrlConnectionNetworkFetchState fetchState, final Callback callback) {
+    fetchState.submitTime = mMonotonicClock.now();
     final Future<?> future = mExecutorService.submit(
         new Runnable() {
           @Override
@@ -80,11 +106,12 @@ public class HttpUrlConnectionNetworkFetcher extends BaseNetworkFetcher<FetchSta
   }
 
   @VisibleForTesting
-  void fetchSync(FetchState fetchState, Callback callback) {
+  void fetchSync(HttpUrlConnectionNetworkFetchState fetchState, Callback callback) {
     HttpURLConnection connection = null;
     InputStream is = null;
     try {
       connection = downloadFrom(fetchState.getUri(), MAX_REDIRECTS);
+      fetchState.responseTime = mMonotonicClock.now();
 
       if (connection != null) {
         is = connection.getInputStream();
@@ -144,6 +171,11 @@ public class HttpUrlConnectionNetworkFetcher extends BaseNetworkFetcher<FetchSta
     return (HttpURLConnection) url.openConnection();
   }
 
+  @Override
+  public void onFetchCompletion(HttpUrlConnectionNetworkFetchState fetchState, int byteSize) {
+    fetchState.fetchCompleteTime = mMonotonicClock.now();
+  }
+
   private static boolean isHttpSuccess(int responseCode) {
     return (responseCode >= HttpURLConnection.HTTP_OK &&
         responseCode < HttpURLConnection.HTTP_MULT_CHOICE);
@@ -167,4 +199,14 @@ public class HttpUrlConnectionNetworkFetcher extends BaseNetworkFetcher<FetchSta
     return String.format(Locale.getDefault(), format, args);
   }
 
+  @Override
+  public Map<String, String> getExtraMap(
+      HttpUrlConnectionNetworkFetchState fetchState, int byteSize) {
+    Map<String, String> extraMap = new HashMap<>(4);
+    extraMap.put(QUEUE_TIME, Long.toString(fetchState.responseTime - fetchState.submitTime));
+    extraMap.put(FETCH_TIME, Long.toString(fetchState.fetchCompleteTime - fetchState.responseTime));
+    extraMap.put(TOTAL_TIME, Long.toString(fetchState.fetchCompleteTime - fetchState.submitTime));
+    extraMap.put(IMAGE_SIZE, Integer.toString(byteSize));
+    return extraMap;
+  }
 }
