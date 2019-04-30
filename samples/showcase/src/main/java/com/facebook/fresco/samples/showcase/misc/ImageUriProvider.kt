0b85879d51c5036d5e46e627a6651afefc0b971a
Alexander Oprisnik <oprisnik@fb.com>
2019-04-30 09:33:23 -0700

    Showcase: Improve ImageUriProvider
    Summary: This cleans up ImageUriProvider, which has been converted to Kotlin with AS.
    
    Reviewed By: lambdapioneer
    
    Differential Revision: D15065901
    
    fbshipit-source-id: e8a7347bd06b8b07dcfc9fd7b04c81e24ada0127

--- samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/misc/ImageUriProvider.kt
+++ samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/misc/ImageUriProvider.kt
@@ -15,7 +15,6 @@ package com.facebook.fresco.samples.showcase.misc
 import android.content.Context
 import android.content.SharedPreferences
 import android.net.Uri
-import android.text.TextUtils
 import androidx.preference.PreferenceManager
 import com.facebook.common.internal.Preconditions
 import java.util.*
@@ -25,40 +24,27 @@ import java.util.*
  */
 class ImageUriProvider constructor(context: Context) {
 
-    private val mSharedPreferences: SharedPreferences
-    private val mRandom = Random()
+    private val sharedPreferences: SharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(context)
 
     var uriOverride: String?
-        get() {
-            val uriOverride = mSharedPreferences.getString(PREF_KEY_URI_OVERRIDE, null)
-            return if (!TextUtils.isEmpty(uriOverride))
-                uriOverride
-            else
-                null
-        }
-        set(uri) = if (uri == null || uri.length == 0) {
-            mSharedPreferences.edit()
+        get() = sharedPreferences.getString(PREF_KEY_URI_OVERRIDE, null)
+        set(uri) = if (uri == null || uri.isEmpty()) {
+            sharedPreferences.edit()
                     .remove(PREF_KEY_URI_OVERRIDE)
                     .apply()
         } else {
             Preconditions.checkArgument(Uri.parse(uri).isAbsolute, "URI must be absolute")
 
-            mSharedPreferences.edit()
+            sharedPreferences.edit()
                     .putString(PREF_KEY_URI_OVERRIDE, uri)
                     .apply()
         }
 
-    val sampleGifUris: List<Uri>
-        get() {
-            val uris = ArrayList<Uri>()
-            for (uri in SAMPLE_URIS_GIFS) {
-                uris.add(Uri.parse(uri))
-            }
-            return uris
-        }
+    val sampleGifUris = SAMPLE_URIS_GIFS.asSequence().map { Uri.parse(it) }.toList()
 
     private val isShouldBreakCacheByDefault: Boolean
-        get() = mSharedPreferences.getBoolean(PREF_KEY_CACHE_BREAKING_BY_DEFAULT, false)
+        get() = sharedPreferences.getBoolean(PREF_KEY_CACHE_BREAKING_BY_DEFAULT, false)
 
     /**
      * The orientation of a sample image
@@ -72,7 +58,12 @@ class ImageUriProvider constructor(context: Context) {
         /**
          * width > height
          */
-        LANDSCAPE
+        LANDSCAPE,
+
+        /**
+         * Any orientation
+         */
+        ANY
     }
 
     /**
@@ -123,104 +114,67 @@ class ImageUriProvider constructor(context: Context) {
         XXL("xxl")
     }
 
-    init {
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-    }
-
     /**
      * Creates an URI of an image that will result in a 404 (not found) HTTP error
      */
-    fun createNonExistingUri(): Uri {
-        return Uri.parse(NON_EXISTING_URI)
-    }
-
-    fun createSampleUri(imageSize: ImageSize, uriModification: UriModification): Uri {
-        return createSampleUri(imageSize, null, uriModification)
-    }
+    val nonExistingUri: Uri by lazy { Uri.parse(NON_EXISTING_URI) }
 
     @JvmOverloads
     fun createSampleUri(
-            imageSize: ImageSize,
-            orientation: Orientation? = null,
+            imageSize: ImageSize = ImageSize.M,
+            orientation: Orientation = Orientation.ANY,
             urlModification: UriModification = UriModification.NONE): Uri {
-        val baseUri: String
-        if (orientation == Orientation.PORTRAIT) {
-            baseUri = chooseRandom(SAMPLE_URIS_PORTRAIT)
-        } else if (orientation == Orientation.LANDSCAPE) {
-            baseUri = chooseRandom(SAMPLE_URIS_LANDSCAPE)
-        } else {
-            baseUri = chooseRandom(SAMPLE_URIS_LANDSCAPE, SAMPLE_URIS_PORTRAIT)
+        val baseUri: String = when (orientation) {
+            Orientation.PORTRAIT -> chooseRandom(*SAMPLE_URIS_PORTRAIT)
+            Orientation.LANDSCAPE -> chooseRandom(*SAMPLE_URIS_LANDSCAPE)
+            Orientation.ANY -> chooseRandom(*SAMPLE_URIS_LANDSCAPE, *SAMPLE_URIS_PORTRAIT)
         }
 
         val fullUri = String.format(baseUri, imageSize.sizeSuffix)
         return applyOverrideSettings(fullUri, urlModification)
     }
 
-    fun createPJPEGSlow(): Uri {
-        return applyOverrideSettings(SAMPLE_URI_PJPEG_SLOW, UriModification.NONE)
-    }
+    fun createPJPEGSlow(): Uri = applyOverrideSettings(SAMPLE_URI_PJPEG_SLOW, UriModification.NONE)
 
     @JvmOverloads
-    fun createPngUri(orientation: Orientation? = null, urlModification: UriModification = UriModification.NONE): Uri {
-        val baseUri: String
-        if (orientation == Orientation.PORTRAIT) {
-            baseUri = chooseRandom(SAMPLE_URIS_PORTRAIT_PNG)
-        } else if (orientation == Orientation.LANDSCAPE) {
-            baseUri = chooseRandom(SAMPLE_URIS_LANDSCAPE_PNG)
-        } else {
-            baseUri = chooseRandom(SAMPLE_URIS_LANDSCAPE_PNG, SAMPLE_URIS_PORTRAIT_PNG)
+    fun createPngUri(
+            orientation: Orientation = Orientation.ANY,
+            urlModification: UriModification = UriModification.NONE): Uri {
+        val baseUri = when (orientation) {
+            Orientation.PORTRAIT -> chooseRandom(*SAMPLE_URIS_PORTRAIT_PNG)
+            Orientation.LANDSCAPE -> chooseRandom(*SAMPLE_URIS_LANDSCAPE_PNG)
+            Orientation.ANY -> chooseRandom(*SAMPLE_URIS_LANDSCAPE_PNG, *SAMPLE_URIS_PORTRAIT_PNG)
         }
         return applyOverrideSettings(baseUri, urlModification)
     }
 
-    fun createWebpStaticUri(): Uri {
-        return applyOverrideSettings(SAMPLE_URI_WEBP_STATIC, UriModification.NONE)
-    }
+    fun createWebpStaticUri(): Uri = applyOverrideSettings(SAMPLE_URI_WEBP_STATIC, UriModification.NONE)
 
-    fun createWebpTranslucentUri(): Uri {
-        return applyOverrideSettings(SAMPLE_URI_WEBP_TRANSLUCENT, UriModification.NONE)
-    }
+    fun createWebpTranslucentUri(): Uri = applyOverrideSettings(SAMPLE_URI_WEBP_TRANSLUCENT, UriModification.NONE)
 
-    fun createWebpAnimatedUri(): Uri {
-        return applyOverrideSettings(SAMPLE_URI_WEBP_ANIMATED, UriModification.NONE)
-    }
+    fun createWebpAnimatedUri(): Uri = applyOverrideSettings(SAMPLE_URI_WEBP_ANIMATED, UriModification.NONE)
 
     fun createGifUri(imageSize: ImageSize): Uri {
         return applyOverrideSettings(
-                String.format((null as Locale?)!!, SAMPLE_URI_GIF_PATTERN, imageSize.sizeSuffix),
+                String.format(SAMPLE_URI_GIF_PATTERN, imageSize.sizeSuffix),
                 UriModification.NONE)
     }
 
     fun createGifUriWithPause(imageSize: ImageSize): Uri {
         return applyOverrideSettings(
-                String.format((null as Locale?)!!, SAMPLE_URI_GIF_WITH_PAUSE_PATTERN, imageSize.sizeSuffix),
+                String.format(SAMPLE_URI_GIF_WITH_PAUSE_PATTERN, imageSize.sizeSuffix),
                 UriModification.NONE)
     }
 
-    fun createKeyframesUri(): Uri {
-        return applyOverrideSettings(SAMPLE_URI_KEYFRAMES, UriModification.NONE)
-    }
+    fun createKeyframesUri() = applyOverrideSettings(SAMPLE_URI_KEYFRAMES, UriModification.NONE)
 
-    fun createSvgUri(): Uri {
-        return applyOverrideSettings(SAMPLE_URI_SVG, UriModification.NONE)
-    }
-
-    fun getSampleUris(imageSize: ImageSize): List<Uri> {
-        val uris = ArrayList<Uri>()
-        for (uri in SAMPLE_URIS_LANDSCAPE) {
-            uris.add(Uri.parse(String.format((null as Locale?)!!, uri, imageSize.sizeSuffix)))
-        }
-        return uris
-    }
+    fun createSvgUri() = applyOverrideSettings(SAMPLE_URI_SVG, UriModification.NONE)
 
     fun getRandomSampleUris(imageSize: ImageSize, numImages: Int): List<Uri> {
-        val uriFormat: String
-        if (imageSize == ImageSize.S) {
-            uriFormat = RANDOM_URI_PATTERN_S
-        } else if (imageSize == ImageSize.M) {
-            uriFormat = RANDOM_URI_PATTERN_M
-        } else {
-            throw IllegalArgumentException(
+        val uriFormat: String = when (imageSize) {
+            ImageSize.S -> RANDOM_URI_PATTERN_S
+            ImageSize.M -> RANDOM_URI_PATTERN_M
+            ImageSize.XS, ImageSize.L, ImageSize.XL, ImageSize.XXL -> throw IllegalArgumentException(
                     "Don't have random sample URIs for image size: $imageSize")
         }
 
@@ -229,7 +183,7 @@ class ImageUriProvider constructor(context: Context) {
 
         for (i in 0 until numImages) {
             val imageId = random.nextInt(RANDOM_URI_MAX_IMAGE_ID)
-            data.add(Uri.parse(String.format((null as Locale?)!!, uriFormat, imageId)))
+            data.add(Uri.parse(String.format(uriFormat, imageId)))
         }
         return data
     }
@@ -258,31 +212,18 @@ class ImageUriProvider constructor(context: Context) {
     }
 
     /**
-     * @return a random element from a given set of arrays (uniform distribution)
+     * @return a random element from a given set of data (uniform distribution)
      */
-    private fun <T> chooseRandom(vararg arrays: Array<T>): T {
-        var l = 0
-        for (array in arrays) {
-            l += array.size
-        }
-        var i = mRandom.nextInt(l)
-        for (array in arrays) {
-            if (i < array.size) {
-                return array[i]
-            }
-            i -= array.size
-        }
-        throw IllegalStateException("unreachable code")
-    }
+    private fun <T> chooseRandom(vararg data: T): T = data.random()
 
     companion object {
 
-        private val PREF_KEY_CACHE_BREAKING_BY_DEFAULT = "uri_cache_breaking"
-        private val PREF_KEY_URI_OVERRIDE = "uri_override"
+        private const val PREF_KEY_CACHE_BREAKING_BY_DEFAULT = "uri_cache_breaking"
+        private const val PREF_KEY_URI_OVERRIDE = "uri_override"
 
-        private val RANDOM_URI_MAX_IMAGE_ID = 1000
-        private val RANDOM_URI_PATTERN_S = "https://picsum.photos/400/400?image=%d"
-        private val RANDOM_URI_PATTERN_M = "https://picsum.photos/800/800?image=%d"
+        private const val RANDOM_URI_MAX_IMAGE_ID = 1000
+        private const val RANDOM_URI_PATTERN_S = "https://picsum.photos/400/400?image=%d"
+        private const val RANDOM_URI_PATTERN_M = "https://picsum.photos/800/800?image=%d"
 
         private val SAMPLE_URIS_LANDSCAPE = arrayOf("https://frescolib.org/static/sample-images/animal_a_%s.jpg", "https://frescolib.org/static/sample-images/animal_b_%s.jpg", "https://frescolib.org/static/sample-images/animal_c_%s.jpg", "https://frescolib.org/static/sample-images/animal_e_%s.jpg", "https://frescolib.org/static/sample-images/animal_f_%s.jpg", "https://frescolib.org/static/sample-images/animal_g_%s.jpg")
 
