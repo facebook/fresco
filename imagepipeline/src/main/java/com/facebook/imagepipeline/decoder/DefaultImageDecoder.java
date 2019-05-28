216096e62b7a81de7ca48db8ff71f3257c93bc27
Kim ke <kim.ke@ringcentral.com>
2019-05-28 03:49:35 -0700

    Fix invalid gif image cause crash (#2344)
    Summary:
    ## Motivation (required)
    
    when we want use lib to get and show invalid gif image, it's will crash on release version.
    Gif url:
    https://glip-vault-1.s3-accelerate.amazonaws.com/web/customer_files/1164984672268/FFD95CE6-F8D5-498D-BF86-E6A817B16EBA_zxc.gif?Expires=2075494478&AWSAccessKeyId=AKIAJROPQDFTIHBTLJJQ&Signature=huivGKdOrIMJYhHrOq6fut2p9Ns%3D&response-content-disposition=attachment
    
    ## Test Plan (required)
    
    Has already tested by our QA team.
    
    ## Next Steps
    Pull Request resolved: https://github.com/facebook/fresco/pull/2344
    
    Reviewed By: lambdapioneer
    
    Differential Revision: D15515977
    
    Pulled By: oprisnik
    
    fbshipit-source-id: ab702bef4012165bd07f271df443d522845cbf7d

--- imagepipeline/src/main/java/com/facebook/imagepipeline/decoder/DefaultImageDecoder.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/decoder/DefaultImageDecoder.java
@@ -132,6 +132,10 @@ public class DefaultImageDecoder implements ImageDecoder {
       final int length,
       final QualityInfo qualityInfo,
       final ImageDecodeOptions options) {
+    if (encodedImage.getWidth() == EncodedImage.UNKNOWN_WIDTH
+        || encodedImage.getHeight() == EncodedImage.UNKNOWN_HEIGHT) {
+      throw new DecodeException("image width or height is incorrect", encodedImage);
+    }
     if (!options.forceStaticImage && mAnimatedGifDecoder != null) {
       return mAnimatedGifDecoder.decode(encodedImage, length, qualityInfo, options);
     }
