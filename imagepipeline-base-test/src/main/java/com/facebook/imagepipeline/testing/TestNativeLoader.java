f547a024892f47c737b6286ddf77d560513b98da
Jędrzej Kula <jkula@fb.com>
2019-09-19 04:38:10 -0700

    Add TestNativeLoader class
    Summary: We need this class to simplify code for some tests
    
    Reviewed By: oprisnik
    
    Differential Revision: D17449355
    
    fbshipit-source-id: fd6978868b1a658f15d7b8cf08428f104ded874b

---
+++ imagepipeline-base-test/src/main/java/com/facebook/imagepipeline/testing/TestNativeLoader.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.testing;
+
+import com.facebook.soloader.NativeLoaderToSoLoaderDelegate;
+import com.facebook.soloader.SoLoader;
+import com.facebook.soloader.nativeloader.NativeLoader;
+
+/** Delegate to properly set and initialize NativeLoader for unit tests. */
+public class TestNativeLoader {
+
+  /**
+   * Initialize NativeLoader by setting NativeLoaderToSoLoaderDelegate as delegate and calling
+   * setInTestMode for SoLoader
+   */
+  public static void init() {
+    NativeLoader.init(new NativeLoaderToSoLoaderDelegate());
+    SoLoader.setInTestMode();
+  }
+}
