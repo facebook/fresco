1884d61893582ce410584a09e012fe345771b706
Aurelien Fredouelle <aurelienf@fb.com>
2019-09-24 08:52:29 -0700

    ImageFormatCheckerUtils: add helper to find pattern at offset
    Reviewed By: defHLT
    
    Differential Revision: D17501862
    
    fbshipit-source-id: 9716d1ebb3fc88fd6dc1b3a70d130659fb719c55

--- imagepipeline-base/src/main/java/com/facebook/imageformat/ImageFormatCheckerUtils.java
+++ imagepipeline-base/src/main/java/com/facebook/imageformat/ImageFormatCheckerUtils.java
@@ -39,14 +39,26 @@ public class ImageFormatCheckerUtils {
    * @return true if byteArray starts with pattern
    */
   public static boolean startsWithPattern(final byte[] byteArray, final byte[] pattern) {
+    return hasPatternAt(byteArray, pattern, 0);
+  }
+
+  /**
+   * Checks if byteArray interpreted as sequence of bytes starts with pattern starting at position
+   * equal to offset.
+   *
+   * @param byteArray the byte array to be checked
+   * @param pattern the pattern to check
+   * @return true if byteArray starts with pattern
+   */
+  public static boolean hasPatternAt(final byte[] byteArray, final byte[] pattern, int offset) {
     Preconditions.checkNotNull(byteArray);
     Preconditions.checkNotNull(pattern);
-    if (pattern.length > byteArray.length) {
+    if (offset + pattern.length > byteArray.length) {
       return false;
     }
 
     for (int i = 0; i < pattern.length; ++i) {
-      if (byteArray[i] != pattern[i]) {
+      if (byteArray[offset + i] != pattern[i]) {
         return false;
       }
     }
