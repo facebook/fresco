1b9dd34f1a9832322f68f25adb7fdeef437210e2
Alexander Oprisnik <oprisnik@fb.com>
2019-11-12 07:54:59 -0800

    Remove old unused release script
    Reviewed By: AurelC2G
    
    Differential Revision: D18450586
    
    fbshipit-source-id: d66411b91f4c8960836cfdab8585f4ba57c2a28e

--- release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-rm -rf $HOME/.gradle/caches
-rm -rf .gradle
-./gradlew clean allclean build uploadArchives
