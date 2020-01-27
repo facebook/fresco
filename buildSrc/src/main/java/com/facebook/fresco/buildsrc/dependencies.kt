2dba9861432d5520ab8ae7bd30ebde78c9ff796f
Alexander Oprisnik <oprisnik@fb.com>
2020-01-27 09:50:25 -0800

    Use buildSrc for dependencies
    Summary:
    We can use buildSrc to simplify dependency management.
    This change migrates JSR305 to use the new format.
    
    Reviewed By: defHLT
    
    Differential Revision: D18912886
    
    fbshipit-source-id: 29291852bc7d1874359682e5325614f622f9decc

---
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/dependencies.kt
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.buildsrc
+
+object Deps {
+    const val jsr305 = "com.google.code.findbugs:jsr305:3.0.2"
+}
