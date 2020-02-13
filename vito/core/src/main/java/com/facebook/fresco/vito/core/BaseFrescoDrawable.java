33e2c698561e4f5931428e63eee10628fed8caa9
Alexander Oprisnik <oprisnik@fb.com>
2020-02-13 06:01:09 -0800

    Added base Fresco Drawable that can be extended
    Differential Revision: D19791263
    
    fbshipit-source-id: 1036d3e24dac879fc1e54c615fb3caceae3fca8d

---
+++ vito/core/src/main/java/com/facebook/fresco/vito/core/BaseFrescoDrawable.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.vito.core;
+
+import android.graphics.drawable.Drawable;
+import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.drawee.drawable.FadeDrawable;
+import com.facebook.drawee.drawable.VisibilityCallback;
+import com.facebook.imagepipeline.image.CloseableImage;
+import java.io.Closeable;
+
+public class BaseFrescoDrawable extends FadeDrawable implements Closeable {
+
+  @VisibleForTesting @Nullable CloseableReference<CloseableImage> mImageReference;
+
+  private static final int LAYER_COUNT = 4;
+
+  private static final int PLACEHOLDER_DRAWABLE_INDEX = 0;
+  private static final int IMAGE_DRAWABLE_INDEX = 1;
+  private static final int PROGRESS_DRAWABLE_INDEX = 2;
+  private static final int OVERLAY_DRAWABLE_INDEX = 3;
+
+  private @Nullable VisibilityCallback mVisibilityCallback;
+
+  public BaseFrescoDrawable() {
+    super(new Drawable[LAYER_COUNT], true);
+  }
+
+  public BaseFrescoDrawable(boolean allLayersVisible) {
+    super(new Drawable[LAYER_COUNT], allLayersVisible);
+  }
+
+  public @Nullable Drawable setImage(
+      @Nullable Drawable imageDrawable,
+      @Nullable CloseableReference<CloseableImage> imageReference) {
+    CloseableReference.closeSafely(mImageReference);
+    mImageReference = CloseableReference.cloneOrNull(imageReference);
+    return setDrawable(IMAGE_DRAWABLE_INDEX, imageDrawable);
+  }
+
+  public @Nullable Drawable setImageDrawable(@Nullable Drawable newDrawable) {
+    return setImage(newDrawable, null);
+  }
+
+  public Drawable setOverlayDrawable(@Nullable Drawable drawable) {
+    return setDrawable(OVERLAY_DRAWABLE_INDEX, drawable);
+  }
+
+  public @Nullable Drawable setProgressDrawable(@Nullable Drawable drawable) {
+    return setDrawable(PROGRESS_DRAWABLE_INDEX, drawable);
+  }
+
+  public @Nullable Drawable setPlaceholderDrawable(@Nullable Drawable drawable) {
+    return setDrawable(PLACEHOLDER_DRAWABLE_INDEX, drawable);
+  }
+
+  public void fadeInImage(int durationMs) {
+    setTransitionDuration(durationMs);
+    beginBatchMode();
+    fadeOutLayer(PLACEHOLDER_DRAWABLE_INDEX);
+    fadeOutLayer(PROGRESS_DRAWABLE_INDEX);
+    fadeInLayer(IMAGE_DRAWABLE_INDEX);
+    endBatchMode();
+  }
+
+  public void showImageImmediately() {
+    hideLayerImmediately(PLACEHOLDER_DRAWABLE_INDEX);
+    showLayerImmediately(IMAGE_DRAWABLE_INDEX);
+  }
+
+  public void showOverlayImmediately() {
+    showLayerImmediately(OVERLAY_DRAWABLE_INDEX);
+  }
+
+  @Override
+  public void close() {
+    CloseableReference.closeSafely(mImageReference);
+    mImageReference = null;
+    for (int i = 0; i < LAYER_COUNT; i++) {
+      setDrawable(i, null);
+    }
+  }
+
+  @Override
+  public boolean setVisible(boolean visible, boolean restart) {
+    if (mVisibilityCallback != null) {
+      mVisibilityCallback.onVisibilityChange(visible);
+    }
+    return super.setVisible(visible, restart);
+  }
+
+  public void setVisibilityCallback(@Nullable VisibilityCallback visibilityCallback) {
+    mVisibilityCallback = visibilityCallback;
+  }
+
+  public Drawable getOverlayDrawable() {
+    return getDrawable(OVERLAY_DRAWABLE_INDEX);
+  }
+}
--- vito/core/src/main/java/com/facebook/fresco/vito/core/FrescoDrawable.java
+++ vito/core/src/main/java/com/facebook/fresco/vito/core/FrescoDrawable.java
@@ -7,86 +7,19 @@
 
 package com.facebook.fresco.vito.core;
 
-import android.graphics.drawable.Drawable;
 import androidx.annotation.Nullable;
-import androidx.annotation.VisibleForTesting;
-import com.facebook.common.references.CloseableReference;
-import com.facebook.drawee.drawable.FadeDrawable;
-import com.facebook.drawee.drawable.VisibilityCallback;
-import com.facebook.imagepipeline.image.CloseableImage;
 import java.io.Closeable;
 
-public class FrescoDrawable extends FadeDrawable implements Closeable {
-
-  @VisibleForTesting @Nullable CloseableReference<CloseableImage> mImageReference;
-
-  private static final int LAYER_COUNT = 4;
-
-  private static final int PLACEHOLDER_DRAWABLE_INDEX = 0;
-  private static final int IMAGE_DRAWABLE_INDEX = 1;
-  private static final int PROGRESS_DRAWABLE_INDEX = 2;
-  private static final int OVERLAY_DRAWABLE_INDEX = 3;
+public class FrescoDrawable extends BaseFrescoDrawable implements Closeable {
 
   private @Nullable FrescoState mFrescoState;
-  private @Nullable VisibilityCallback mVisibilityCallback;
 
   public FrescoDrawable() {
-    super(new Drawable[LAYER_COUNT], true);
+    super();
   }
 
   public FrescoDrawable(boolean allLayersVisible) {
-    super(new Drawable[LAYER_COUNT], allLayersVisible);
-  }
-
-  public @Nullable Drawable setImage(
-      @Nullable Drawable imageDrawable,
-      @Nullable CloseableReference<CloseableImage> imageReference) {
-    CloseableReference.closeSafely(mImageReference);
-    mImageReference = CloseableReference.cloneOrNull(imageReference);
-    return setDrawable(IMAGE_DRAWABLE_INDEX, imageDrawable);
-  }
-
-  public @Nullable Drawable setImageDrawable(@Nullable Drawable newDrawable) {
-    return setImage(newDrawable, null);
-  }
-
-  public Drawable setOverlayDrawable(@Nullable Drawable drawable) {
-    return setDrawable(OVERLAY_DRAWABLE_INDEX, drawable);
-  }
-
-  public @Nullable Drawable setProgressDrawable(@Nullable Drawable drawable) {
-    return setDrawable(PROGRESS_DRAWABLE_INDEX, drawable);
-  }
-
-  public @Nullable Drawable setPlaceholderDrawable(@Nullable Drawable drawable) {
-    return setDrawable(PLACEHOLDER_DRAWABLE_INDEX, drawable);
-  }
-
-  public void fadeInImage(int durationMs) {
-    setTransitionDuration(durationMs);
-    beginBatchMode();
-    fadeOutLayer(PLACEHOLDER_DRAWABLE_INDEX);
-    fadeOutLayer(PROGRESS_DRAWABLE_INDEX);
-    fadeInLayer(IMAGE_DRAWABLE_INDEX);
-    endBatchMode();
-  }
-
-  public void showImageImmediately() {
-    hideLayerImmediately(PLACEHOLDER_DRAWABLE_INDEX);
-    showLayerImmediately(IMAGE_DRAWABLE_INDEX);
-  }
-
-  public void showOverlayImmediately() {
-    showLayerImmediately(OVERLAY_DRAWABLE_INDEX);
-  }
-
-  @Override
-  public void close() {
-    CloseableReference.closeSafely(mImageReference);
-    mImageReference = null;
-    for (int i = 0; i < LAYER_COUNT; i++) {
-      setDrawable(i, null);
-    }
+    super(allLayersVisible);
   }
 
   @Nullable
@@ -96,21 +29,5 @@ public class FrescoDrawable extends FadeDrawable implements Closeable {
 
   public void setFrescoState(@Nullable FrescoState frescoState) {
     mFrescoState = frescoState;
-  }
-
-  @Override
-  public boolean setVisible(boolean visible, boolean restart) {
-    if (mVisibilityCallback != null) {
-      mVisibilityCallback.onVisibilityChange(visible);
-    }
-    return super.setVisible(visible, restart);
-  }
-
-  public void setVisibilityCallback(@Nullable VisibilityCallback visibilityCallback) {
-    mVisibilityCallback = visibilityCallback;
-  }
-
-  public Drawable getOverlayDrawable() {
-    return getDrawable(OVERLAY_DRAWABLE_INDEX);
   }
 }
