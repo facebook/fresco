b6e4c65d9c630ac6b92772e687f87e5e5863e878
Alexander Oprisnik <oprisnik@fb.com>
2020-03-06 05:42:32 -0800

    Cancel prefetch for Litho spec
    Reviewed By: defHLT
    
    Differential Revision: D20282212
    
    fbshipit-source-id: a509ebe91c6587f688383b4541a36800fc5f3215

--- vito/litho/src/main/java/com/facebook/fresco/vito/litho/FrescoVitoImage2Spec.java
+++ vito/litho/src/main/java/com/facebook/fresco/vito/litho/FrescoVitoImage2Spec.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.view.View;
 import androidx.core.util.ObjectsCompat;
 import androidx.core.view.accessibility.AccessibilityNodeInfoCompat;
+import com.facebook.datasource.DataSource;
 import com.facebook.fresco.vito.core.FrescoDrawable2;
 import com.facebook.fresco.vito.core.PrefetchTarget;
 import com.facebook.fresco.vito.core.VitoImageRequest;
@@ -23,8 +24,10 @@ import com.facebook.litho.AccessibilityRole;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
 import com.facebook.litho.Diff;
+import com.facebook.litho.Output;
 import com.facebook.litho.Size;
 import com.facebook.litho.annotations.CachedValue;
+import com.facebook.litho.annotations.FromPrepare;
 import com.facebook.litho.annotations.MountSpec;
 import com.facebook.litho.annotations.OnBind;
 import com.facebook.litho.annotations.OnCalculateCachedValue;
@@ -79,9 +82,11 @@ public class FrescoVitoImage2Spec {
   static void onPrepare(
       ComponentContext c,
       @Prop(optional = true) final @Nullable Object callerContext,
-      @CachedValue VitoImageRequest imageRequest) {
-    FrescoContextProvider.getPrefetcher()
-        .prefetch(PrefetchTarget.MEMORY_DECODED, imageRequest, callerContext);
+      @CachedValue VitoImageRequest imageRequest,
+      Output<DataSource<Void>> prefetchDataSource) {
+    prefetchDataSource.set(
+        FrescoContextProvider.getPrefetcher()
+            .prefetch(PrefetchTarget.MEMORY_DECODED, imageRequest, callerContext));
   }
 
   @OnMount
@@ -90,9 +95,13 @@ public class FrescoVitoImage2Spec {
       final FrescoDrawable2 frescoDrawable,
       @Prop(optional = true) final @Nullable Object callerContext,
       @Prop(optional = true) final @Nullable ImageListener imageListener,
-      @CachedValue VitoImageRequest imageRequest) {
+      @CachedValue VitoImageRequest imageRequest,
+      @FromPrepare DataSource<Void> prefetchDataSource) {
     FrescoContextProvider.getController()
         .fetch(frescoDrawable, imageRequest, callerContext, imageListener);
+    if (prefetchDataSource != null) {
+      prefetchDataSource.close();
+    }
   }
 
   @OnBind
@@ -101,21 +110,37 @@ public class FrescoVitoImage2Spec {
       final FrescoDrawable2 frescoDrawable,
       @Prop(optional = true) final @Nullable Object callerContext,
       @Prop(optional = true) final @Nullable ImageListener imageListener,
-      @CachedValue VitoImageRequest imageRequest) {
+      @CachedValue VitoImageRequest imageRequest,
+      @FromPrepare DataSource<Void> prefetchDataSource) {
     // We fetch in both mount and bind in case an unbind event triggered a delayed release.
     // We'll only trigger an actual fetch if needed. Most of the time, this will be a no-op.
     FrescoContextProvider.getController()
         .fetch(frescoDrawable, imageRequest, callerContext, imageListener);
+    if (prefetchDataSource != null) {
+      prefetchDataSource.close();
+    }
   }
 
   @OnUnbind
-  static void onUnbind(ComponentContext c, FrescoDrawable2 frescoDrawable) {
+  static void onUnbind(
+      ComponentContext c,
+      FrescoDrawable2 frescoDrawable,
+      @FromPrepare DataSource<Void> prefetchDataSource) {
     FrescoContextProvider.getController().releaseDelayed(frescoDrawable);
+    if (prefetchDataSource != null) {
+      prefetchDataSource.close();
+    }
   }
 
   @OnUnmount
-  static void onUnmount(ComponentContext c, FrescoDrawable2 frescoDrawable) {
+  static void onUnmount(
+      ComponentContext c,
+      FrescoDrawable2 frescoDrawable,
+      @FromPrepare DataSource<Void> prefetchDataSource) {
     FrescoContextProvider.getController().release(frescoDrawable);
+    if (prefetchDataSource != null) {
+      prefetchDataSource.close();
+    }
   }
 
   @ShouldUpdate(onMount = true)
