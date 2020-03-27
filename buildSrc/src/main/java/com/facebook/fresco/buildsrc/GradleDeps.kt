62c0ce17ba2b199ed5731d89b54236a57500c49e
Alexander Oprisnik <oprisnik@fb.com>
2020-03-27 04:26:51 -0700

    Added GradleDeps to buildSrc for Android Gradle plugin and downgrade version
    Summary:
    We want to move to buildSrc, so we should have a separate class for Gradle deps.
    
    Also, this downgrades the Gradle version back to 3.5.3 so that we can also use Android Studio 3.5. The current version requires at least 3.6.
    
    Reviewed By: xapienz
    
    Differential Revision: D20675521
    
    fbshipit-source-id: 26cad85f6d0b6c1bf169e3053f88642093259114

---
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/GradleDeps.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.buildsrc
+
+object GradleDeps {
+
+    object Android {
+        private const val version = "3.5.3"
+
+        const val gradlePlugin = "com.android.tools.build:gradle:$version"
+    }
+}
