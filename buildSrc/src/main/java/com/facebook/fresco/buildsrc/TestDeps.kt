bb117422a715aebe41c47ee8e0c5da75789a75ec
Alexander Oprisnik <oprisnik@fb.com>
2020-06-16 04:09:33 -0700

    Added TestDeps to buildsrc and moved junit
    Reviewed By: defHLT
    
    Differential Revision: D22010617
    
    fbshipit-source-id: 9686d36e4ce75b336f18becc966bf5727b41d033

---
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/TestDeps.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.buildsrc
+
+object TestDeps {
+
+    const val junit = "junit:junit:4.12"
+}
