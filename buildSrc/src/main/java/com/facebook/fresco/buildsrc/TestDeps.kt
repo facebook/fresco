72bf1efec8a71973bfb8b5d7cacf2e9b540d3f31
Alexander Oprisnik <oprisnik@fb.com>
2020-09-11 16:44:55 -0700

    Use buildSrc for Powermock deps
    Reviewed By: defHLT
    
    Differential Revision: D23623999
    
    fbshipit-source-id: d6e3ca432b6d1cc409c2ec1e08926cedd97b787d

--- buildSrc/src/main/java/com/facebook/fresco/buildsrc/TestDeps.kt
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/TestDeps.kt
@@ -10,8 +10,17 @@ package com.facebook.fresco.buildsrc
 object TestDeps {
 
     const val junit = "junit:junit:4.12"
-    
+
     const val mockitoCore = "org.mockito:mockito-core:2.26.0"
-    
+
     const val festAssertCore = "org.easytesting:fest-assert-core:2.0M10"
+
+    object Powermock {
+        private const val version = "2.0.2"
+
+        const val apiMockito = "org.powermock:powermock-api-mockito2:$version"
+        const val moduleJunit4 = "org.powermock:powermock-module-junit4:$version"
+        const val moduleJunit4Rule = "org.powermock:powermock-module-junit4-rule:$version"
+        const val classloadingXstream = "org.powermock:powermock-classloading-xstream:$version"
+    }
 }
