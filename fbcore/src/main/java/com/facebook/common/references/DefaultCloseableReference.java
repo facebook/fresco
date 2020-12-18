56bbe00da9429986d118378fd1395834fff26ed8
Jitu Das <jitudas@fb.com>
2020-12-18 12:43:03 -0800

    Record stacktraces for clones of closeable reference
    Reviewed By: markivfb
    
    Differential Revision: D25524635
    
    fbshipit-source-id: 7e738f2367a51bb40fb9ab9d45da00da075d2e54

--- fbcore/src/main/java/com/facebook/common/references/DefaultCloseableReference.java
+++ fbcore/src/main/java/com/facebook/common/references/DefaultCloseableReference.java
@@ -33,7 +33,8 @@ public class DefaultCloseableReference<T> extends CloseableReference<T> {
   @Override
   public CloseableReference<T> clone() {
     Preconditions.checkState(isValid());
-    return new DefaultCloseableReference<T>(mSharedReference, mLeakHandler, mStacktrace);
+    return new DefaultCloseableReference<T>(
+        mSharedReference, mLeakHandler, mStacktrace != null ? new Throwable(mStacktrace) : null);
   }
 
   @Override
