cbcd8c9b322470dcbccdf21cbef825b6424bf9d0
Patrick Pijnappel <ppijnappel@twitter.com>
2021-01-06 11:38:21 +1100

    Update libpng to version 1.6.37

--- static-webp/src/main/jni/third-party/libpng-1.6.37/pnglibconf.h
+++ static-webp/src/main/jni/third-party/libpng-1.6.37/pnglibconf.h
@@ -1,8 +1,9 @@
 /* pnglibconf.h - library build configuration */
 
-/* libpng version 1.6.36.git */
+/* libpng version 1.6.37 */
 
-/* Copyright (c) 1998-2017 Glenn Randers-Pehrson */
+/* Copyright (c) 2018-2019 Cosmin Truta */
+/* Copyright (c) 1998-2002,2004,2006-2018 Glenn Randers-Pehrson */
 
 /* This code is released under the libpng license. */
 /* For conditions of distribution and use, see the disclaimer */
