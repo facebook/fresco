b558f9e8f1efb5eea78645e95838696142ddeca0
Alexander Oprisnik <oprisnik@fb.com>
2021-02-01 09:45:57 -0800

    Update SoLoader to 0.10.1
    Summary: 0.10.0 has issues, so 0.10.1 has been released.
    
    Reviewed By: passy
    
    Differential Revision: D26173518
    
    fbshipit-source-id: c78dd66e1eb4933545e1daa701a4fbb30e30a4a9

--- buildSrc/src/main/java/com/facebook/fresco/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/dependencies.kt
@@ -50,7 +50,7 @@ object Deps {
   }
 
   object SoLoader {
-    private const val version = "0.10.0"
+    private const val version = "0.10.1"
     const val soloaderAnnotation = "com.facebook.soloader:annotation:$version"
     const val nativeloader = "com.facebook.soloader:nativeloader:$version"
     const val soloader = "com.facebook.soloader:soloader:$version"
