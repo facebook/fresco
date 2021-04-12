5ea3877bfdd45a46dabfdfad4ec3f9d011730a74
Alexander Oprisnik <oprisnik@fb.com>
2021-04-12 08:54:00 -0700

    Clean up native circular bitmap transformation code
    Summary: The native bitmap transformation can now be passed down to the component, so we do not need reflection.
    
    Reviewed By: colriot
    
    Differential Revision: D27649088
    
    fbshipit-source-id: 5ecfe6d8d8e18c1f9030b61a6a3fc32a38912125

--- vito/nativecode/src/main/java/com/facebook/fresco/vito/nativecode/CircularBitmapTransformation.java
+++ vito/nativecode/src/main/java/com/facebook/fresco/vito/nativecode/CircularBitmapTransformation.java
@@ -8,10 +8,9 @@
 package com.facebook.fresco.vito.nativecode;
 
 import android.graphics.Bitmap;
+import com.facebook.imagepipeline.nativecode.NativeRoundingFilter;
 import com.facebook.imagepipeline.transformation.BitmapTransformation;
 import com.facebook.infer.annotation.Nullsafe;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import javax.annotation.Nullable;
 
 @Nullsafe(Nullsafe.Mode.LOCAL)
@@ -20,22 +19,6 @@ public class CircularBitmapTransformation implements BitmapTransformation {
   private final boolean mAntiAliased;
 
   private final boolean mUseFastNativeRounding;
-
-  private static Method sToCircle;
-
-  private static Method sToCircleFast;
-
-  static {
-    try {
-      Class<?> clazz = Class.forName("com.facebook.imagepipeline.nativecode.NativeRoundingFilter");
-      sToCircle = clazz.getMethod("toCircle", Bitmap.class, boolean.class);
-      sToCircleFast = clazz.getMethod("toCircleFast", Bitmap.class, boolean.class);
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException("Native code for rounding unsupported", e);
-    } catch (NoSuchMethodException e) {
-      throw new RuntimeException("Native code for rounding unsupported", e);
-    }
-  }
 
   public CircularBitmapTransformation(boolean antiAliased) {
     this(antiAliased, false);
@@ -48,16 +31,10 @@ public class CircularBitmapTransformation implements BitmapTransformation {
 
   @Override
   public void transform(Bitmap bitmap) {
-    try {
-      if (mUseFastNativeRounding) {
-        sToCircleFast.invoke(null, bitmap, mAntiAliased);
-      } else {
-        sToCircle.invoke(null, bitmap, mAntiAliased);
-      }
-    } catch (IllegalAccessException e) {
-      throw new RuntimeException("Native code for rounding unsupported", e);
-    } catch (InvocationTargetException e) {
-      throw new RuntimeException("Native code for rounding unsupported", e);
+    if (mUseFastNativeRounding) {
+      NativeRoundingFilter.toCircleFast(bitmap, mAntiAliased);
+    } else {
+      NativeRoundingFilter.toCircle(bitmap, mAntiAliased);
     }
   }
 
