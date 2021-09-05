0d0167c84a8e415f2480eb9dd78ad3f3a4fc9ccd
Eirini Lygerou <irini.lyg@gmail.com>
2021-09-05 18:19:33 +0300

    Add other neon c files which the jsimd.c is depending on.

--- static-webp/src/main/jni/third-party/libjpeg-turbo-2.1.1/Android.mk
+++ static-webp/src/main/jni/third-party/libjpeg-turbo-2.1.1/Android.mk
@@ -18,12 +18,44 @@ JPEGTURBO_SRC_FILES := \
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 JPEGTURBO_SRC_FILES += \
 	simd/arm/aarch32/jsimd_neon.S \
-	simd/arm/aarch32/jsimd.c
+	simd/arm/aarch32/jsimd.c \
+	simd/arm/jccolor-neon.c \
+	simd/arm/jcgray-neon.c \
+	simd/arm/jcgryext-neon.c \
+	simd/arm/jcphuff-neon.c \
+	simd/arm/jcsample-neon.c \
+	simd/arm/jdcolext-neon.c \
+	simd/arm/jdcolor-neon.c \
+	simd/arm/jdmerge-neon.c \
+	simd/arm/jdmrgext-neon.c \
+	simd/arm/jdsample-neon.c \
+	simd/arm/jfdctfst-neon.c \
+	simd/arm/jfdctint-neon.c \
+	simd/arm/jidctfst-neon.c \
+	simd/arm/jidctint-neon.c \
+	simd/arm/jidctred-neon.c \
+	simd/arm/jquanti-neon.c
 
 else ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
 JPEGTURBO_SRC_FILES += \
 	simd/arm/aarch64/jsimd_neon.S \
-	simd/arm/aarch64/jsimd.c
+	simd/arm/aarch64/jsimd.c \
+	simd/arm/jccolor-neon.c \
+	simd/arm/jcgray-neon.c \
+	simd/arm/jcgryext-neon.c \
+	simd/arm/jcphuff-neon.c \
+	simd/arm/jcsample-neon.c \
+	simd/arm/jdcolext-neon.c \
+	simd/arm/jdcolor-neon.c \
+	simd/arm/jdmerge-neon.c \
+	simd/arm/jdmrgext-neon.c \
+	simd/arm/jdsample-neon.c \
+	simd/arm/jfdctfst-neon.c \
+	simd/arm/jfdctint-neon.c \
+	simd/arm/jidctfst-neon.c \
+	simd/arm/jidctint-neon.c \
+	simd/arm/jidctred-neon.c \
+	simd/arm/jquanti-neon.c
 
 else
 JPEGTURBO_SRC_FILES += jsimd_none.c
