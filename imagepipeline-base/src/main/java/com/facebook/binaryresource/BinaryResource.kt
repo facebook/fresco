76461b3458d9b22ecf786588a48649fa809a0a92
Alexander Oprisnik <oprisnik@fb.com>
2021-12-07 14:34:27 -0800

    Convert libraries/fresco/imagepipeline-base/src/main/java/com/facebook/binaryresource to Kotlin
    Reviewed By: defHLT
    
    Differential Revision: D32756651
    
    fbshipit-source-id: d1ce443836da89e52e4ccbbb730d77c06591cc56

--- imagepipeline-base/src/main/java/com/facebook/binaryresource/BinaryResource.kt
+++ imagepipeline-base/src/main/java/com/facebook/binaryresource/BinaryResource.kt
@@ -5,11 +5,10 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.binaryresource;
+package com.facebook.binaryresource
 
-import com.facebook.infer.annotation.Nullsafe;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.IOException
+import java.io.InputStream
 
 /*
  * Interface representing a sequence of bytes that abstracts underlying source (e.g. file).
@@ -20,40 +19,40 @@ import java.io.InputStream;
  *
  * <p>Inspired partly by Guava's ByteSource class, but does not use its implementation.
  */
-@Nullsafe(Nullsafe.Mode.STRICT)
-public interface BinaryResource {
+
+interface BinaryResource {
 
   /**
-   * Opens a new {@link InputStream} for reading from this source. This method should return a new,
+   * Opens a new [InputStream] for reading from this source. This method should return a new,
    * independent stream each time it is called.
    *
-   * <p>The caller is responsible for ensuring that the returned stream is closed.
+   * The caller is responsible for ensuring that the returned stream is closed.
    *
    * @throws IOException if an I/O error occurs in the process of opening the stream
    */
-  InputStream openStream() throws IOException;
+  @Throws(IOException::class) fun openStream(): InputStream
 
   /**
    * Reads the full contents of this byte source as a byte array.
    *
    * @throws IOException if an I/O error occurs in the process of reading from this source
    */
-  byte[] read() throws IOException;
+  @Throws(IOException::class) fun read(): ByteArray
 
   /**
    * Returns the size of this source in bytes. This may be a heavyweight operation that will open a
-   * stream, read (or {@link InputStream#skip(long) skip}, if possible) to the end of the stream and
-   * return the total number of bytes that were read.
+   * stream, read (or [skip][InputStream.skip], if possible) to the end of the stream and return the
+   * total number of bytes that were read.
    *
-   * <p>For some sources, such as a file, this method may use a more efficient implementation. Note
-   * that in such cases, it is <i>possible</i> that this method will return a different number of
-   * bytes than would be returned by reading all of the bytes (for example, some special files may
-   * return a size of 0 despite actually having content when read).
+   * For some sources, such as a file, this method may use a more efficient implementation. Note
+   * that in such cases, it is *possible* that this method will return a different number of bytes
+   * than would be returned by reading all of the bytes (for example, some special files may return
+   * a size of 0 despite actually having content when read).
    *
-   * <p>In either case, if this is a mutable source such as a file, the size it returns may not be
-   * the same number of bytes a subsequent read would return.
+   * In either case, if this is a mutable source such as a file, the size it returns may not be the
+   * same number of bytes a subsequent read would return.
    *
    * @throws IOException if an I/O error occurs in the process of reading the size of this source
    */
-  long size();
+  fun size(): Long
 }
