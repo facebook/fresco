2cc09ba1aec7b75f2cc76a4e3a19d1dcd03f9f92
CodemodService FBSourceKtfmtLinterBot <>
2021-12-07 19:57:30 -0800

    Daily `arc lint --take KTFMT`
    Reviewed By: zertosh
    
    Differential Revision: D32940376
    
    fbshipit-source-id: 503b7fb53cbd8050948e107fb8eb673541df901d

--- imagepipeline-base/src/main/java/com/facebook/binaryresource/FileBinaryResource.kt
+++ imagepipeline-base/src/main/java/com/facebook/binaryresource/FileBinaryResource.kt
@@ -8,7 +8,6 @@
 package com.facebook.binaryresource
 
 import com.facebook.common.internal.Files
-import com.facebook.common.internal.Preconditions
 import java.io.File
 import java.io.FileInputStream
 import java.io.IOException
