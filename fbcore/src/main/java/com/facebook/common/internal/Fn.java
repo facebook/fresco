b7778fce28158b91c5eba304056e0d96f33a828f
Andres Suarez <asuarez@fb.com>
2021-12-27 14:37:26 -0800

    Update copyright headers from Facebook to Meta
    Reviewed By: bhamodi
    
    Differential Revision: D33329868
    
    fbshipit-source-id: 706ae2580a81d65afd72abfcc9be630e8cde7391

--- fbcore/src/main/java/com/facebook/common/internal/Fn.java
+++ fbcore/src/main/java/com/facebook/common/internal/Fn.java
@@ -1,4 +1,9 @@
-// (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 package com.facebook.common.internal;
 
