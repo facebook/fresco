0a33886e85389c7092320e5a05a166c1f46a510a
Alexander Oprisnik <oprisnik@fb.com>
2022-06-17 11:05:05 -0700

    Migrate ImageOptionsDrawableFactory to Kotlin
    Reviewed By: defHLT
    
    Differential Revision: D37182422
    
    fbshipit-source-id: 92d700ad53546271377977f09113e3f64df4254e

---
+++ vito/options/src/main/java/com/facebook/fresco/vito/options/ImageOptionsDrawableFactory.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.vito.options
+
+import android.graphics.drawable.Drawable
+import com.facebook.imagepipeline.image.CloseableImage
+
+interface ImageOptionsDrawableFactory {
+  fun createDrawable(closeableImage: CloseableImage, imageOptions: ImageOptions): Drawable?
+}
