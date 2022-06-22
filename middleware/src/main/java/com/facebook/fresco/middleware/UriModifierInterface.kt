d8bb8f5c251d54433be3635f7f6a4e274e943000
Artem Kholodnyi <defhlt@fb.com>
2022-06-22 11:01:56 -0700

    Add UriModifierInterface
    Reviewed By: oprisnik
    
    Differential Revision: D37238243
    
    fbshipit-source-id: 00acc8546783c856b60ad0329634ffed706e7126

---
+++ middleware/src/main/java/com/facebook/fresco/middleware/UriModifierInterface.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.middleware
+
+import android.net.Uri
+import com.facebook.drawee.drawable.ScalingUtils.ScaleType
+
+data class Dimensions(val w: Int, val h: Int) {
+  override fun toString() = "${w}x${h}"
+}
+
+interface UriModifierInterface {
+  fun modifyUri(uri: Uri, viewport: Dimensions?, scaleType: ScaleType): Uri = uri
+}
+
+object UriModifier : UriModifierInterface {
+  @kotlin.jvm.JvmField var INSTANCE: UriModifierInterface = this
+}
