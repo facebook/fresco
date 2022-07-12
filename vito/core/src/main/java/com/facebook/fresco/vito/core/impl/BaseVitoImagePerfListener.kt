d584735e197a1bb058c47b0c4e63eee3038c144a
Alexander Oprisnik <oprisnik@fb.com>
2022-07-12 12:30:50 -0700

    Migrate Vito listener implementations in core/impl to Kotlin
    Reviewed By: defHLT
    
    Differential Revision: D37786837
    
    fbshipit-source-id: 4ee9b18c41dff63564c4f77799279d8c2158d17d

---
+++ vito/core/src/main/java/com/facebook/fresco/vito/core/impl/BaseVitoImagePerfListener.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.vito.core.impl
+
+import com.facebook.fresco.vito.core.FrescoDrawableInterface
+import com.facebook.fresco.vito.core.VitoImagePerfListener
+
+open class BaseVitoImagePerfListener : VitoImagePerfListener {
+  override fun onImageMount(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onImageUnmount(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onImageBind(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onImageUnbind(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onImageFetch(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onImageSuccess(drawable: FrescoDrawableInterface, wasImmediate: Boolean) = Unit
+
+  override fun onImageError(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onImageRelease(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onScheduleReleaseDelayed(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onScheduleReleaseNextFrame(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onReleaseImmediately(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onDrawableReconfigured(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onIgnoreResult(drawable: FrescoDrawableInterface) = Unit
+
+  override fun onIgnoreFailure(drawable: FrescoDrawableInterface) = Unit
+}
---
+++ vito/core/src/main/java/com/facebook/fresco/vito/core/impl/CombinedImageListenerImpl.kt
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.vito.core.impl
+
+import android.graphics.drawable.Drawable
+import com.facebook.drawee.backends.pipeline.info.ImageOrigin
+import com.facebook.fresco.ui.common.BaseControllerListener2
+import com.facebook.fresco.ui.common.ControllerListener2
+import com.facebook.fresco.ui.common.ControllerListener2.Extras
+import com.facebook.fresco.ui.common.VitoUtils
+import com.facebook.fresco.vito.core.CombinedImageListener
+import com.facebook.fresco.vito.core.VitoImageRequest
+import com.facebook.fresco.vito.core.VitoImageRequestListener
+import com.facebook.fresco.vito.listener.ImageListener
+import com.facebook.imagepipeline.image.ImageInfo
+import java.io.Closeable
+import java.io.IOException
+
+class CombinedImageListenerImpl : CombinedImageListener {
+
+  private var vitoImageRequestListener: VitoImageRequestListener? = null
+  override var imageListener: ImageListener? = null
+  private var controllerListener2: ControllerListener2<ImageInfo>? =
+      BaseControllerListener2.getNoOpListener()
+  private var imagePerfControllerListener: ControllerListener2<ImageInfo>? = null
+
+  override fun setVitoImageRequestListener(vitoImageRequestListener: VitoImageRequestListener?) {
+    this.vitoImageRequestListener = vitoImageRequestListener
+  }
+
+  override fun setControllerListener2(controllerListener2: ControllerListener2<ImageInfo>?) {
+    this.controllerListener2 = controllerListener2
+  }
+
+  fun setImagePerfControllerListener(imagePerfControllerListener: ControllerListener2<ImageInfo>?) {
+    this.imagePerfControllerListener = imagePerfControllerListener
+  }
+
+  override fun onSubmit(
+      id: Long,
+      imageRequest: VitoImageRequest,
+      callerContext: Any?,
+      extras: Extras?
+  ) {
+    vitoImageRequestListener?.onSubmit(id, imageRequest, callerContext, extras)
+    imageListener?.onSubmit(id, callerContext)
+    val stringId = VitoUtils.getStringId(id)
+    controllerListener2?.onSubmit(stringId, callerContext, extras)
+    imagePerfControllerListener?.onSubmit(stringId, callerContext, extras)
+  }
+
+  override fun onPlaceholderSet(id: Long, imageRequest: VitoImageRequest, placeholder: Drawable?) {
+    vitoImageRequestListener?.onPlaceholderSet(id, imageRequest, placeholder)
+    imageListener?.onPlaceholderSet(id, placeholder)
+  }
+
+  override fun onFinalImageSet(
+      id: Long,
+      imageRequest: VitoImageRequest,
+      @ImageOrigin imageOrigin: Int,
+      imageInfo: ImageInfo?,
+      extras: Extras?,
+      drawable: Drawable?
+  ) {
+    vitoImageRequestListener?.onFinalImageSet(
+        id, imageRequest, imageOrigin, imageInfo, extras, drawable)
+    imageListener?.onFinalImageSet(id, imageOrigin, imageInfo, drawable)
+    val stringId = VitoUtils.getStringId(id)
+    controllerListener2?.onFinalImageSet(stringId, imageInfo, extras)
+    imagePerfControllerListener?.onFinalImageSet(stringId, imageInfo, extras)
+  }
+
+  override fun onIntermediateImageSet(
+      id: Long,
+      imageRequest: VitoImageRequest,
+      imageInfo: ImageInfo?
+  ) {
+    vitoImageRequestListener?.onIntermediateImageSet(id, imageRequest, imageInfo)
+    imageListener?.onIntermediateImageSet(id, imageInfo)
+    val stringId = VitoUtils.getStringId(id)
+    controllerListener2?.onIntermediateImageSet(stringId, imageInfo)
+    imagePerfControllerListener?.onIntermediateImageSet(stringId, imageInfo)
+  }
+
+  override fun onIntermediateImageFailed(
+      id: Long,
+      imageRequest: VitoImageRequest,
+      throwable: Throwable?
+  ) {
+    vitoImageRequestListener?.onIntermediateImageFailed(id, imageRequest, throwable)
+    imageListener?.onIntermediateImageFailed(id, throwable)
+    val stringId = VitoUtils.getStringId(id)
+    controllerListener2?.onIntermediateImageFailed(stringId)
+    imagePerfControllerListener?.onIntermediateImageFailed(stringId)
+  }
+
+  override fun onFailure(
+      id: Long,
+      imageRequest: VitoImageRequest,
+      error: Drawable?,
+      throwable: Throwable?,
+      extras: Extras?
+  ) {
+    vitoImageRequestListener?.onFailure(id, imageRequest, error, throwable, extras)
+    imageListener?.onFailure(id, error, throwable)
+    val stringId = VitoUtils.getStringId(id)
+    controllerListener2?.onFailure(stringId, throwable, extras)
+    imagePerfControllerListener?.onFailure(stringId, throwable, extras)
+  }
+
+  override fun onRelease(id: Long, imageRequest: VitoImageRequest, extras: Extras?) {
+    vitoImageRequestListener?.onRelease(id, imageRequest, extras)
+    imageListener?.onRelease(id)
+    val stringId = VitoUtils.getStringId(id)
+    controllerListener2?.onRelease(stringId, extras)
+    imagePerfControllerListener?.onRelease(stringId, extras)
+  }
+
+  override fun onReset() {
+    try {
+      (imagePerfControllerListener as? Closeable)?.close()
+    } catch (e: IOException) {}
+  }
+}
