c80c411cb8c4fa2bb593187c2f99bffb0e2b80f8
Ibrahim Yilmaz <ibrahimyil@fb.com>
2022-07-21 03:57:33 -0700

    Convert VitoImagePipelineImpl.java to Kotlin
    Reviewed By: oprisnik
    
    Differential Revision: D37999671
    
    fbshipit-source-id: e573d41840260f4750cd072c74264cb9c16b61f2

---
+++ vito/core/src/main/java/com/facebook/fresco/vito/core/impl/VitoImagePipelineImpl.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.vito.core.impl
+
+import android.content.res.Resources
+import android.graphics.Rect
+import com.facebook.common.references.CloseableReference
+import com.facebook.datasource.DataSource
+import com.facebook.fresco.middleware.Dimensions
+import com.facebook.fresco.middleware.UriModifier
+import com.facebook.fresco.ui.common.VitoUtils
+import com.facebook.fresco.vito.core.ImagePipelineUtils
+import com.facebook.fresco.vito.core.VitoImagePipeline
+import com.facebook.fresco.vito.core.VitoImageRequest
+import com.facebook.fresco.vito.options.ImageOptions
+import com.facebook.fresco.vito.options.ImageOptions.Companion.defaults
+import com.facebook.fresco.vito.source.ImageSource
+import com.facebook.fresco.vito.source.ImageSourceProvider
+import com.facebook.fresco.vito.source.SingleImageSource
+import com.facebook.imagepipeline.core.ImagePipeline
+import com.facebook.imagepipeline.image.CloseableImage
+import com.facebook.imagepipeline.listener.RequestListener
+import com.facebook.imagepipeline.systrace.FrescoSystrace
+
+/** Vito image pipeline to fetch an image for a given VitoImageRequest. */
+class VitoImagePipelineImpl(
+    private val imagePipeline: ImagePipeline,
+    private val imagePipelineUtils: ImagePipelineUtils
+) : VitoImagePipeline {
+
+  override fun createImageRequest(
+      resources: Resources,
+      imageSource: ImageSource,
+      options: ImageOptions?
+  ): VitoImageRequest = createImageRequest(resources, imageSource, options, null)
+
+  override fun createImageRequest(
+      resources: Resources,
+      imageSource: ImageSource,
+      options: ImageOptions?,
+      viewport: Rect?
+  ): VitoImageRequest {
+    val imageOptions = options ?: defaults()
+
+    val finalImageSource =
+        when {
+          imageOptions.experimentalDynamicSize && imageSource is SingleImageSource -> {
+            val uri = imageSource.uri
+            val maybeModifiedUri =
+                UriModifier.INSTANCE.modifyUri(
+                    uri,
+                    viewport?.let { Dimensions(it.width(), it.height()) },
+                    imageOptions.actualImageScaleType)
+
+            if (maybeModifiedUri != uri) ImageSourceProvider.forUri(maybeModifiedUri)
+            else imageSource
+          }
+          else -> imageSource
+        }
+
+    val finalImageRequest =
+        ImageSourceToImagePipelineAdapter.maybeExtractFinalImageRequest(
+            finalImageSource, imagePipelineUtils, imageOptions)
+    val finalImageCacheKey = finalImageRequest?.let { imagePipeline.getCacheKey(it, null) }
+
+    return VitoImageRequest(
+        resources, finalImageSource, imageOptions, finalImageRequest, finalImageCacheKey)
+  }
+
+  override fun getCachedImage(imageRequest: VitoImageRequest): CloseableReference<CloseableImage>? {
+    if (FrescoSystrace.isTracing()) {
+      FrescoSystrace.beginSection("VitoImagePipeline#getCachedImage")
+    }
+
+    return try {
+      val cachedImageReference = imagePipeline.getCachedImage(imageRequest.finalImageCacheKey)
+      if (CloseableReference.isValid(cachedImageReference)) {
+        cachedImageReference
+      } else {
+        null
+      }
+    } finally {
+      if (FrescoSystrace.isTracing()) {
+        FrescoSystrace.endSection()
+      }
+    }
+  }
+
+  override fun fetchDecodedImage(
+      imageRequest: VitoImageRequest,
+      callerContext: Any?,
+      requestListener: RequestListener?,
+      uiComponentId: Long
+  ): DataSource<CloseableReference<CloseableImage>> =
+      ImageSourceToImagePipelineAdapter.createDataSourceSupplier(
+              imageRequest.imageSource,
+              imagePipeline,
+              imagePipelineUtils,
+              imageRequest.imageOptions,
+              callerContext,
+              requestListener,
+              VitoUtils.getStringId(uiComponentId))
+          .get()
+}
