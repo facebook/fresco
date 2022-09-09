234030c9c0a2896d1ed17503f02804d81b2a4086
Aniruddha Zalani <zalani@fb.com>
2022-09-09 03:01:35 -0700

    Fresco Kotlin conversion: Migrate file fbandroid/libraries/fresco/middleware/src/main/java/com/facebook/fresco/middleware/MiddlewareUtils.java
    Reviewed By: oprisnik
    
    Differential Revision: D39310504
    
    fbshipit-source-id: d420ed86b546153d0bab063cef8fe4c357aad26a

---
+++ middleware/src/main/java/com/facebook/fresco/middleware/MiddlewareUtils.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.middleware
+
+import android.graphics.PointF
+import android.graphics.Rect
+import android.net.Uri
+import com.facebook.fresco.ui.common.ControllerListener2.Extras
+
+object MiddlewareUtils {
+  @JvmStatic
+  fun obtainExtras(
+      componentAttribution: Map<String, Any>,
+      shortcutAttribution: Map<String, Any>,
+      dataSourceExtras: Map<String, Any>?,
+      viewportDimensions: Rect?,
+      scaleType: String?,
+      focusPoint: PointF?,
+      imageExtras: Map<String?, Any?>?,
+      callerContext: Any?,
+      mainUri: Uri?
+  ): Extras {
+    val extras = Extras()
+    if (viewportDimensions != null) {
+      extras.viewportWidth = viewportDimensions.width()
+      extras.viewportHeight = viewportDimensions.height()
+    }
+    extras.scaleType = scaleType
+    if (focusPoint != null) {
+      extras.focusX = focusPoint.x
+      extras.focusY = focusPoint.y
+    }
+    extras.callerContext = callerContext
+    extras.mainUri = mainUri
+    extras.datasourceExtras = dataSourceExtras
+    extras.imageExtras = imageExtras
+    extras.shortcutExtras = shortcutAttribution
+    extras.componentExtras = componentAttribution
+    return extras
+  }
+}
