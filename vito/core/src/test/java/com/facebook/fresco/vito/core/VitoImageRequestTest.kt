01d0930b3ac4347c43726551c979afec8b550e6b
Sachin Tewari <stew@fb.com>
2022-09-14 06:54:55 -0700

    Migrate VitoImageRequestTest.java to Kotlin
    Reviewed By: oprisnik
    
    Differential Revision: D39469910
    
    fbshipit-source-id: 443c96a6e95a0341ea9acb6c62aaa503c19a3f6d

---
+++ vito/core/src/test/java/com/facebook/fresco/vito/core/VitoImageRequestTest.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.fresco.vito.core
+
+import android.content.res.Resources
+import com.facebook.fresco.vito.options.ImageOptions
+import com.facebook.fresco.vito.source.ImageSource
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.kotlin.mock
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class VitoImageRequestTest {
+
+  @Test
+  fun testEquals() {
+    val res = mock<Resources>()
+    val imageSource = mock<ImageSource>()
+    val options = mock<ImageOptions>()
+    val a = VitoImageRequest(res, imageSource, options, null, null)
+    val b = VitoImageRequest(res, imageSource, options, null, null)
+
+    assertThat(a).isEqualTo(b)
+  }
+
+  @Test
+  fun testHashCode() {
+    val res = mock<Resources>()
+    val imageSource = mock<ImageSource>()
+    val options = mock<ImageOptions>()
+    val a = VitoImageRequest(res, imageSource, options, null, null)
+    val b = VitoImageRequest(res, imageSource, options, null, null)
+
+    assertThat(a.hashCode()).isEqualTo(b.hashCode())
+  }
+}
