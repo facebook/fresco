eacdb2f359266aa5ca478e660a3970b4ba92b33a
Artem Kholodnyi <defhlt@fb.com>
2022-09-26 08:00:50 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ProducerConstants.java
    Reviewed By: oprisnik
    
    Differential Revision: D39767835
    
    fbshipit-source-id: ff6a17648c3b83b78b533aa99fa9891a9564ae2e

---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ProducerConstants.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.producers
+
+/**
+ * Constants to be used various [Producer]s for logging purposes in the extra maps for the
+ * [com.facebook.imagepipeline.listener.RequestListener].
+ *
+ * The elements are package visible on purpose such that the individual producers create public
+ * constants of the ones that they actually use.
+ */
+internal object ProducerConstants {
+  const val EXTRA_CACHED_VALUE_FOUND = "cached_value_found"
+  const val EXTRA_BITMAP_SIZE = "bitmapSize"
+  const val EXTRA_HAS_GOOD_QUALITY = "hasGoodQuality"
+  const val EXTRA_IMAGE_TYPE = "imageType"
+  const val EXTRA_IS_FINAL = "isFinal"
+  const val EXTRA_IMAGE_FORMAT_NAME = "imageFormat"
+  const val EXTRA_BYTES = "byteCount"
+  const val ENCODED_IMAGE_SIZE = "encodedImageSize"
+  const val REQUESTED_IMAGE_SIZE = "requestedImageSize"
+  const val SAMPLE_SIZE = "sampleSize"
+  const val NON_FATAL_DECODE_ERROR = "non_fatal_decode_error"
+}
