715cc6ff476990f6c5886f0d9d77e300bf0c9557
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2022-09-29 13:51:18 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/producers/LocalAssetFetchProducer.java
    Reviewed By: oprisnik
    
    Differential Revision: D39884132
    
    fbshipit-source-id: 3f52c3bed423075437753f1be5642c42b22d1d74

---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/LocalAssetFetchProducer.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.producers
+
+import android.content.res.AssetFileDescriptor
+import android.content.res.AssetManager
+import com.facebook.common.memory.PooledByteBufferFactory
+import com.facebook.imagepipeline.image.EncodedImage
+import com.facebook.imagepipeline.request.ImageRequest
+import java.io.IOException
+import java.util.concurrent.Executor
+
+/** Executes a local fetch from an asset. */
+class LocalAssetFetchProducer(
+    executor: Executor,
+    pooledByteBufferFactory: PooledByteBufferFactory,
+    private val assetManager: AssetManager
+) : LocalFetchProducer(executor, pooledByteBufferFactory) {
+
+  @Throws(IOException::class)
+  override fun getEncodedImage(imageRequest: ImageRequest): EncodedImage? =
+      getEncodedImage(
+          assetManager.open(getAssetName(imageRequest), AssetManager.ACCESS_STREAMING),
+          getLength(imageRequest))
+
+  private fun getLength(imageRequest: ImageRequest): Int {
+    var fd: AssetFileDescriptor? = null
+    return try {
+      fd = assetManager.openFd(getAssetName(imageRequest))
+      fd.length.toInt()
+    } catch (e: IOException) {
+      -1
+    } finally {
+      try {
+        fd?.close()
+      } catch (ignored: IOException) {
+        // There's nothing we can do with the exception when closing descriptor.
+      }
+    }
+  }
+
+  override fun getProducerName(): String = PRODUCER_NAME
+
+  companion object {
+    const val PRODUCER_NAME = "LocalAssetFetchProducer"
+    private fun getAssetName(imageRequest: ImageRequest): String =
+        imageRequest.sourceUri.path!!.substring(1)
+  }
+}
