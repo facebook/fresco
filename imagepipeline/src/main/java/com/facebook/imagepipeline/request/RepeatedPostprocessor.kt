4c98d05a8defc7b7af48ef9290f8cfcea7a0b053
Alexander Oprisnik <oprisnik@fb.com>
2022-10-07 09:12:29 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/request/RepeatedPostprocessor.java
    Reviewed By: IanChilds
    
    Differential Revision: D39881705
    
    fbshipit-source-id: 01269db335b7df9f8b5fceff031e499dc7aef4fc

--- imagepipeline/src/main/java/com/facebook/imagepipeline/request/RepeatedPostprocessor.kt
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/request/RepeatedPostprocessor.kt
@@ -5,19 +5,18 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.imagepipeline.request;
+package com.facebook.imagepipeline.request
 
 /**
  * Use an instance of this interface to perform post-process operations that must be performed more
  * than once.
  */
-public interface RepeatedPostprocessor extends Postprocessor {
-
+interface RepeatedPostprocessor : Postprocessor {
   /**
    * Callback used to pass the postprocessor a reference to the object that will run the
-   * postprocessor's {@code PostProcessor#process} method when the client requires.
+   * postprocessor's `PostProcessor#process` method when the client requires.
    *
    * @param runner
    */
-  void setCallback(RepeatedPostprocessorRunner runner);
+  fun setCallback(runner: RepeatedPostprocessorRunner)
 }
