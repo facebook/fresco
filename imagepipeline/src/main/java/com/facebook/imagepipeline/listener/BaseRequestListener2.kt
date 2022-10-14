68440220fb3fdf9b496c017f98b2b32365b82ff5
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2022-10-14 16:10:36 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/listener/BaseRequestListener2.java
    Reviewed By: oprisnik
    
    Differential Revision: D40298730
    
    fbshipit-source-id: 6d9db6e52edb8207868d572fa131d13e881a7999

---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/listener/BaseRequestListener2.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.listener
+
+import com.facebook.imagepipeline.producers.ProducerContext
+
+class BaseRequestListener2 : RequestListener2 {
+
+  override fun onRequestStart(producerContext: ProducerContext) = Unit
+
+  override fun onRequestSuccess(producerContext: ProducerContext) = Unit
+
+  override fun onRequestFailure(producerContext: ProducerContext, throwable: Throwable?) = Unit
+
+  override fun onRequestCancellation(producerContext: ProducerContext) = Unit
+
+  override fun onProducerStart(producerContext: ProducerContext, producerName: String) = Unit
+
+  override fun onProducerEvent(
+      producerContext: ProducerContext,
+      producerName: String,
+      eventName: String
+  ) = Unit
+
+  override fun onProducerFinishWithSuccess(
+      producerContext: ProducerContext,
+      producerName: String,
+      extraMap: Map<String, String>?
+  ) = Unit
+
+  override fun onProducerFinishWithFailure(
+      producerContext: ProducerContext,
+      producerName: String,
+      t: Throwable?,
+      extraMap: Map<String, String>?
+  ) = Unit
+
+  override fun onProducerFinishWithCancellation(
+      producerContext: ProducerContext,
+      producerName: String,
+      extraMap: Map<String, String>?
+  ) = Unit
+
+  override fun onUltimateProducerReached(
+      producerContext: ProducerContext,
+      producerName: String,
+      successful: Boolean
+  ) = Unit
+
+  override fun requiresExtraMap(producerContext: ProducerContext, producerName: String): Boolean =
+      false
+}
