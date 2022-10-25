e0bd8fc1b387881ab217d30910996d2cbe941112
Alexander Oprisnik <oprisnik@meta.com>
2022-10-25 08:29:29 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/platform/PlatformDecoder.java
    Reviewed By: bnalls33
    
    Differential Revision: D40301264
    
    fbshipit-source-id: 6a64cdcdfed13abf0f1a9b40d8250a1f91dfc0a9

--- imagepipeline/src/main/java/com/facebook/imagepipeline/platform/PlatformDecoder.kt
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/platform/PlatformDecoder.kt
@@ -5,81 +5,81 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.imagepipeline.platform;
+package com.facebook.imagepipeline.platform
 
-import android.graphics.Bitmap;
-import android.graphics.ColorSpace;
-import android.graphics.Rect;
-import com.facebook.common.references.CloseableReference;
-import com.facebook.imagepipeline.image.EncodedImage;
-import com.facebook.infer.annotation.Nullsafe;
-import javax.annotation.Nullable;
+import android.graphics.Bitmap
+import android.graphics.ColorSpace
+import android.graphics.Rect
+import com.facebook.common.references.CloseableReference
+import com.facebook.imagepipeline.image.EncodedImage
 
-@Nullsafe(Nullsafe.Mode.STRICT)
-public interface PlatformDecoder {
+interface PlatformDecoder {
+
   /**
-   * Creates a bitmap from encoded bytes. Supports JPEG but callers should use {@link
-   * #decodeJPEGFromEncodedImage} for partial JPEGs. In addition, a region to decode can be supplied
+   * Creates a bitmap from encoded bytes. Supports JPEG but callers should use
+   * [decodeJPEGFromEncodedImage] for partial JPEGs. In addition, a region to decode can be supplied
    * in order to minimize memory usage. NOTE: Not all platform decoders necessarily support
    * supplying specific regions.
    *
-   * <p>Note: This needs to be kept because of dependencies issues.
+   * Note: This needs to be kept because of dependencies issues.
    *
    * @param encodedImage the reference to the encoded image with the reference to the encoded bytes
-   * @param bitmapConfig the {@link android.graphics.Bitmap.Config} used to create the decoded
-   *     Bitmap
+   * @param bitmapConfig the [android.graphics.Bitmap.Config] used to create the decoded Bitmap
    * @param regionToDecode optional image region to decode or null to decode the whole image
    * @return the bitmap
    * @throws TooManyBitmapsException if the pool is full
    * @throws java.lang.OutOfMemoryError if the Bitmap cannot be allocated
    */
-  CloseableReference<Bitmap> decodeFromEncodedImage(
-      final EncodedImage encodedImage, Bitmap.Config bitmapConfig, @Nullable Rect regionToDecode);
+  fun decodeFromEncodedImage(
+      encodedImage: EncodedImage,
+      bitmapConfig: Bitmap.Config,
+      regionToDecode: Rect?
+  ): CloseableReference<Bitmap>
 
   /**
    * Creates a bitmap from encoded JPEG bytes. Supports a partial JPEG image. In addition, a region
    * to decode can be supplied in order to minimize memory usage. NOTE: Not all platform decoders
    * necessarily support supplying specific regions.
    *
-   * <p>Note: This needs to be kept because of dependencies issues.
+   * Note: This needs to be kept because of dependencies issues.
    *
    * @param encodedImage the reference to the encoded image with the reference to the encoded bytes
-   * @param bitmapConfig the {@link android.graphics.Bitmap.Config} used to create the decoded
-   *     Bitmap
+   * @param bitmapConfig the [android.graphics.Bitmap.Config] used to create the decoded Bitmap
    * @param regionToDecode optional image region to decode or null to decode the whole image.
    * @param length the number of encoded bytes in the buffer
    * @return the bitmap
    * @throws TooManyBitmapsException if the pool is full
    * @throws java.lang.OutOfMemoryError if the Bitmap cannot be allocated
    */
-  CloseableReference<Bitmap> decodeJPEGFromEncodedImage(
-      EncodedImage encodedImage,
-      Bitmap.Config bitmapConfig,
-      @Nullable Rect regionToDecode,
-      int length);
+  fun decodeJPEGFromEncodedImage(
+      encodedImage: EncodedImage,
+      bitmapConfig: Bitmap.Config,
+      regionToDecode: Rect?,
+      length: Int
+  ): CloseableReference<Bitmap>
 
   /**
-   * Creates a bitmap from encoded bytes. Supports JPEG but callers should use {@link
-   * #decodeJPEGFromEncodedImage} for partial JPEGs. In addition, a region to decode can be supplied
+   * Creates a bitmap from encoded bytes. Supports JPEG but callers should use
+   * [decodeJPEGFromEncodedImage] for partial JPEGs. In addition, a region to decode can be supplied
    * in order to minimize memory usage. NOTE: Not all platform decoders necessarily support
    * supplying specific regions.
    *
    * @param encodedImage the reference to the encoded image with the reference to the encoded bytes
-   * @param bitmapConfig the {@link android.graphics.Bitmap.Config} used to create the decoded
-   *     Bitmap
+   * @param bitmapConfig the [android.graphics.Bitmap.Config] used to create the decoded Bitmap
    * @param regionToDecode optional image region to decode or null to decode the whole image
    * @param colorSpace the target color space of the decoded bitmap, must be one of the named color
-   *     space in {@link android.graphics.ColorSpace.Named}. If null, then SRGB color space is
-   *     assumed if the SDK version >= 26.
+   * space in [android.graphics.ColorSpace.Named]. If null, then SRGB color space is assumed if the
+   * SDK version >= 26.
    * @return the bitmap
    * @throws TooManyBitmapsException if the pool is full
    * @throws java.lang.OutOfMemoryError if the Bitmap cannot be allocated
    */
-  CloseableReference<Bitmap> decodeFromEncodedImageWithColorSpace(
-      final EncodedImage encodedImage,
-      Bitmap.Config bitmapConfig,
-      @Nullable Rect regionToDecode,
-      @Nullable final ColorSpace colorSpace);
+  fun decodeFromEncodedImageWithColorSpace(
+      encodedImage: EncodedImage,
+      bitmapConfig: Bitmap.Config,
+      regionToDecode: Rect?,
+      colorSpace: ColorSpace?
+  ): CloseableReference<Bitmap>
 
   /**
    * Creates a bitmap from encoded JPEG bytes. Supports a partial JPEG image. In addition, a region
@@ -87,21 +87,21 @@ public interface PlatformDecoder {
    * necessarily support supplying specific regions.
    *
    * @param encodedImage the reference to the encoded image with the reference to the encoded bytes
-   * @param bitmapConfig the {@link android.graphics.Bitmap.Config} used to create the decoded
-   *     Bitmap
+   * @param bitmapConfig the [android.graphics.Bitmap.Config] used to create the decoded Bitmap
    * @param regionToDecode optional image region to decode or null to decode the whole image.
    * @param length the number of encoded bytes in the buffer
    * @param colorSpace the target color space of the decoded bitmap, must be one of the named color
-   *     space in {@link android.graphics.ColorSpace.Named}. If null, then SRGB color space is
-   *     assumed if the SDK version >= 26.
+   * space in [android.graphics.ColorSpace.Named]. If null, then SRGB color space is assumed if the
+   * SDK version >= 26.
    * @return the bitmap
    * @throws TooManyBitmapsException if the pool is full
    * @throws java.lang.OutOfMemoryError if the Bitmap cannot be allocated
    */
-  CloseableReference<Bitmap> decodeJPEGFromEncodedImageWithColorSpace(
-      EncodedImage encodedImage,
-      Bitmap.Config bitmapConfig,
-      @Nullable Rect regionToDecode,
-      int length,
-      @Nullable final ColorSpace colorSpace);
+  fun decodeJPEGFromEncodedImageWithColorSpace(
+      encodedImage: EncodedImage,
+      bitmapConfig: Bitmap.Config,
+      regionToDecode: Rect?,
+      length: Int,
+      colorSpace: ColorSpace?
+  ): CloseableReference<Bitmap>
 }
