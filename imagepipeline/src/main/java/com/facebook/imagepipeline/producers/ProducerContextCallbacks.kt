b5d65d5ee378b818baf54e00fea04359f0162529
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2022-10-25 08:48:27 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ProducerContextCallbacks.java
    Reviewed By: oprisnik
    
    Differential Revision: D40584665
    
    fbshipit-source-id: 931726f732fbf6dff6e301b03b89b17ccbca5529

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ProducerContextCallbacks.kt
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ProducerContextCallbacks.kt
@@ -5,20 +5,20 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.imagepipeline.producers;
+package com.facebook.imagepipeline.producers
 
 /** Callbacks that are called when something changes in a request sequence. */
-public interface ProducerContextCallbacks {
+interface ProducerContextCallbacks {
 
   /** Method that is called when a client cancels the request. */
-  void onCancellationRequested();
+  fun onCancellationRequested()
 
   /** Method that is called when a request is no longer a prefetch, or vice versa. */
-  void onIsPrefetchChanged();
+  fun onIsPrefetchChanged()
 
   /** Method that is called when intermediate results start or stop being expected. */
-  void onIsIntermediateResultExpectedChanged();
+  fun onIsIntermediateResultExpectedChanged()
 
   /** Method that is called when the priority of the request changes. */
-  void onPriorityChanged();
+  fun onPriorityChanged()
 }
