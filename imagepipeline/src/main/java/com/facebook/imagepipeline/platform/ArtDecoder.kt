712474b1be6d758167280af95bd5a6401e7d515f
Alexander Oprisnik <oprisnik@meta.com>
2022-10-25 08:24:16 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/platform/ArtDecoder.java
    Reviewed By: strulovich
    
    Differential Revision: D40430982
    
    fbshipit-source-id: b67babda06146228ef95a39341134df9a7470b88

---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/platform/ArtDecoder.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.platform
+
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.graphics.BitmapFactory.Options
+import android.os.Build
+import androidx.core.util.Pools
+import com.facebook.imagepipeline.memory.BitmapPool
+import com.facebook.imageutils.BitmapUtil
+import java.nio.ByteBuffer
+import javax.annotation.concurrent.ThreadSafe
+
+/** Bitmap decoder for ART VM (Lollipop and up). */
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+@ThreadSafe
+class ArtDecoder(bitmapPool: BitmapPool, decodeBuffers: Pools.Pool<ByteBuffer>) :
+    DefaultDecoder(bitmapPool, decodeBuffers) {
+  override fun getBitmapSize(width: Int, height: Int, options: Options): Int {
+    @SuppressLint("RestrictedApi") val c = checkNotNull(options.inPreferredConfig)
+    return BitmapUtil.getSizeInByteForBitmap(width, height, c)
+  }
+}
