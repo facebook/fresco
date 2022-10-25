36ba463cb41fe52b0f1fd97e147765bd8f1bd567
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2022-10-25 08:29:26 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ThumbnailProducer.java
    Reviewed By: oprisnik
    
    Differential Revision: D40590220
    
    fbshipit-source-id: f22d7717dba45504c8cd7d066749c990b842aa87

--- imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ThumbnailProducer.kt
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/producers/ThumbnailProducer.kt
@@ -5,21 +5,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.imagepipeline.producers;
+package com.facebook.imagepipeline.producers
 
-import com.facebook.imagepipeline.common.ResizeOptions;
-import javax.annotation.Nullable;
+import com.facebook.imagepipeline.common.ResizeOptions
 
 /**
  * Implemented producers can be queried for whether they are likely to be able to produce a result
  * of the desired size.
  *
- * <p>{@link #produceResults(Consumer, ProducerContext)} may send a null image to the consumer, even
- * if an image is available, if the ultimate image is smaller than wanted. This may happen even if
- * the producer thought it would be able to satisfy the request.
+ * [produceResults(Consumer, ProducerContext)] may send a null image to the consumer, even if an
+ * image is available, if the ultimate image is smaller than wanted. This may happen even if the
+ * producer thought it would be able to satisfy the request.
  */
-public interface ThumbnailProducer<T> extends Producer<T> {
-
+interface ThumbnailProducer<T> : Producer<T> {
   /**
    * Checks whether the producer may be able to produce images of the specified size. This makes no
    * promise about being able to produce images for a particular source, only generally being able
@@ -28,5 +26,5 @@ public interface ThumbnailProducer<T> extends Producer<T> {
    * @param resizeOptions the resize options from the current request
    * @return true if the producer can meet these needs
    */
-  boolean canProvideImageForSize(@Nullable ResizeOptions resizeOptions);
+  fun canProvideImageForSize(resizeOptions: ResizeOptions?): Boolean
 }
