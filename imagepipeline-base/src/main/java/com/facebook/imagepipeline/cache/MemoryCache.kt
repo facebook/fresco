dbfd19466c791c214b9ff10c52765cfa7c67ba63
Alexander Oprisnik <oprisnik@meta.com>
2022-10-27 08:32:31 -0700

    fbandroid/libraries/fresco/imagepipeline-base/src/main/java/com/facebook/imagepipeline/cache/MemoryCache.java
    Reviewed By: defHLT
    
    Differential Revision: D39922209
    
    fbshipit-source-id: b054072cce7e7dd5f463cbf40911913de752b9ac

--- imagepipeline-base/src/main/java/com/facebook/imagepipeline/cache/MemoryCache.kt
+++ imagepipeline-base/src/main/java/com/facebook/imagepipeline/cache/MemoryCache.kt
@@ -5,44 +5,40 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.imagepipeline.cache;
+package com.facebook.imagepipeline.cache
 
-import com.facebook.cache.common.HasDebugData;
-import com.facebook.common.internal.Predicate;
-import com.facebook.common.memory.MemoryTrimType;
-import com.facebook.common.memory.MemoryTrimmable;
-import com.facebook.common.references.CloseableReference;
-import com.facebook.infer.annotation.Nullsafe;
-import javax.annotation.Nullable;
+import com.facebook.cache.common.HasDebugData
+import com.facebook.common.internal.Predicate
+import com.facebook.common.memory.MemoryTrimType
+import com.facebook.common.memory.MemoryTrimmable
+import com.facebook.common.references.CloseableReference
 
 /**
  * Interface for the image pipeline memory cache.
  *
  * @param <K> the key type
- * @param <V> the value type
+ * @param <V> the value type </V></K>
  */
-@Nullsafe(Nullsafe.Mode.STRICT)
-public interface MemoryCache<K, V> extends MemoryTrimmable, HasDebugData {
+interface MemoryCache<K, V> : MemoryTrimmable, HasDebugData {
 
   /** Interface used to specify the trimming strategy for the cache. */
-  interface CacheTrimStrategy {
-    double getTrimRatio(MemoryTrimType trimType);
+  fun interface CacheTrimStrategy {
+    fun getTrimRatio(trimType: MemoryTrimType): Double
   }
 
   /**
    * Caches the the given key-value pair.
    *
-   * <p>The cache returns a new copy of the provided reference which should be used instead of the
+   * The cache returns a new copy of the provided reference which should be used instead of the
    * original one. The client should close the returned reference when it is not required anymore.
    *
-   * <p>If the cache failed to cache the given value, then the null reference is returned.
+   * If the cache failed to cache the given value, then the null reference is returned.
    *
    * @param key
    * @param value
    * @return a new reference to be used, or null if the caching failed
    */
-  @Nullable
-  CloseableReference<V> cache(K key, CloseableReference<V> value);
+  fun cache(key: K, value: CloseableReference<V>): CloseableReference<V>?
 
   /**
    * Gets the item with the given key, or null if there is no such item.
@@ -50,18 +46,16 @@ public interface MemoryCache<K, V> extends MemoryTrimmable, HasDebugData {
    * @param key
    * @return a reference to the cached value, or null if the item was not found
    */
-  @Nullable
-  CloseableReference<V> get(K key);
+  operator fun get(key: K): CloseableReference<V>?
 
   /**
    * Gets the item with the given key for debug purposes. For instance, for LRU caches this will not
-   * change the LRU order. Use {@link #get(K)} instead.
+   * change the LRU order. Use [get(K)] instead.
    *
    * @param key
    * @return a cached value or null if the item was not found
    */
-  @Nullable
-  V inspect(K key);
+  fun inspect(key: K): V?
 
   /**
    * Probes whether the object corresponding to the key is in the cache. Note that the act of
@@ -69,7 +63,7 @@ public interface MemoryCache<K, V> extends MemoryTrimmable, HasDebugData {
    *
    * @param key
    */
-  void probe(K key);
+  fun probe(key: K)
 
   /**
    * Removes all the items from the cache whose keys match the specified predicate.
@@ -77,7 +71,7 @@ public interface MemoryCache<K, V> extends MemoryTrimmable, HasDebugData {
    * @param predicate returns true if an item with the given key should be removed
    * @return number of the items removed from the cache
    */
-  int removeAll(Predicate<K> predicate);
+  fun removeAll(predicate: Predicate<K>): Int
 
   /**
    * Find if any of the items from the cache whose keys match the specified predicate.
@@ -85,7 +79,7 @@ public interface MemoryCache<K, V> extends MemoryTrimmable, HasDebugData {
    * @param predicate returns true if an item with the given key matches
    * @return true if the predicate was found in the cache, false otherwise
    */
-  boolean contains(Predicate<K> predicate);
+  operator fun contains(predicate: Predicate<K>): Boolean
 
   /**
    * Check if the cache contains an item for the given key.
@@ -93,11 +87,11 @@ public interface MemoryCache<K, V> extends MemoryTrimmable, HasDebugData {
    * @param key
    * @return true if the key was found in the cache, false otherwise
    */
-  boolean contains(K key);
+  operator fun contains(key: K): Boolean
 
   /** Gets the total number of all currently cached items. */
-  int getCount();
+  val count: Int
 
   /** Gets the total size in bytes of all currently cached items. */
-  int getSizeInBytes();
+  val sizeInBytes: Int
 }
