dec007a8c11caf45b0479b54fb6712eb6de8fb1f
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2022-10-31 14:04:52 -0700

    fbandroid/libraries/fresco/imagepipeline/src/main/java/com/facebook/imagepipeline/platform/PlatformDecoderFactory.java
    Reviewed By: strulovich
    
    Differential Revision: D40798830
    
    fbshipit-source-id: 80a000b15d94fec3c812b4070babe8c60feed078

---
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/platform/PlatformDecoderFactory.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.platform
+
+import android.os.Build
+import androidx.core.util.Pools
+import androidx.core.util.Pools.SynchronizedPool
+import com.facebook.common.memory.DecodeBufferHelper
+import com.facebook.imagepipeline.core.NativeCodeSetup
+import com.facebook.imagepipeline.memory.FlexByteArrayPool
+import com.facebook.imagepipeline.memory.PoolFactory
+import java.lang.reflect.InvocationTargetException
+import java.nio.ByteBuffer
+
+object PlatformDecoderFactory {
+
+  /**
+   * Provide the implementation of the PlatformDecoder for the current platform using the provided
+   * PoolFactory
+   *
+   * @param poolFactory The PoolFactory
+   * @return The PlatformDecoder implementation
+   */
+  @JvmStatic
+  @JvmOverloads
+  fun buildPlatformDecoder(
+      poolFactory: PoolFactory,
+      gingerbreadDecoderEnabled: Boolean,
+      useDecodeBufferHelper: Boolean = false
+  ): PlatformDecoder =
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        OreoDecoder(poolFactory.bitmapPool, createPool(poolFactory, useDecodeBufferHelper))
+      } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ||
+          !NativeCodeSetup.getUseNativeCode()) {
+        ArtDecoder(poolFactory.bitmapPool, createPool(poolFactory, useDecodeBufferHelper))
+      } else {
+        try {
+          if (gingerbreadDecoderEnabled && Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            val clazz =
+                Class.forName("com.facebook.imagepipeline.platform.GingerbreadPurgeableDecoder")
+            clazz.getConstructor().newInstance() as PlatformDecoder
+          } else {
+            val clazz = Class.forName("com.facebook.imagepipeline.platform.KitKatPurgeableDecoder")
+            clazz
+                .getConstructor(FlexByteArrayPool::class.java)
+                .newInstance(poolFactory.flexByteArrayPool) as PlatformDecoder
+          }
+        } catch (e: ClassNotFoundException) {
+          throw RuntimeException("Wrong Native code setup, reflection failed.", e)
+        } catch (e: IllegalAccessException) {
+          throw RuntimeException("Wrong Native code setup, reflection failed.", e)
+        } catch (e: NoSuchMethodException) {
+          throw RuntimeException("Wrong Native code setup, reflection failed.", e)
+        } catch (e: InvocationTargetException) {
+          throw RuntimeException("Wrong Native code setup, reflection failed.", e)
+        } catch (e: InstantiationException) {
+          throw RuntimeException("Wrong Native code setup, reflection failed.", e)
+        }
+      }
+
+  @JvmStatic
+  fun createPool(poolFactory: PoolFactory, useDecodeBufferHelper: Boolean): Pools.Pool<ByteBuffer> {
+    if (useDecodeBufferHelper) {
+      return DecodeBufferHelper.INSTANCE
+    }
+    val maxNumThreads = poolFactory.flexByteArrayPoolMaxNumThreads
+    val pool: Pools.Pool<ByteBuffer> = SynchronizedPool(maxNumThreads)
+    for (i in 0 until maxNumThreads) {
+      pool.release(ByteBuffer.allocate(DecodeBufferHelper.getRecommendedDecodeBufferSize()))
+    }
+    return pool
+  }
+}
