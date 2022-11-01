1439d7a22d6dd339064a32406e7bd309a96ec6ab
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2022-11-01 13:06:09 -0700

    fbandroid/libraries/fresco/imagepipeline-base/src/main/java/com/facebook/imagepipeline/animated/factory/AnimatedFactoryProvider.java
    Reviewed By: strulovich
    
    Differential Revision: D40893373
    
    fbshipit-source-id: f60a745b0cd5268caca731e2cb4090842db9c78e

---
+++ imagepipeline-base/src/main/java/com/facebook/imagepipeline/animated/factory/AnimatedFactoryProvider.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.imagepipeline.animated.factory
+
+import com.facebook.cache.common.CacheKey
+import com.facebook.common.executors.SerialExecutorService
+import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory
+import com.facebook.imagepipeline.cache.CountingMemoryCache
+import com.facebook.imagepipeline.core.ExecutorSupplier
+import com.facebook.imagepipeline.image.CloseableImage
+import java.util.concurrent.ExecutorService
+
+object AnimatedFactoryProvider {
+  private var implLoaded = false
+  private var impl: AnimatedFactory? = null
+
+  @JvmStatic
+  fun getAnimatedFactory(
+      platformBitmapFactory: PlatformBitmapFactory?,
+      executorSupplier: ExecutorSupplier?,
+      backingCache: CountingMemoryCache<CacheKey?, CloseableImage?>?,
+      downscaleFrameToDrawableDimensions: Boolean,
+      serialExecutorService: ExecutorService?
+  ): AnimatedFactory? {
+    if (!implLoaded) {
+      try {
+        val clazz = Class.forName("com.facebook.fresco.animation.factory.AnimatedFactoryV2Impl")
+        val constructor =
+            clazz.getConstructor(
+                PlatformBitmapFactory::class.java,
+                ExecutorSupplier::class.java,
+                CountingMemoryCache::class.java,
+                java.lang.Boolean.TYPE,
+                SerialExecutorService::class.java)
+        impl =
+            constructor.newInstance(
+                platformBitmapFactory,
+                executorSupplier,
+                backingCache,
+                downscaleFrameToDrawableDimensions,
+                serialExecutorService) as AnimatedFactory
+      } catch (e: Throwable) {
+        // Head in the sand
+      }
+      if (impl != null) {
+        implLoaded = true
+      }
+    }
+    return impl
+  }
+}
