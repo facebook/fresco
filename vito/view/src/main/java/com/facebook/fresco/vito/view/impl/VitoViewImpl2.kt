bbbf774ea3a1a32acaf3dcb40194f9d8f49e0d28
Alexander Oprisnik <oprisnik@meta.com>
2022-12-01 05:00:06 -0800

    Annotate Vito View config values with JvmField
    Reviewed By: strulovich
    
    Differential Revision: D41560254
    
    fbshipit-source-id: 2921a4113f096915ac929acc813dfab558bb630e

--- vito/view/src/main/java/com/facebook/fresco/vito/view/impl/VitoViewImpl2.kt
+++ vito/view/src/main/java/com/facebook/fresco/vito/view/impl/VitoViewImpl2.kt
@@ -25,8 +25,8 @@ import com.facebook.fresco.vito.source.ImageSource
 
 /** Vito View implementation */
 object VitoViewImpl2 {
-  @JvmStatic var useVisibilityCallbacks: Supplier<Boolean> = Suppliers.BOOLEAN_TRUE
-  @JvmStatic var useSimpleFetchLogic: Supplier<Boolean> = Suppliers.BOOLEAN_FALSE
+  @JvmField var useVisibilityCallbacks: Supplier<Boolean> = Suppliers.BOOLEAN_TRUE
+  @JvmField var useSimpleFetchLogic: Supplier<Boolean> = Suppliers.BOOLEAN_FALSE
 
   private val onAttachStateChangeListenerCallback: OnAttachStateChangeListener =
       object : OnAttachStateChangeListener {
