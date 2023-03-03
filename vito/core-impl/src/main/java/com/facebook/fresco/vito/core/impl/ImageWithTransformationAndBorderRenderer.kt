db108c0721adf47bb406bae1b5ca14284455f87d
Neha Gupta <nehgupta12@meta.com>
2023-03-03 09:04:19 -0800

    Vito Kotlin border padding not working
    Reviewed By: oprisnik
    
    Differential Revision: D43742038
    
    fbshipit-source-id: 2c7b1ffcb48f32cfeb0fbedebc0dfcf655a5cbdf

--- vito/core-impl/src/main/java/com/facebook/fresco/vito/core/impl/ImageWithTransformationAndBorderRenderer.kt
+++ vito/core-impl/src/main/java/com/facebook/fresco/vito/core/impl/ImageWithTransformationAndBorderRenderer.kt
@@ -29,16 +29,23 @@ class ImageWithTransformationAndBorderRenderer {
         paint: Paint,
         alpha: Int = 255
     ): RenderCommand {
+      val borderPadding: Float = borderOptions?.padding ?: 0f
       val borderWidth: Float = borderOptions?.width ?: 0f
       val hasBorders: Boolean = borderWidth > 0f
       val scaleDownInsideBorders = borderOptions?.scaleDownInsideBorders ?: false
-
       val layerBounds = RectF(bounds)
 
       val imageTransform: Matrix?
       val imageShape: Shape
-      if (hasBorders && scaleDownInsideBorders) {
-        val insideBorderBounds = RectF(layerBounds).apply { inset(borderWidth, borderWidth) }
+      if (hasBorders && (scaleDownInsideBorders || borderPadding != 0f)) {
+        val insideBorderBounds =
+            if (scaleDownInsideBorders) {
+              RectF(layerBounds).apply {
+                inset(borderWidth + borderPadding, borderWidth + borderPadding)
+              }
+            } else {
+              RectF(layerBounds).apply { inset(borderWidth, borderWidth) }
+            }
         val insideBorderTransform =
             Matrix().apply {
               setRectToRect(layerBounds, insideBorderBounds, Matrix.ScaleToFit.FILL)
