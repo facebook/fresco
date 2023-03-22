e8b27cb1420045eb27783fe42a46df3278aa6ece
generatedunixname89002005287564 <generatedunixname89002005287564@fb.com>
2023-03-22 10:39:51 -0700

    convert_fest_assertions: //fbandroid/libraries/fresco/drawee-span/src/test/java/com/facebook/drawee/span:span
    Reviewed By: jselbo
    
    Differential Revision: D44294517
    
    fbshipit-source-id: 4b0408d42923ac823ffa9348a488be9b23024df3

--- drawee-span/src/test/java/com/facebook/drawee/span/DraweeSpanTest.java
+++ drawee-span/src/test/java/com/facebook/drawee/span/DraweeSpanTest.java
@@ -7,7 +7,7 @@
 
 package com.facebook.drawee.span;
 
-import static org.fest.assertions.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
