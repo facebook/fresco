f2189db656eab3d7cb42dfe5ce46315abadbd2fc
Artem Kholodnyi <defhlt@meta.com>
2023-06-09 12:31:00 -0700

    Convert ImageMetaData
    Reviewed By: oprisnik
    
    Differential Revision: D46479149
    
    fbshipit-source-id: b52220e8443d3fda93d0bf92bcd81209ad6eb579

---
+++ imagepipeline-base/src/main/java/com/facebook/imageutils/ImageMetaData.kt
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package com.facebook.imageutils
+
+import android.graphics.ColorSpace
+import android.util.Pair
+
+/** Wrapper class representing the recovered meta data of an image when decoding. */
+class ImageMetaData(width: Int, height: Int, colorSpace: ColorSpace?) {
+
+  val dimensions: Pair<Int, Int>? = if (width == -1 || height == -1) null else Pair(width, height)
+  val colorSpace: ColorSpace?
+
+  init {
+
+    this.colorSpace = colorSpace
+  }
+}
