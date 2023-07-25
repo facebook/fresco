8e3789d29f8cd1ce00863a1227f751704410eb23
Alexander Oprisnik <oprisnik@meta.com>
2023-07-25 08:00:43 -0700

    Fix RoundingOptions equality checks by manually implementing equals and hashCode
    Reviewed By: steelrooter
    
    Differential Revision: D47754908
    
    fbshipit-source-id: bbaae97791baa08da476c378049ae2f23f199c1b

--- vito/options/src/main/java/com/facebook/fresco/vito/options/RoundingOptions.kt
+++ vito/options/src/main/java/com/facebook/fresco/vito/options/RoundingOptions.kt
@@ -7,6 +7,8 @@
 
 package com.facebook.fresco.vito.options
 
+import java.util.Arrays
+
 data class RoundingOptions(
     val isCircular: Boolean,
     val cornerRadius: Float,
@@ -16,10 +18,41 @@ data class RoundingOptions(
 ) {
   fun hasRoundedCorners(): Boolean = cornerRadius != CORNER_RADIUS_UNSET || cornerRadii != null
 
+  override fun equals(other: Any?): Boolean {
+    if (!fixEqualityChecks) {
+      return super.equals(other)
+    }
+
+    if (this === other) {
+      return true
+    }
+    if (other == null || javaClass != other.javaClass) return false
+    other as RoundingOptions
+    return isCircular == other.isCircular &&
+        cornerRadius == other.cornerRadius &&
+        cornerRadii.contentEquals(other.cornerRadii) &&
+        isAntiAliased == other.isAntiAliased &&
+        isForceRoundAtDecode == other.isForceRoundAtDecode
+  }
+
+  override fun hashCode(): Int {
+    if (!fixEqualityChecks) {
+      return super.hashCode()
+    }
+    var result = if (isCircular) 1 else 0
+    result = 31 * result + cornerRadius.hashCode()
+    result = 31 * result + Arrays.hashCode(cornerRadii)
+    result = 31 * result + if (isAntiAliased) 1 else 0
+    result = 31 * result + if (isForceRoundAtDecode) 1 else 0
+    return result
+  }
+
   companion object {
     const val CORNER_RADIUS_UNSET = 0f
     val AS_CIRCLE = RoundingOptions(true, CORNER_RADIUS_UNSET, null, false, false)
     val AS_CIRCLE_ANTI_ALIASING = RoundingOptions(true, CORNER_RADIUS_UNSET, null, true, false)
+
+    var fixEqualityChecks: Boolean = true
 
     @JvmStatic fun asCircle(): RoundingOptions = AS_CIRCLE
 
