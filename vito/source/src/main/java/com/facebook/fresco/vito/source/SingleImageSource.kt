18fe11a7d0d2099c5dd2a3703dfab30df398cc2b
Sachin Tewari <stew@meta.com>
2023-08-01 11:40:46 -0700

    Hand roll equals & hashCode for SingleImageSource
    Reviewed By: oprisnik
    
    Differential Revision: D47956786
    
    fbshipit-source-id: 99c5cb505c5fc5949690bb3eeb6f7f30bd11de73

--- vito/source/src/main/java/com/facebook/fresco/vito/source/SingleImageSource.kt
+++ vito/source/src/main/java/com/facebook/fresco/vito/source/SingleImageSource.kt
@@ -9,9 +9,36 @@ package com.facebook.fresco.vito.source
 
 import android.net.Uri
 
+@Suppress("KtDataClass")
 data class SingleImageSource(val uri: Uri, val extras: Map<String, Any>? = null) : ImageSource {
 
   fun getExtra(key: String): Any? = extras?.get(key)
 
   fun getStringExtra(key: String): String? = getExtra(key) as? String
+
+  override fun equals(other: Any?): Boolean {
+    if (this === other) {
+      return true
+    }
+    if (javaClass != other?.javaClass) {
+      return false
+    }
+
+    val otherImageSource: SingleImageSource = other as SingleImageSource
+
+    if (uri != otherImageSource.uri) {
+      return false
+    }
+    if (extras != otherImageSource.extras) {
+      return false
+    }
+
+    return true
+  }
+
+  override fun hashCode(): Int {
+    var result = uri.hashCode()
+    result = 31 * result + (extras?.hashCode() ?: 0)
+    return result
+  }
 }
