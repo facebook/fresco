c1f5262eee5625fab341af7d54f67137b32dcbdd
Sachin Tewari <stew@meta.com>
2023-08-09 21:44:40 -0700

    Add a toggle for overridden data class behaviour of BorderOptions & RoundingOptions
    Reviewed By: oprisnik
    
    Differential Revision: D48114160
    
    fbshipit-source-id: 0d3d1d77adfa83eb73b9743d0551b9a747f56cef

--- vito/options/src/main/java/com/facebook/fresco/vito/options/RoundingOptions.kt
+++ vito/options/src/main/java/com/facebook/fresco/vito/options/RoundingOptions.kt
@@ -9,6 +9,7 @@ package com.facebook.fresco.vito.options
 
 import java.util.Arrays
 
+@Suppress("KtDataClass")
 data class RoundingOptions(
     val isCircular: Boolean,
     val cornerRadius: Float,
@@ -16,34 +17,40 @@ data class RoundingOptions(
     val isAntiAliased: Boolean,
     val isForceRoundAtDecode: Boolean
 ) {
+
   fun hasRoundedCorners(): Boolean = cornerRadius != CORNER_RADIUS_UNSET || cornerRadii != null
 
   override fun equals(other: Any?): Boolean {
-    if (!fixEqualityChecks) {
+    if (ImageOptionsConfig.doNotUseOverriddenDataClassMembers) {
       return super.equals(other)
     }
 
     if (this === other) {
       return true
     }
-    if (other == null || javaClass != other.javaClass) return false
-    other as RoundingOptions
-    return isCircular == other.isCircular &&
-        cornerRadius == other.cornerRadius &&
-        cornerRadii.contentEquals(other.cornerRadii) &&
-        isAntiAliased == other.isAntiAliased &&
-        isForceRoundAtDecode == other.isForceRoundAtDecode
+    if (javaClass != other?.javaClass) {
+      return false
+    }
+
+    val otherOptions: RoundingOptions = other as RoundingOptions
+
+    return isCircular == otherOptions.isCircular &&
+        cornerRadius == otherOptions.cornerRadius &&
+        cornerRadii.contentEquals(otherOptions.cornerRadii) &&
+        isAntiAliased == otherOptions.isAntiAliased &&
+        isForceRoundAtDecode == otherOptions.isForceRoundAtDecode
   }
 
   override fun hashCode(): Int {
-    if (!fixEqualityChecks) {
+    if (ImageOptionsConfig.doNotUseOverriddenDataClassMembers) {
       return super.hashCode()
     }
-    var result = if (isCircular) 1 else 0
+
+    var result = isCircular.hashCode()
     result = 31 * result + cornerRadius.hashCode()
     result = 31 * result + Arrays.hashCode(cornerRadii)
-    result = 31 * result + if (isAntiAliased) 1 else 0
-    result = 31 * result + if (isForceRoundAtDecode) 1 else 0
+    result = 31 * result + isAntiAliased.hashCode()
+    result = 31 * result + isForceRoundAtDecode.hashCode()
     return result
   }
 
@@ -51,8 +58,6 @@ data class RoundingOptions(
     const val CORNER_RADIUS_UNSET = 0f
     val AS_CIRCLE = RoundingOptions(true, CORNER_RADIUS_UNSET, null, false, false)
     val AS_CIRCLE_ANTI_ALIASING = RoundingOptions(true, CORNER_RADIUS_UNSET, null, true, false)
-
-    var fixEqualityChecks: Boolean = true
 
     @JvmStatic fun asCircle(): RoundingOptions = AS_CIRCLE
 
