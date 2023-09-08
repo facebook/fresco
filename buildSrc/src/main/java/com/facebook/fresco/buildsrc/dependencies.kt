09a0a5b31cdcfe1c6207b44e11e5659ccaadea3c
Pascal Hartig <realpassy@meta.com>
2023-09-08 04:48:14 -0700

    Bump litho to 0.48.0 (#2740)
    Summary:
    [android] Bump litho to 0.48.0
    
    Pull Request resolved: https://github.com/facebook/fresco/pull/2740
    
    Reviewed By: steelrooter
    
    Differential Revision: D48863620
    
    Pulled By: passy
    
    fbshipit-source-id: 51a4ba1db0dafaad11dddd16e909edaafec099bc

--- buildSrc/src/main/java/com/facebook/fresco/buildsrc/dependencies.kt
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/dependencies.kt
@@ -34,7 +34,7 @@ object Deps {
   }
 
   object Litho {
-    private const val version = "0.41.2"
+    private const val version = "0.48.0"
 
     const val core = "com.facebook.litho:litho-core:$version"
     const val lithoAnnotations = "com.facebook.litho:litho-annotations:$version"
