21a8b54e891020ed82c59914c9ddb430424ab0c1
Pascal Hartig <phartig@rdrei.net>
2023-09-08 14:15:28 +0100

    Bump litho package too

--- buildSrc/src/main/java/com/facebook/fresco/buildsrc/FrescoConfig.kt
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/FrescoConfig.kt
@@ -11,7 +11,7 @@ object FrescoConfig {
   const val compileSdkVersion = 34
   const val minSdkVersion = 21
   const val flipperPluginMinSdkVersion = 21
-  const val vitoLithoMinSdkVersion = 16
+  const val vitoLithoMinSdkVersion = 21
   const val samplesMinSdkVersion = 21
   const val targetSdkVersion = 34
 }
