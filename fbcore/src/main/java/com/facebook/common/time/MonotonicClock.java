1066ce0d126ef161c3ebe9b6e11532d8ad2632e5
Adam Woods-Mccormick <alanbly@meta.com>
2023-11-28 12:25:28 -0800

    T165647038 - Unify monotonic clock times
    Differential Revision: D50143098
    
    fbshipit-source-id: 1cff992c1e12fac23f556dfce0869e4e14735b44

--- fbcore/src/main/java/com/facebook/common/time/MonotonicClock.java
+++ fbcore/src/main/java/com/facebook/common/time/MonotonicClock.java
@@ -8,6 +8,7 @@
 package com.facebook.common.time;
 
 import com.facebook.common.internal.DoNotStrip;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.concurrent.ThreadSafe;
 
 /** A clock that is guaranteed not to go backward. */
@@ -19,10 +20,54 @@ public interface MonotonicClock {
    * returned from this clock in this process. They have no meaning outside of this process and
    * should not be written to disk.
    *
-   * <p>The difference between two timestamps is an interval, in milliseconds.
+   * <p>The difference between two timestamps is an interval, in nanoseconds.
    *
-   * @return A timestamp for the current time, in ms.
+   * @return A timestamp for the current time, in nanoseconds.
    */
   @DoNotStrip
-  long now();
+  long nowNanos();
+
+  /**
+   * Produce a timestamp. Values returned from this method may only be compared to other values
+   * returned from this clock in this process. They have no meaning outside of this process and
+   * should not be written to disk. This method uses the same timesource as {@link #nowNanos()}
+   *
+   * <p>The difference between two timestamps is an interval, in milliseconds.
+   *
+   * @return A timestamp for the current time, in milliseconds.
+   */
+  @DoNotStrip
+  default long now() {
+    return TimeUnit.NANOSECONDS.toMillis(nowNanos());
+  }
+
+  /**
+   * Produce a non-singleton MonotonicClock instance wrapping the given Clock.
+   *
+   * @return A MonotonicClock that uses the provided Clock.
+   */
+  static MonotonicClock of(Clock provider) {
+    return new MonotonicClockWrapper(provider);
+  }
+
+  public static final class MonotonicClockWrapper implements MonotonicClock {
+    private final Clock provider;
+    private long mLast;
+
+    private MonotonicClockWrapper(Clock provider) {
+      this.provider = provider;
+      mLast = provider.now();
+    }
+
+    @Override
+    public long nowNanos() {
+      return TimeUnit.MILLISECONDS.toNanos(now());
+    }
+
+    @Override
+    public long now() {
+      mLast = Math.max(mLast, provider.now());
+      return mLast;
+    }
+  }
 }
