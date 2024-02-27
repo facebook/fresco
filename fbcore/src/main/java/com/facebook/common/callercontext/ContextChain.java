011637b6d0ee9a4791be4655042498e508b075c0
Marshall Mann-Wood <mrmannwood@meta.com>
2024-02-27 13:16:38 -0800

    Ship use new hash function
    Reviewed By: okelepko
    
    Differential Revision: D54265549
    
    fbshipit-source-id: 8fc9e33843b5e2a5dabf6a7d833e8572bc644df6

--- fbcore/src/main/java/com/facebook/common/callercontext/ContextChain.java
+++ fbcore/src/main/java/com/facebook/common/callercontext/ContextChain.java
@@ -46,7 +46,6 @@ public class ContextChain implements Parcelable {
   private String mSerializedNodeString;
 
   private static boolean sUseConcurrentHashMap = false;
-  private static boolean sUseNewHashFunction = false;
 
   public ContextChain(
       final String tag,
@@ -100,10 +99,6 @@ public class ContextChain implements Parcelable {
 
   public static void setUseConcurrentHashMap(boolean useConcurrentHashMap) {
     sUseConcurrentHashMap = useConcurrentHashMap;
-  }
-
-  public static void setUseNewHashFunction(boolean useNewHashFunction) {
-    sUseNewHashFunction = useNewHashFunction;
   }
 
   public String getName() {
@@ -195,13 +190,7 @@ public class ContextChain implements Parcelable {
 
   @Override
   public int hashCode() {
-    if (sUseNewHashFunction) {
-      return Objects.hash(mParent, getNodeString());
-    } else {
-      int result = super.hashCode();
-      result = 31 * result + (getNodeString().hashCode());
-      return result;
-    }
+    return Objects.hash(mParent, getNodeString());
   }
 
   @Override
