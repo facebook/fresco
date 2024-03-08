95d5b5d4a05ce6abb5af231853b4f7d54494f83d
Artem Kholodnyi <defhlt@meta.com>
2024-03-08 07:27:12 -0800

    Resolve API stability warnings in ImageDataModel
    Reviewed By: oprisnik
    
    Differential Revision: D54678954
    
    fbshipit-source-id: 09504ccc83d8752e0e261748537dd0e0efd8639e

--- vito/renderer/src/main/java/com/facebook/fresco/vito/renderer/ImageDataModel.kt
+++ vito/renderer/src/main/java/com/facebook/fresco/vito/renderer/ImageDataModel.kt
@@ -14,29 +14,29 @@ import android.graphics.drawable.Drawable
 import android.graphics.drawable.NinePatchDrawable
 
 sealed class ImageDataModel {
-  open val width = -1
-  open val height = -1
-  open val defaultPaintFlags = Paint.ANTI_ALIAS_FLAG
+  open val width: Int = -1
+  open val height: Int = -1
+  open val defaultPaintFlags: Int = Paint.ANTI_ALIAS_FLAG
 
-  open fun setCallback(callback: Drawable.Callback?) = Unit
+  open fun setCallback(callback: Drawable.Callback?): Unit = Unit
 
-  open fun onAttach() = Unit
+  open fun onAttach(): Unit = Unit
 
-  open fun onDetach() = Unit
+  open fun onDetach(): Unit = Unit
 }
 
 class ColorIntImageDataModel(val colorInt: Int) : ImageDataModel()
 
 class BitmapImageDataModel(val bitmap: Bitmap, val isBitmapCircular: Boolean = false) :
     ImageDataModel() {
-  override val width = bitmap.width
-  override val height = bitmap.height
+  override val width: Int = bitmap.width
+  override val height: Int = bitmap.height
   override val defaultPaintFlags: Int = Paint.FILTER_BITMAP_FLAG or Paint.DITHER_FLAG
 }
 
 open class DrawableImageDataModel(val drawable: Drawable) : ImageDataModel() {
-  override val width = if (drawable is NinePatchDrawable) -1 else drawable.intrinsicWidth
-  override val height = if (drawable is NinePatchDrawable) -1 else drawable.intrinsicHeight
+  override val width: Int = if (drawable is NinePatchDrawable) -1 else drawable.intrinsicWidth
+  override val height: Int = if (drawable is NinePatchDrawable) -1 else drawable.intrinsicHeight
 
   override fun setCallback(callback: Drawable.Callback?) {
     drawable.callback = callback
