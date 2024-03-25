1df6db652276eddefb0924bf99287da45716ada5
Vignesh <emvignesh16@gmail.com>
2024-03-25 10:12:23 +0530

    Add an ExampleItem for AVIF

---
+++ samples/showcase/src/main/java/com/facebook/fresco/samples/showcase/imageformat/avif/ImageFormatAvifFragment.kt
@@ -0,0 +1,5 @@
+package com.facebook.fresco.samples.showcase.imageformat.avif
+
+import com.facebook.fresco.samples.showcase.BaseShowcaseFragment
+
+class ImageFormatAvifFragment : BaseShowcaseFragment()
