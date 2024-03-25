aa934752b564c7864102f9773d4b4248c69316b4
Vignesh <emvignesh16@gmail.com>
2024-03-25 12:27:00 +0530

    Use bitmaps from BitmapPool

--- imagepipeline/src/main/java/com/facebook/imagepipeline/decoder/factory/provider/AvifDecoderFactoryProvider.kt
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/decoder/factory/provider/AvifDecoderFactoryProvider.kt
@@ -1,6 +1,7 @@
 package com.facebook.imagepipeline.decoder.factory.provider
 
 import com.facebook.imagepipeline.decoder.factory.AvifDecoderFactory
+import com.facebook.imagepipeline.memory.BitmapPool
 
 object AvifDecoderFactoryProvider {
 
@@ -9,13 +10,14 @@ object AvifDecoderFactoryProvider {
   private var avifDecoderFactory: AvifDecoderFactory? = null
 
   @JvmStatic
-  fun loadIfExists(): AvifDecoderFactory? {
+  fun loadIfExists(bitmapPool: BitmapPool): AvifDecoderFactory? {
     if (checked) return avifDecoderFactory
 
     try {
       avifDecoderFactory = Class
               .forName("com.facebook.avifsupport.AvifDecoderFactoryImpl")
-              .newInstance() as? AvifDecoderFactory
+              .getConstructor(BitmapPool::class.java)
+              .newInstance(bitmapPool) as? AvifDecoderFactory
     } catch (_: Throwable) {
       // Not available
     }
--- static-avif/src/main/java/com/facebook/avifsupport/AvifDecoderFactoryImpl.kt
+++ static-avif/src/main/java/com/facebook/avifsupport/AvifDecoderFactoryImpl.kt
@@ -3,8 +3,11 @@ package com.facebook.avifsupport
 import com.facebook.common.internal.DoNotStrip
 import com.facebook.imagepipeline.decoder.ImageDecoder
 import com.facebook.imagepipeline.decoder.factory.AvifDecoderFactory
+import com.facebook.imagepipeline.memory.BitmapPool
 import com.facebook.infer.annotation.Nullsafe
 
 @Nullsafe(Nullsafe.Mode.STRICT)
 @DoNotStrip
-class AvifDecoderFactoryImpl(override val avifDecoder: ImageDecoder = AvifDecoder()) : AvifDecoderFactory
+class AvifDecoderFactoryImpl(bitmapPool: BitmapPool) : AvifDecoderFactory {
+  override val avifDecoder: ImageDecoder = AvifDecoder(bitmapPool)
+}
