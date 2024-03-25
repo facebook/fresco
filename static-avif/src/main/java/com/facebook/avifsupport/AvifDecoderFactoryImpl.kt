c99b57cdd888cd0a3cd04528910c6c2b77d7571c
Vignesh <emvignesh16@gmail.com>
2024-03-25 11:48:41 +0530

    Load AvifDecoderFactoryImpl using AvifDecoderFactoryProvider

--- static-avif/src/main/java/com/facebook/avifsupport/AvifDecoderFactoryImpl.kt
+++ static-avif/src/main/java/com/facebook/avifsupport/AvifDecoderFactoryImpl.kt
@@ -1,6 +1,10 @@
 package com.facebook.avifsupport
 
+import com.facebook.common.internal.DoNotStrip
 import com.facebook.imagepipeline.decoder.ImageDecoder
 import com.facebook.imagepipeline.decoder.factory.AvifDecoderFactory
+import com.facebook.infer.annotation.Nullsafe
 
+@Nullsafe(Nullsafe.Mode.STRICT)
+@DoNotStrip
 class AvifDecoderFactoryImpl(override val avifDecoder: ImageDecoder? = null) : AvifDecoderFactory
