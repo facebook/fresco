b7b38ee664ba575b2fbef77732de423e68dc0667
unknown <alanyu2015@gmail.com>
2024-04-18 16:57:41 +0800

    Upgrade libjpeg-turbo to 2.1.5

--- static-webp/src/main/jni/third-party/libjpeg-turbo-2.1.5/jconfig.h
+++ static-webp/src/main/jni/third-party/libjpeg-turbo-2.1.5/jconfig.h
@@ -4,7 +4,7 @@
 #define JPEG_LIB_VERSION  80	/* Version 6b */
 
 /* libjpeg-turbo version */
-#define LIBJPEG_TURBO_VERSION 1.5.3
+#define LIBJPEG_TURBO_VERSION 2.1.5
 
 /* libjpeg-turbo version in integer form */
 #define LIBJPEG_TURBO_VERSION_NUMBER 0
--- static-webp/src/main/jni/third-party/libjpeg-turbo-2.1.5/jconfigint.h
+++ static-webp/src/main/jni/third-party/libjpeg-turbo-2.1.5/jconfigint.h
@@ -1,5 +1,5 @@
 /* libjpeg-turbo build number */
-#define BUILD "20180808"
+#define BUILD "20240418"
 
 /* Compiler's inline keyword */
 /* #undef inline */
@@ -7,15 +7,28 @@
 /* How to obtain function inlining. */
 #define INLINE inline __attribute__((always_inline))
 
+/* How to obtain thread-local storage */
+#define THREAD_LOCAL  __thread
+
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "libjpeg-turbo"
 
 /* Version number of package */
-#define VERSION "1.5.3"
+#define VERSION "2.1.5"
 
 /* The size of `size_t', as computed by sizeof. */
 #ifdef __LP64__
 #  define SIZEOF_SIZE_T 8
 #else
 #  define SIZEOF_SIZE_T 4
+#endif
+
+#if defined(__has_attribute)
+#if __has_attribute(fallthrough)
+#define FALLTHROUGH __attribute__((fallthrough));
+#else
+#define FALLTHROUGH
+#endif
+#else
+#define FALLTHROUGH
 #endif
