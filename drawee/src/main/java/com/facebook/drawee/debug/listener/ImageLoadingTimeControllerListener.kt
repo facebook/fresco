4e49544079c02ab30d395bd9411ee6cc5e4350af
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-07-18 13:33:10 -0700

    //fbandroid/libraries/fresco/drawee/src/main/java/com/facebook/drawee/debug/listener:listener
    Reviewed By: nicholeic
    
    Differential Revision: D59904842
    
    fbshipit-source-id: ac3f5279269c534e9979c54cf132196cba45308e

---
+++ drawee/src/main/java/com/facebook/drawee/debug/listener/ImageLoadingTimeControllerListener.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.drawee.debug.listener
+
+import android.graphics.drawable.Animatable
+import com.facebook.drawee.controller.BaseControllerListener
+
+/**
+ * Currently we are measuring this from Submit to Final Image.But can be extended to include
+ * intermediate time and failure cases also
+ */
+class ImageLoadingTimeControllerListener(
+    private val imageLoadingTimeListener: ImageLoadingTimeListener?
+) : BaseControllerListener<Any?>() {
+
+  private var requestSubmitTimeMs = -1L
+  private var finalImageSetTimeMs = -1L
+
+  override fun onSubmit(id: String, callerContext: Any) {
+    requestSubmitTimeMs = System.currentTimeMillis()
+  }
+
+  override fun onFinalImageSet(id: String, imageInfo: Any?, animatable: Animatable?) {
+    finalImageSetTimeMs = System.currentTimeMillis()
+    imageLoadingTimeListener?.onFinalImageSet(finalImageSetTimeMs - requestSubmitTimeMs)
+  }
+}
