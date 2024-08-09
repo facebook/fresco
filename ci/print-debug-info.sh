bcc74eb2991b9bbc7c5280b4b0fa5057fa9b015e
Sachin Tewari <stew@meta.com>
2024-08-09 06:58:45 -0700

    Fix broken GH Actions pipeline
    Reviewed By: oprisnik, defHLT
    
    Differential Revision: D61016105
    
    fbshipit-source-id: b003ca103ce5b3406f70f3ecf2c10adc0eb57508

---
+++ ci/print-debug-info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+echo ""
+echo "Fresco CI Debug information begin"
+
+echo ""
+echo "env PATH: $PATH"
+
+echo ""
+echo "env ANDROID_NDK_HOME: $ANDROID_NDK_HOME"
+
+echo ""
+echo "Fresco CI Debug information end"
