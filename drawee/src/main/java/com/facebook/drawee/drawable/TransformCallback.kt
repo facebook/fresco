5382afcea02bcac5a3dae8d523171b3dad45bc17
Sachin Tewari <stew@meta.com>
2024-09-10 04:12:25 -0700

    Convert fbandroid/libraries/fresco/drawee/src/m/j/c/f/drawee/drawable/TransformCallback to Kotlin
    Reviewed By: kartavya-ramnani
    
    Differential Revision: D62388696
    
    fbshipit-source-id: af73ea78fb35e14255e22682f00a04b927e27623

--- drawee/src/main/java/com/facebook/drawee/drawable/TransformCallback.kt
+++ drawee/src/main/java/com/facebook/drawee/drawable/TransformCallback.kt
@@ -5,28 +5,28 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.drawee.drawable;
+package com.facebook.drawee.drawable
 
-import android.graphics.Matrix;
-import android.graphics.RectF;
+import android.graphics.Matrix
+import android.graphics.RectF
 
 /**
  * Callback that is used to pass any transformation matrix and the root bounds from a parent
  * drawable to its child.
  */
-public interface TransformCallback {
+interface TransformCallback {
 
   /**
    * Called when the drawable needs to get all matrices applied to it.
    *
    * @param transform Matrix that is applied to the drawable by the parent drawables.
    */
-  void getTransform(Matrix transform);
+  fun getTransform(transform: Matrix)
 
   /**
    * Called when the drawable needs to get its root bounds.
    *
    * @param bounds The root bounds of the drawable.
    */
-  void getRootBounds(RectF bounds);
+  fun getRootBounds(bounds: RectF)
 }
