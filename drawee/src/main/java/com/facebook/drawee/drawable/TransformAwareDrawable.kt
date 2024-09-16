197426edf834b44ef301978f4e9f81404c22685f
Sachin Tewari <stew@meta.com>
2024-09-16 04:12:29 -0700

    Convert fbandroid/libraries/fresco/drawee/src/m/j/c/f/drawee/drawable/TransformAwareDrawable to Kotlin
    Reviewed By: kartavya-ramnani
    
    Differential Revision: D62638915
    
    fbshipit-source-id: 21fe9aa315554bf5fe2dc6ea7c4be785d88dbbe6

--- drawee/src/main/java/com/facebook/drawee/drawable/TransformAwareDrawable.kt
+++ drawee/src/main/java/com/facebook/drawee/drawable/TransformAwareDrawable.kt
@@ -4,20 +4,14 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-package com.facebook.drawee.drawable;
-
-import androidx.annotation.Nullable;
-import com.facebook.infer.annotation.Nullsafe;
+package com.facebook.drawee.drawable
 
 /** Interface that enables setting a transform callback. */
-@Nullsafe(Nullsafe.Mode.LOCAL)
-public interface TransformAwareDrawable {
-
+fun interface TransformAwareDrawable {
   /**
    * Sets a transform callback.
    *
    * @param transformCallback the transform callback to be set
    */
-  void setTransformCallback(@Nullable TransformCallback transformCallback);
+  fun setTransformCallback(transformCallback: TransformCallback?)
 }
