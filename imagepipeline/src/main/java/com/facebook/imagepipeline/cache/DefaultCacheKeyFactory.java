bc37887d39a28920fcd9a2855cb1687a68ae4d47
Ilia Zaslavski <iliazas@meta.com>
2025-01-02 14:44:06 -0800

    Pass the param to the cache key factory to control the fixing the leak of bloks model by cached key
    Reviewed By: oprisnik
    
    Differential Revision: D67767055
    
    fbshipit-source-id: aaefed460c36e25d5fb5d1ef7b3f9b863a70e918

--- imagepipeline/src/main/java/com/facebook/imagepipeline/cache/DefaultCacheKeyFactory.java
+++ imagepipeline/src/main/java/com/facebook/imagepipeline/cache/DefaultCacheKeyFactory.java
@@ -21,6 +21,10 @@ public class DefaultCacheKeyFactory implements CacheKeyFactory {
 
   private static @Nullable DefaultCacheKeyFactory sInstance = null;
 
+  // Experiment with removing caller context from cache key. This should prevent leaking
+  // the caller context object by the cache key.
+  private static boolean sShouldRemoveCallerContextFromCacheKey = false;
+
   protected DefaultCacheKeyFactory() {}
 
   public static synchronized DefaultCacheKeyFactory getInstance() {
@@ -28,6 +32,11 @@ public class DefaultCacheKeyFactory implements CacheKeyFactory {
       sInstance = new DefaultCacheKeyFactory();
     }
     return sInstance;
+  }
+
+  public static void setShouldRemoveCallerContextFromCacheKey(
+      boolean shouldRemoveCallerContextFromCacheKey) {
+    sShouldRemoveCallerContextFromCacheKey = shouldRemoveCallerContextFromCacheKey;
   }
 
   @Override
@@ -40,7 +49,13 @@ public class DefaultCacheKeyFactory implements CacheKeyFactory {
             request.getImageDecodeOptions(),
             null,
             null);
-    cacheKey.setCallerContext(callerContext);
+
+    if (sShouldRemoveCallerContextFromCacheKey) {
+      cacheKey.setCallerContext(null);
+    } else {
+      cacheKey.setCallerContext(callerContext);
+    }
+
     return cacheKey;
   }
 
@@ -65,7 +80,13 @@ public class DefaultCacheKeyFactory implements CacheKeyFactory {
             request.getImageDecodeOptions(),
             postprocessorCacheKey,
             postprocessorName);
-    cacheKey.setCallerContext(callerContext);
+
+    if (sShouldRemoveCallerContextFromCacheKey) {
+      cacheKey.setCallerContext(null);
+    } else {
+      cacheKey.setCallerContext(callerContext);
+    }
+
     return cacheKey;
   }
 
