044bbe33fad35277dd9d6ea5009e7a991690f4cd
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-01-28 06:55:52 -0800

    fbandroid/libraries/fresco/memory-types/nativememory/src/test/java/com/facebook/imagepipeline/testing/FakeNativeMemoryChunk.java
    Reviewed By: jocelynluizzi13
    
    Differential Revision: D68765500
    
    fbshipit-source-id: 747c29b27bef8f7a0ee638d5c08c21dcbdc68804

--- memory-types/nativememory/src/test/java/com/facebook/imagepipeline/testing/FakeNativeMemoryChunk.java
+++ memory-types/nativememory/src/test/java/com/facebook/imagepipeline/testing/FakeNativeMemoryChunk.java
@@ -9,6 +9,7 @@ package com.facebook.imagepipeline.testing;
 
 import com.facebook.imagepipeline.memory.MemoryChunk;
 import com.facebook.imagepipeline.memory.NativeMemoryChunk;
+import com.facebook.infer.annotation.Nullsafe;
 import javax.annotation.Nullable;
 
 /**
@@ -16,6 +17,7 @@ import javax.annotation.Nullable;
  * to test out pools and other functionality. This uses byte arrays instead of actual native memory,
  * but supports the same public interface
  */
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public class FakeNativeMemoryChunk extends NativeMemoryChunk {
   @Nullable private byte[] mBuf;
 
@@ -36,24 +38,30 @@ public class FakeNativeMemoryChunk extends NativeMemoryChunk {
 
   @Override
   public int getSize() {
+    // NULLSAFE_FIXME[Nullable Dereference]
     return mBuf.length;
   }
 
   @Override
   public int write(int nativeMemoryOffset, byte[] byteArray, int byteArrayOffset, int count) {
+    // NULLSAFE_FIXME[Nullable Dereference]
     int numToWrite = Math.min(count, mBuf.length - nativeMemoryOffset);
+    // NULLSAFE_FIXME[Parameter Not Nullable]
     System.arraycopy(byteArray, byteArrayOffset, mBuf, nativeMemoryOffset, numToWrite);
     return numToWrite;
   }
 
   @Override
   public byte read(int nativeMemoryOffset) {
+    // NULLSAFE_FIXME[Nullable Dereference]
     return mBuf[nativeMemoryOffset];
   }
 
   @Override
   public int read(int nativeMemoryOffset, byte[] byteArray, int byteArrayOffset, int count) {
+    // NULLSAFE_FIXME[Nullable Dereference]
     int numToRead = Math.min(count, mBuf.length - nativeMemoryOffset);
+    // NULLSAFE_FIXME[Parameter Not Nullable]
     System.arraycopy(mBuf, nativeMemoryOffset, byteArray, byteArrayOffset, numToRead);
     return numToRead;
   }
@@ -61,7 +69,9 @@ public class FakeNativeMemoryChunk extends NativeMemoryChunk {
   @Override
   public void copy(int offset, MemoryChunk other, int otherOffset, int count) {
     FakeNativeMemoryChunk that = (FakeNativeMemoryChunk) other;
+    // NULLSAFE_FIXME[Nullable Dereference]
     int numToCopy = Math.min(count, mBuf.length - offset);
+    // NULLSAFE_FIXME[Parameter Not Nullable]
     System.arraycopy(mBuf, offset, that.mBuf, otherOffset, numToCopy);
   }
 }
