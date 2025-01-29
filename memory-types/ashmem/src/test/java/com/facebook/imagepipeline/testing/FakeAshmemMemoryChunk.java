08559760f68d22bed59ce9ae9e84a3866cc0e817
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-01-29 05:32:30 -0800

    fbandroid/libraries/fresco/memory-types/ashmem/src/test/java/com/facebook/imagepipeline/testing/FakeAshmemMemoryChunk.java
    Reviewed By: oprisnik
    
    Differential Revision: D68821806
    
    fbshipit-source-id: 1fc0e79cccb441455cc9b5e891f87edb508be420

--- memory-types/ashmem/src/test/java/com/facebook/imagepipeline/testing/FakeAshmemMemoryChunk.java
+++ memory-types/ashmem/src/test/java/com/facebook/imagepipeline/testing/FakeAshmemMemoryChunk.java
@@ -9,6 +9,7 @@ package com.facebook.imagepipeline.testing;
 
 import com.facebook.imagepipeline.memory.AshmemMemoryChunk;
 import com.facebook.imagepipeline.memory.MemoryChunk;
+import com.facebook.infer.annotation.Nullsafe;
 import javax.annotation.Nullable;
 
 /**
@@ -16,6 +17,7 @@ import javax.annotation.Nullable;
  * out pools and other functionality. This uses byte arrays instead of actual ashmem memory, but
  * supports the same public interface
  */
+@Nullsafe(Nullsafe.Mode.LOCAL)
 public class FakeAshmemMemoryChunk extends AshmemMemoryChunk {
   @Nullable private byte[] mBuf;
 
@@ -36,24 +38,30 @@ public class FakeAshmemMemoryChunk extends AshmemMemoryChunk {
 
   @Override
   public int getSize() {
+    // NULLSAFE_FIXME[Nullable Dereference]
     return mBuf.length;
   }
 
   @Override
   public int write(int ashmemMemoryOffset, byte[] byteArray, int byteArrayOffset, int count) {
+    // NULLSAFE_FIXME[Nullable Dereference]
     int numToWrite = Math.min(count, mBuf.length - ashmemMemoryOffset);
+    // NULLSAFE_FIXME[Parameter Not Nullable]
     System.arraycopy(byteArray, byteArrayOffset, mBuf, ashmemMemoryOffset, numToWrite);
     return numToWrite;
   }
 
   @Override
   public byte read(int ashmemMemoryOffset) {
+    // NULLSAFE_FIXME[Nullable Dereference]
     return mBuf[ashmemMemoryOffset];
   }
 
   @Override
   public int read(int ashmemMemoryOffset, byte[] byteArray, int byteArrayOffset, int count) {
+    // NULLSAFE_FIXME[Nullable Dereference]
     int numToRead = Math.min(count, mBuf.length - ashmemMemoryOffset);
+    // NULLSAFE_FIXME[Parameter Not Nullable]
     System.arraycopy(mBuf, ashmemMemoryOffset, byteArray, byteArrayOffset, numToRead);
     return numToRead;
   }
@@ -61,7 +69,9 @@ public class FakeAshmemMemoryChunk extends AshmemMemoryChunk {
   @Override
   public void copy(int offset, MemoryChunk other, int otherOffset, int count) {
     FakeAshmemMemoryChunk that = (FakeAshmemMemoryChunk) other;
+    // NULLSAFE_FIXME[Nullable Dereference]
     int numToCopy = Math.min(count, mBuf.length - offset);
+    // NULLSAFE_FIXME[Parameter Not Nullable]
     System.arraycopy(mBuf, offset, that.mBuf, otherOffset, numToCopy);
   }
 }
