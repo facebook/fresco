8adef9e50720b0d73a449ae5300d55ddadff5b4b
Trevor Masters <tmasters@meta.com>
2025-02-13 15:14:42 -0800

    Migrate more mid-level deps away from powermock
    Reviewed By: cpepin
    
    Differential Revision: D69595761
    
    fbshipit-source-id: 8137262d4fc6b8799eaeaf3ce08b4d20afc258f9

--- drawee/src/test/java/com/facebook/drawee/drawable/DrawableTestUtils.java
+++ drawee/src/test/java/com/facebook/drawee/drawable/DrawableTestUtils.java
@@ -7,9 +7,9 @@
 
 package com.facebook.drawee.drawable;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyBoolean;
-import static org.mockito.Matchers.anyInt;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
