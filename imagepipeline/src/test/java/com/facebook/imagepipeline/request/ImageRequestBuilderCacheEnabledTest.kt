00462fce5c869bbdf14c63e1e93db77c6a195b15
Andy Wu <andywu2513@meta.com>
2025-07-29 09:04:15 -0700

    Convert ImageRequestBuilderCacheEnabledTest to Kotlin
    Reviewed By: kartavya-ramnani
    
    Differential Revision: D79175197
    
    fbshipit-source-id: 9a95603f18fc2ea67c81cadc211de4f01f9a2c4d

---
+++ imagepipeline/src/test/java/com/facebook/imagepipeline/request/ImageRequestBuilderCacheEnabledTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+package com.facebook.imagepipeline.request
+
+import android.net.Uri
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.ParameterizedRobolectricTestRunner
+
+@RunWith(ParameterizedRobolectricTestRunner::class)
+class ImageRequestBuilderCacheEnabledTest(
+    private val uriScheme: String?,
+    private val expectedDefaultDiskCacheEnabled: Boolean
+) {
+
+  @Test
+  fun testIsDiskCacheEnabledByDefault() {
+    val imageRequestBuilder = createBuilder()
+    assertThat(imageRequestBuilder.isDiskCacheEnabled()).isEqualTo(expectedDefaultDiskCacheEnabled)
+  }
+
+  @Test
+  fun testIsDiskCacheDisabledIfRequested() {
+    val imageRequestBuilder = createBuilder()
+    imageRequestBuilder.disableDiskCache()
+    assertThat(imageRequestBuilder.isDiskCacheEnabled()).isFalse()
+  }
+
+  private fun createBuilder(): ImageRequestBuilder {
+    return ImageRequestBuilder.newBuilderWithSource(Uri.parse("$uriScheme://request"))
+  }
+
+  companion object {
+    @ParameterizedRobolectricTestRunner.Parameters(name = "URI of scheme \"{0}://\"")
+    @JvmStatic
+    fun data(): Collection<Array<Any?>> {
+      return listOf(
+          arrayOf("asset", false),
+          arrayOf("content", false),
+          arrayOf("data", false),
+          arrayOf("file", false),
+          arrayOf("http", true),
+          arrayOf("https", true),
+          arrayOf("res", false))
+    }
+  }
+}
