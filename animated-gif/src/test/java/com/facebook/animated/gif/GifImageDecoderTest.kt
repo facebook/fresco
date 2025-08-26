4573b46208b1fb756066efc46b8ea0848281204b
Andy Wu <andywu2513@meta.com>
2025-08-26 08:52:18 -0700

    Refactor Tests for GifImageDecoder
    Reviewed By: kartavya-ramnani
    
    Differential Revision: D81029539
    
    fbshipit-source-id: e36e46ced0764fd190f2ec080c08595e881cbd94

--- animated-gif/src/test/java/com/facebook/animated/gif/GifImageDecoderTest.kt
+++ animated-gif/src/test/java/com/facebook/animated/gif/GifImageDecoderTest.kt
@@ -5,31 +5,30 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-package com.facebook.imagepipeline.animated.factory
+package com.facebook.animated.gif
 
 import android.graphics.Bitmap
-import android.graphics.Rect
-import com.facebook.animated.gif.GifImage
 import com.facebook.common.memory.PooledByteBuffer
 import com.facebook.common.references.CloseableReference
 import com.facebook.common.references.ResourceReleaser
 import com.facebook.imageformat.ImageFormat
-import com.facebook.imagepipeline.animated.base.AnimatedDrawableBackend
 import com.facebook.imagepipeline.animated.base.AnimatedImageResult
-import com.facebook.imagepipeline.animated.impl.AnimatedDrawableBackendProvider
 import com.facebook.imagepipeline.animated.impl.AnimatedImageCompositor
 import com.facebook.imagepipeline.bitmaps.PlatformBitmapFactory
 import com.facebook.imagepipeline.common.ImageDecodeOptions
 import com.facebook.imagepipeline.image.CloseableAnimatedImage
+import com.facebook.imagepipeline.image.CloseableStaticBitmap
 import com.facebook.imagepipeline.image.EncodedImage
+import com.facebook.imagepipeline.image.ImmutableQualityInfo
 import com.facebook.imagepipeline.testing.MockBitmapFactory
 import com.facebook.imagepipeline.testing.TrivialBufferPooledByteBuffer
 import com.facebook.imagepipeline.testing.TrivialPooledByteBuffer
 import java.io.ByteArrayOutputStream
 import java.io.IOException
 import java.nio.ByteBuffer
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.After
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -45,31 +44,30 @@ import org.mockito.kotlin.verifyNoMoreInteractions
 import org.mockito.kotlin.whenever
 import org.robolectric.RobolectricTestRunner
 
-/** Tests for [AnimatedImageFactory] */
+/** Tests for [GifImageDecoder] */
 @RunWith(RobolectricTestRunner::class)
-class AnimatedImageFactoryGifImplTest {
-  private var mockAnimatedDrawableBackendProvider: AnimatedDrawableBackendProvider? = null
+class GifImageDecoderTest {
   private var mockBitmapFactory: PlatformBitmapFactory? = null
-  private var animatedImageFactory: AnimatedImageFactory? = null
+  private var gifImageDecoder: GifImageDecoder? = null
 
-  private var gifImageMock: GifImage? = null
   private var gifImageStaticMock: MockedStatic<GifImage?>? = null
 
   @Before
   fun setup() {
     gifImageStaticMock = Mockito.mockStatic(GifImage::class.java)
-    gifImageMock = mock<GifImage>()
 
-    mockAnimatedDrawableBackendProvider = mock<AnimatedDrawableBackendProvider>()
     mockBitmapFactory = mock<PlatformBitmapFactory>()
 
-    val backendProvider = mockAnimatedDrawableBackendProvider
     val bitmapFactory = mockBitmapFactory
-    if (backendProvider != null && bitmapFactory != null) {
-      animatedImageFactory = AnimatedImageFactoryImpl(backendProvider, bitmapFactory, false)
+    if (bitmapFactory != null) {
+      gifImageDecoder =
+          GifImageDecoder(
+              bitmapFactory,
+              isNewRenderImplementation = false,
+              downscaleFrameToDrawableDimensions = false,
+              treatAnimatedImagesAsStateful = true,
+          )
     }
-
-    AnimatedImageFactoryImpl.gifAnimatedImageDecoder = gifImageMock
   }
 
   @After
@@ -81,10 +79,10 @@ class AnimatedImageFactoryGifImplTest {
   fun testCreateDefaultsUsingPointer() {
     val mockGifImage: GifImage? = mock<GifImage>()
 
-    // Expect a call to GifImage.createFromByteBuffer
+    // Expect a call to GifImage.createFromNativeMemory
     val byteBuffer: TrivialPooledByteBuffer = createByteBuffer()
     whenever(
-            gifImageMock?.decodeFromNativeMemory(
+            GifImage.createFromNativeMemory(
                 ArgumentMatchers.eq(byteBuffer.getNativePtr()),
                 ArgumentMatchers.eq(byteBuffer.size()),
                 Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
@@ -102,7 +100,7 @@ class AnimatedImageFactoryGifImplTest {
     // Expect a call to GifImage.createFromByteBuffer
     val byteBuffer: TrivialBufferPooledByteBuffer = createDirectByteBuffer()
     whenever(
-            gifImageMock?.decodeFromByteBuffer(
+            GifImage.createFromByteBuffer(
                 ArgumentMatchers.eq<ByteBuffer?>(byteBuffer.getByteBuffer()),
                 Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
             )
@@ -118,10 +116,10 @@ class AnimatedImageFactoryGifImplTest {
     val mockGifImage: GifImage = mock<GifImage>()
     val mockBitmap: Bitmap = MockBitmapFactory.create(50, 50, DEFAULT_BITMAP_CONFIG)
 
-    // Expect a call to GifImage.createFromByteBuffer
+    // Expect a call to GifImage.createFromNativeMemory
     val byteBuffer: TrivialPooledByteBuffer = createByteBuffer()
     whenever(
-            gifImageMock?.decodeFromNativeMemory(
+            GifImage.createFromNativeMemory(
                 ArgumentMatchers.eq(byteBuffer.getNativePtr()),
                 ArgumentMatchers.eq(byteBuffer.size()),
                 Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
@@ -130,6 +128,8 @@ class AnimatedImageFactoryGifImplTest {
         .thenReturn(mockGifImage)
     whenever(mockGifImage.getWidth()).thenReturn(50)
     whenever(mockGifImage.getHeight()).thenReturn(50)
+    whenever(mockGifImage.getFrameCount()).thenReturn(1)
+    whenever(mockGifImage.getFrameDurations()).thenReturn(intArrayOf(100))
 
     testCreateWithPreviewBitmap(mockGifImage, mockBitmap, byteBuffer)
   }
@@ -143,7 +143,7 @@ class AnimatedImageFactoryGifImplTest {
     // Expect a call to GifImage.createFromByteBuffer
     val byteBuffer: TrivialBufferPooledByteBuffer = createDirectByteBuffer()
     whenever(
-            gifImageMock?.decodeFromByteBuffer(
+            GifImage.createFromByteBuffer(
                 ArgumentMatchers.eq<ByteBuffer?>(byteBuffer.getByteBuffer()),
                 Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
             )
@@ -151,6 +151,8 @@ class AnimatedImageFactoryGifImplTest {
         .thenReturn(mockGifImage)
     whenever(mockGifImage.getWidth()).thenReturn(50)
     whenever(mockGifImage.getHeight()).thenReturn(50)
+    whenever(mockGifImage.getFrameCount()).thenReturn(1)
+    whenever(mockGifImage.getFrameDurations()).thenReturn(intArrayOf(100))
 
     testCreateWithPreviewBitmap(mockGifImage, mockBitmap, byteBuffer)
   }
@@ -163,10 +165,10 @@ class AnimatedImageFactoryGifImplTest {
     val mockBitmap1: Bitmap = MockBitmapFactory.create(50, 50, DEFAULT_BITMAP_CONFIG)
     val mockBitmap2: Bitmap = MockBitmapFactory.create(50, 50, DEFAULT_BITMAP_CONFIG)
 
-    // Expect a call to GifImage.createFromByteBuffer
+    // Expect a call to GifImage.createFromNativeMemory
     val byteBuffer: TrivialPooledByteBuffer = createByteBuffer()
     whenever(
-            gifImageMock?.decodeFromNativeMemory(
+            GifImage.createFromNativeMemory(
                 ArgumentMatchers.eq(byteBuffer.getNativePtr()),
                 ArgumentMatchers.eq(byteBuffer.size()),
                 Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
@@ -175,6 +177,8 @@ class AnimatedImageFactoryGifImplTest {
         .thenReturn(mockGifImage)
     whenever(mockGifImage.getWidth()).thenReturn(50)
     whenever(mockGifImage.getHeight()).thenReturn(50)
+    whenever(mockGifImage.getFrameCount()).thenReturn(2)
+    whenever(mockGifImage.getFrameDurations()).thenReturn(intArrayOf(100, 150))
 
     testCreateWithDecodeAllFrames(mockGifImage, mockBitmap1, mockBitmap2, byteBuffer)
   }
@@ -190,7 +194,7 @@ class AnimatedImageFactoryGifImplTest {
     // Expect a call to GifImage.createFromByteBuffer
     val byteBuffer: TrivialBufferPooledByteBuffer = createDirectByteBuffer()
     whenever(
-            gifImageMock?.decodeFromByteBuffer(
+            GifImage.createFromByteBuffer(
                 ArgumentMatchers.eq<ByteBuffer?>(byteBuffer.getByteBuffer()),
                 Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
             )
@@ -198,6 +202,8 @@ class AnimatedImageFactoryGifImplTest {
         .thenReturn(mockGifImage)
     whenever(mockGifImage.getWidth()).thenReturn(50)
     whenever(mockGifImage.getHeight()).thenReturn(50)
+    whenever(mockGifImage.getFrameCount()).thenReturn(2)
+    whenever(mockGifImage.getFrameDurations()).thenReturn(intArrayOf(100, 150))
 
     testCreateWithDecodeAllFrames(mockGifImage, mockBitmap1, mockBitmap2, byteBuffer)
   }
@@ -208,20 +214,20 @@ class AnimatedImageFactoryGifImplTest {
     encodedImage.setImageFormat(ImageFormat.UNKNOWN)
 
     val closeableImage: CloseableAnimatedImage? =
-        animatedImageFactory?.decodeGif(
+        gifImageDecoder?.decode(
             encodedImage,
+            byteBuffer.size(),
+            ImmutableQualityInfo.FULL_QUALITY,
             ImageDecodeOptions.defaults(),
-            DEFAULT_BITMAP_CONFIG,
         ) as? CloseableAnimatedImage
 
     // Verify we got the right result
     val imageResult: AnimatedImageResult? = closeableImage?.getImageResult()
-    Assert.assertSame(mockGifImage, imageResult?.getImage())
-    Assert.assertNull(imageResult?.getPreviewBitmap())
-    Assert.assertFalse(imageResult?.hasDecodedFrame(0) ?: false)
+    assertThat(imageResult?.getImage()).isSameAs(mockGifImage)
+    assertThat(imageResult?.getPreviewBitmap()).isNull()
+    assertThat(imageResult?.hasDecodedFrame(0) ?: false).isFalse()
 
     // Should not have interacted with these.
-    mockAnimatedDrawableBackendProvider?.let { Mockito.verifyNoInteractions(it) }
     mockBitmapFactory?.let { Mockito.verifyNoInteractions(it) }
   }
 
@@ -231,15 +237,6 @@ class AnimatedImageFactoryGifImplTest {
       mockBitmap: Bitmap,
       byteBuffer: PooledByteBuffer,
   ) {
-    // For decoding preview frame, expect some calls.
-    val mockAnimatedDrawableBackend: AnimatedDrawableBackend = createAnimatedDrawableBackendMock(1)
-    whenever(
-            mockAnimatedDrawableBackendProvider?.get(
-                Matchers.any<AnimatedImageResult?>(AnimatedImageResult::class.java),
-                Matchers.isNull<Rect?>(Rect::class.java),
-            )
-        )
-        .thenReturn(mockAnimatedDrawableBackend)
     whenever(mockBitmapFactory?.createBitmapInternal(50, 50, DEFAULT_BITMAP_CONFIG))
         .thenReturn(CloseableReference.of<Bitmap?>(mockBitmap, FAKE_BITMAP_RESOURCE_RELEASER))
 
@@ -251,24 +248,19 @@ class AnimatedImageFactoryGifImplTest {
           EncodedImage(CloseableReference.of<PooledByteBuffer?>(byteBuffer, FAKE_RESOURCE_RELEASER))
       encodedImage.setImageFormat(ImageFormat.UNKNOWN)
       val closeableImage: CloseableAnimatedImage? =
-          animatedImageFactory?.decodeGif(encodedImage, imageDecodeOptions, DEFAULT_BITMAP_CONFIG)
-              as? CloseableAnimatedImage
+          gifImageDecoder?.decode(
+              encodedImage,
+              byteBuffer.size(),
+              ImmutableQualityInfo.FULL_QUALITY,
+              imageDecodeOptions,
+          ) as? CloseableAnimatedImage
 
       // Verify we got the right result
       val imageResult: AnimatedImageResult? = closeableImage?.getImageResult()
-      Assert.assertSame(mockGifImage, imageResult?.getImage())
-      Assert.assertNotNull(imageResult?.getPreviewBitmap())
-      Assert.assertFalse(imageResult?.hasDecodedFrame(0) ?: false)
+      assertThat(imageResult?.getImage()).isSameAs(mockGifImage)
+      assertThat(imageResult?.getPreviewBitmap()).isNotNull()
+      assertThat(imageResult?.hasDecodedFrame(0) ?: false).isFalse()
 
-      // Should not have interacted with these.
-      mockAnimatedDrawableBackendProvider?.let { provider ->
-        verify(provider)
-            .get(
-                Matchers.any<AnimatedImageResult?>(AnimatedImageResult::class.java),
-                Matchers.isNull<Rect?>(Rect::class.java),
-            )
-        verifyNoMoreInteractions(provider)
-      }
       mockBitmapFactory?.let { factory ->
         verify(factory).createBitmapInternal(50, 50, DEFAULT_BITMAP_CONFIG)
         verifyNoMoreInteractions(factory)
@@ -284,17 +276,6 @@ class AnimatedImageFactoryGifImplTest {
       mockBitmap2: Bitmap,
       byteBuffer: PooledByteBuffer,
   ) {
-    // For decoding preview frame, expect some calls.
-    val mockAnimatedDrawableBackend: AnimatedDrawableBackend = createAnimatedDrawableBackendMock(2)
-
-    whenever(
-            mockAnimatedDrawableBackendProvider?.get(
-                Matchers.any<AnimatedImageResult?>(AnimatedImageResult::class.java),
-                Matchers.isNull<Rect?>(Rect::class.java),
-            )
-        )
-        .thenReturn(mockAnimatedDrawableBackend)
-
     whenever(mockBitmapFactory?.createBitmapInternal(50, 50, DEFAULT_BITMAP_CONFIG))
         .thenReturn(CloseableReference.of<Bitmap?>(mockBitmap1, FAKE_BITMAP_RESOURCE_RELEASER))
         .thenReturn(CloseableReference.of<Bitmap?>(mockBitmap2, FAKE_BITMAP_RESOURCE_RELEASER))
@@ -311,25 +292,20 @@ class AnimatedImageFactoryGifImplTest {
       encodedImage.setImageFormat(ImageFormat.UNKNOWN)
 
       val closeableImage: CloseableAnimatedImage? =
-          animatedImageFactory?.decodeGif(encodedImage, imageDecodeOptions, DEFAULT_BITMAP_CONFIG)
-              as? CloseableAnimatedImage
+          gifImageDecoder?.decode(
+              encodedImage,
+              byteBuffer.size(),
+              ImmutableQualityInfo.FULL_QUALITY,
+              imageDecodeOptions,
+          ) as? CloseableAnimatedImage
 
       // Verify we got the right result
       val imageResult: AnimatedImageResult? = closeableImage?.getImageResult()
-      Assert.assertSame(mockGifImage, imageResult?.getImage())
-      Assert.assertNotNull(imageResult?.getDecodedFrame(0))
-      Assert.assertNotNull(imageResult?.getDecodedFrame(1))
-      Assert.assertNotNull(imageResult?.getPreviewBitmap())
+      assertThat(imageResult?.getImage()).isSameAs(mockGifImage)
+      assertThat(imageResult?.getDecodedFrame(0)).isNotNull()
+      assertThat(imageResult?.getDecodedFrame(1)).isNotNull()
+      assertThat(imageResult?.getPreviewBitmap()).isNotNull()
 
-      // Should not have interacted with these.
-      mockAnimatedDrawableBackendProvider?.let { provider ->
-        verify(provider)
-            .get(
-                Matchers.any<AnimatedImageResult?>(AnimatedImageResult::class.java),
-                Matchers.isNull<Rect?>(Rect::class.java),
-            )
-        verifyNoMoreInteractions(provider)
-      }
       mockBitmapFactory?.let { factory ->
         verify(factory, times(2)).createBitmapInternal(50, 50, DEFAULT_BITMAP_CONFIG)
         verifyNoMoreInteractions(factory)
@@ -343,22 +319,80 @@ class AnimatedImageFactoryGifImplTest {
     }
   }
 
+  @Test
+  fun testDecodeForceStaticImage() {
+    val mockGifImage: GifImage = mock<GifImage>()
+    val mockBitmap: Bitmap = MockBitmapFactory.create(50, 50, DEFAULT_BITMAP_CONFIG)
+
+    val byteBuffer: TrivialPooledByteBuffer = createByteBuffer()
+    whenever(
+            GifImage.createFromNativeMemory(
+                ArgumentMatchers.eq(byteBuffer.getNativePtr()),
+                ArgumentMatchers.eq(byteBuffer.size()),
+                Matchers.any<ImageDecodeOptions?>(ImageDecodeOptions::class.java),
+            )
+        )
+        .thenReturn(mockGifImage)
+    whenever(mockGifImage.getWidth()).thenReturn(50)
+    whenever(mockGifImage.getHeight()).thenReturn(50)
+    whenever(mockGifImage.getFrameCount()).thenReturn(1)
+    whenever(mockGifImage.getFrameDurations()).thenReturn(intArrayOf(100))
+    whenever(mockBitmapFactory?.createBitmapInternal(50, 50, DEFAULT_BITMAP_CONFIG))
+        .thenReturn(CloseableReference.of<Bitmap?>(mockBitmap, FAKE_BITMAP_RESOURCE_RELEASER))
+
+    Mockito.mockConstruction(AnimatedImageCompositor::class.java).use {
+        animatedImageCompositorConstruction: MockedConstruction<AnimatedImageCompositor> ->
+      val imageDecodeOptions: ImageDecodeOptions =
+          ImageDecodeOptions.newBuilder().setForceStaticImage(true).build()
+      val encodedImage: EncodedImage =
+          EncodedImage(CloseableReference.of<PooledByteBuffer?>(byteBuffer, FAKE_RESOURCE_RELEASER))
+      encodedImage.setImageFormat(ImageFormat.UNKNOWN)
+
+      val closeableImage: CloseableStaticBitmap? =
+          gifImageDecoder?.decode(
+              encodedImage,
+              byteBuffer.size(),
+              ImmutableQualityInfo.FULL_QUALITY,
+              imageDecodeOptions,
+          ) as? CloseableStaticBitmap
+
+      // Verify static bitmap instead of animated image
+      assertThat(closeableImage).isNotNull()
+      assertThat(closeableImage?.underlyingBitmap).isNotNull()
+
+      mockBitmapFactory?.let { factory ->
+        verify(factory).createBitmapInternal(50, 50, DEFAULT_BITMAP_CONFIG)
+        verifyNoMoreInteractions(factory)
+      }
+      verify(animatedImageCompositorConstruction.constructed()[0]).renderFrame(0, mockBitmap)
+    }
+  }
+
+  @Test
+  fun testDecodeWithInvalidGif() {
+    // Create an invalid GIF by using an empty byte buffer
+    val invalidGifData = ByteArray(0) // Empty array will cause validation to fail
+    val byteBuffer = TrivialPooledByteBuffer(invalidGifData)
+
+    val encodedImage: EncodedImage =
+        EncodedImage(CloseableReference.of<PooledByteBuffer?>(byteBuffer, FAKE_RESOURCE_RELEASER))
+    encodedImage.setImageFormat(ImageFormat.UNKNOWN)
+
+    assertThatThrownBy {
+          gifImageDecoder?.decode(
+              encodedImage,
+              byteBuffer.size(),
+              ImmutableQualityInfo.FULL_QUALITY,
+              ImageDecodeOptions.defaults(),
+          )
+        }
+        .isInstanceOf(UnsupportedOperationException::class.java)
+        .hasMessageContaining("Invalid image")
+  }
+
   private fun createByteBuffer(): TrivialPooledByteBuffer {
     val gifData: ByteArray = createValidGif()
     return TrivialPooledByteBuffer(gifData)
-  }
-
-  /**
-   * Creates the mock for the AnimatedDrawableBackend with the number of frame * * @param frameCount
-   * The number of frame to mock
-   */
-  private fun createAnimatedDrawableBackendMock(frameCount: Int): AnimatedDrawableBackend {
-    // For decoding preview frame, expect some calls.
-    val mockAnimatedDrawableBackend: AnimatedDrawableBackend = mock<AnimatedDrawableBackend>()
-    whenever(mockAnimatedDrawableBackend.getFrameCount()).thenReturn(frameCount)
-    whenever(mockAnimatedDrawableBackend.getWidth()).thenReturn(50)
-    whenever(mockAnimatedDrawableBackend.getHeight()).thenReturn(50)
-    return mockAnimatedDrawableBackend
   }
 
   companion object {
@@ -366,12 +400,12 @@ class AnimatedImageFactoryGifImplTest {
 
     private val FAKE_RESOURCE_RELEASER: ResourceReleaser<PooledByteBuffer> =
         object : ResourceReleaser<PooledByteBuffer> {
-          override fun release(value: PooledByteBuffer) {}
+          override fun release(value: PooledByteBuffer) = Unit
         }
 
     private val FAKE_BITMAP_RESOURCE_RELEASER: ResourceReleaser<Bitmap> =
         object : ResourceReleaser<Bitmap> {
-          override fun release(value: Bitmap) {}
+          override fun release(value: Bitmap) = Unit
         }
 
     private fun createDirectByteBuffer(): TrivialBufferPooledByteBuffer {
