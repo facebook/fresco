a95beaf12743d2485f12f9babd30f1533fecec14
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-10-21 05:09:57 -0700

    fbandroid/libraries/fresco/memory-types/ashmem/src/test/java/com/facebook/imagepipeline/memory/MemoryPooledByteBufferOutputStreamTest.java
    Reviewed By: oprisnik
    
    Differential Revision: D85124049
    
    fbshipit-source-id: 19c7e68fb66014987f2b7c25adb5ac1bfadede5a

--- memory-types/ashmem/src/test/java/com/facebook/imagepipeline/memory/MemoryPooledByteBufferOutputStreamTest.java
+++ memory-types/ashmem/src/test/java/com/facebook/imagepipeline/memory/MemoryPooledByteBufferOutputStreamTest.java
@@ -7,11 +7,12 @@
 
 package com.facebook.imagepipeline.memory;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.facebook.common.internal.ImmutableMap;
 import com.facebook.common.references.CloseableReference;
 import com.facebook.imagepipeline.testing.FakeAshmemMemoryChunkPool;
 import java.util.Arrays;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -74,98 +75,97 @@ public class MemoryPooledByteBufferOutputStreamTest {
       throws Exception {
     MemoryPooledByteBufferOutputStream os1 = new MemoryPooledByteBufferOutputStream(mPool);
     MemoryPooledByteBuffer sb1 = doWrite(os1, mData);
-    Assert.assertEquals(16, sb1.getCloseableReference().get().getSize());
+    assertThat(sb1.getCloseableReference().get().getSize()).isEqualTo(16);
     assertArrayEquals(mData, getBytes(sb1), mData.length);
     mStats.refresh();
-    Assert.assertEquals(
-        ImmutableMap.of(
-            32, new IntPair(0, 0),
-            16, new IntPair(1, 0),
-            8, new IntPair(0, 1),
-            4, new IntPair(0, 1)),
-        mStats.getBucketStats());
+    assertThat(mStats.getBucketStats())
+        .isEqualTo(
+            ImmutableMap.of(
+                32, new IntPair(0, 0),
+                16, new IntPair(1, 0),
+                8, new IntPair(0, 1),
+                4, new IntPair(0, 1)));
   }
 
   private void testBasic_2(final MemoryChunkPool mPool, final PoolStats<byte[]> mStats)
       throws Exception {
     MemoryPooledByteBufferOutputStream os2 = new MemoryPooledByteBufferOutputStream(mPool, 8);
     MemoryPooledByteBuffer sb2 = doWrite(os2, mData);
-    Assert.assertEquals(16, sb2.getCloseableReference().get().getSize());
+    assertThat(sb2.getCloseableReference().get().getSize()).isEqualTo(16);
     assertArrayEquals(mData, getBytes(sb2), mData.length);
     mStats.refresh();
-    Assert.assertEquals(
-        ImmutableMap.of(
-            32, new IntPair(0, 0),
-            16, new IntPair(1, 0),
-            8, new IntPair(0, 1),
-            4, new IntPair(0, 0)),
-        mStats.getBucketStats());
+    assertThat(mStats.getBucketStats())
+        .isEqualTo(
+            ImmutableMap.of(
+                32, new IntPair(0, 0),
+                16, new IntPair(1, 0),
+                8, new IntPair(0, 1),
+                4, new IntPair(0, 0)));
   }
 
   private void testBasic_3(final MemoryChunkPool mPool, final PoolStats<byte[]> mStats)
       throws Exception {
     MemoryPooledByteBufferOutputStream os3 = new MemoryPooledByteBufferOutputStream(mPool, 16);
     MemoryPooledByteBuffer sb3 = doWrite(os3, mData);
-    Assert.assertEquals(16, sb3.getCloseableReference().get().getSize());
+    assertThat(sb3.getCloseableReference().get().getSize()).isEqualTo(16);
     assertArrayEquals(mData, getBytes(sb3), mData.length);
     mStats.refresh();
-    Assert.assertEquals(
-        ImmutableMap.of(
-            32, new IntPair(0, 0),
-            16, new IntPair(1, 0),
-            8, new IntPair(0, 0),
-            4, new IntPair(0, 0)),
-        mStats.getBucketStats());
+    assertThat(mStats.getBucketStats())
+        .isEqualTo(
+            ImmutableMap.of(
+                32, new IntPair(0, 0),
+                16, new IntPair(1, 0),
+                8, new IntPair(0, 0),
+                4, new IntPair(0, 0)));
   }
 
   private void testBasic_4(final MemoryChunkPool mPool, final PoolStats<byte[]> mStats)
       throws Exception {
     MemoryPooledByteBufferOutputStream os4 = new MemoryPooledByteBufferOutputStream(mPool, 32);
     MemoryPooledByteBuffer sb4 = doWrite(os4, mData);
-    Assert.assertEquals(32, sb4.getCloseableReference().get().getSize());
+    assertThat(sb4.getCloseableReference().get().getSize()).isEqualTo(32);
     assertArrayEquals(mData, getBytes(sb4), mData.length);
     mStats.refresh();
-    Assert.assertEquals(
-        ImmutableMap.of(
-            32, new IntPair(1, 0),
-            16, new IntPair(0, 0),
-            8, new IntPair(0, 0),
-            4, new IntPair(0, 0)),
-        mStats.getBucketStats());
+    assertThat(mStats.getBucketStats())
+        .isEqualTo(
+            ImmutableMap.of(
+                32, new IntPair(1, 0),
+                16, new IntPair(0, 0),
+                8, new IntPair(0, 0),
+                4, new IntPair(0, 0)));
   }
 
   private static void testClose(final MemoryChunkPool mPool, final PoolStats<byte[]> mStats) {
     MemoryPooledByteBufferOutputStream os = new MemoryPooledByteBufferOutputStream(mPool);
     os.close();
     mStats.refresh();
-    Assert.assertEquals(
-        ImmutableMap.of(
-            32, new IntPair(0, 0),
-            16, new IntPair(0, 0),
-            8, new IntPair(0, 0),
-            4, new IntPair(0, 1)),
-        mStats.getBucketStats());
+    assertThat(mStats.getBucketStats())
+        .isEqualTo(
+            ImmutableMap.of(
+                32, new IntPair(0, 0),
+                16, new IntPair(0, 0),
+                8, new IntPair(0, 0),
+                4, new IntPair(0, 1)));
   }
 
   private static void testToByteBufException(final MemoryChunkPool mPool) {
     MemoryPooledByteBufferOutputStream os1 = new MemoryPooledByteBufferOutputStream(mPool);
     os1.close();
     os1.toByteBuffer();
-    Assert.fail();
   }
 
   private void testWriteAfterToByteBuf(final MemoryChunkPool mPool) throws Exception {
     MemoryPooledByteBufferOutputStream os1 = new MemoryPooledByteBufferOutputStream(mPool);
     MemoryPooledByteBuffer buf1 = doWrite(os1, Arrays.copyOf(mData, 9));
     MemoryPooledByteBuffer buf2 = doWrite(os1, Arrays.copyOf(mData, 3));
-    Assert.assertEquals(12, buf2.size());
+    assertThat(buf2.size()).isEqualTo(12);
 
     final CloseableReference<MemoryChunk> chunk = buf1.getCloseableReference();
-    Assert.assertEquals(3, chunk.getUnderlyingReferenceTestOnly().getRefCountTestOnly());
+    assertThat(chunk.getUnderlyingReferenceTestOnly().getRefCountTestOnly()).isEqualTo(3);
     os1.close();
     buf1.close();
     buf2.close();
-    Assert.assertEquals(0, chunk.getUnderlyingReferenceTestOnly().getRefCountTestOnly());
+    assertThat(chunk.getUnderlyingReferenceTestOnly().getRefCountTestOnly()).isEqualTo(0);
   }
 
   // write out the contents of data into the output stream
@@ -179,10 +179,10 @@ public class MemoryPooledByteBufferOutputStreamTest {
 
   // assert that the first 'length' bytes of expected are the same as those in 'actual'
   private static void assertArrayEquals(byte[] expected, byte[] actual, int length) {
-    Assert.assertTrue(expected.length >= length);
-    Assert.assertTrue(actual.length >= length);
+    assertThat(expected.length >= length).isTrue();
+    assertThat(actual.length >= length).isTrue();
     for (int i = 0; i < length; i++) {
-      Assert.assertEquals(expected[i], actual[i]);
+      assertThat(actual[i]).isEqualTo(expected[i]);
     }
   }
 
