2b25299e540b24b7459de2c052370930ebe6673e
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-10-22 23:49:23 -0700

    fbandroid/libraries/fresco/samples/scrollperf/src/test/java/com/facebook/samples/scrollperf/util/TimeWasterTest.java
    Reviewed By: nlutsenko
    
    Differential Revision: D85307249
    
    fbshipit-source-id: e8be26ffa969bf4bb869f40f09e91d353f9c2637

--- samples/scrollperf/src/test/java/com/facebook/samples/scrollperf/util/TimeWasterTest.java
+++ samples/scrollperf/src/test/java/com/facebook/samples/scrollperf/util/TimeWasterTest.java
@@ -7,7 +7,7 @@
 
 package com.facebook.samples.scrollperf.util;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
@@ -17,36 +17,36 @@ public class TimeWasterTest {
   @Test
   public void testFib_0_0() {
     final long result = TimeWaster.Fib(0);
-    assertEquals(0, result);
+    assertThat(result).isEqualTo(0);
   }
 
   @Test
   public void testFib_1_1() {
     final long result = TimeWaster.Fib(1);
-    assertEquals(1, result);
+    assertThat(result).isEqualTo(1);
   }
 
   @Test
   public void testFib_2_1() {
     final long result = TimeWaster.Fib(2);
-    assertEquals(1, result);
+    assertThat(result).isEqualTo(1);
   }
 
   @Test
   public void testFib_5_5() {
     final long result = TimeWaster.Fib(5);
-    assertEquals(5, result);
+    assertThat(result).isEqualTo(5);
   }
 
   @Test
   public void testFib_10_55() {
     final long result = TimeWaster.Fib(10);
-    assertEquals(55, result);
+    assertThat(result).isEqualTo(55);
   }
 
   @Test
   public void testFib_20_6765() {
     final long result = TimeWaster.Fib(20);
-    assertEquals(6765, result);
+    assertThat(result).isEqualTo(6765);
   }
 }
