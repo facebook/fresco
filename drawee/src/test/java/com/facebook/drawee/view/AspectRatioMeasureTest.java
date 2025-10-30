b99762843b45762a096b1a4de5298f4617c950aa
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-10-30 06:36:46 -0700

    fbandroid/libraries/fresco/drawee/src/test/java/com/facebook/drawee/view/AspectRatioMeasureTest.java
    Reviewed By: oprisnik
    
    Differential Revision: D85855231
    
    fbshipit-source-id: e8426e1cd12b46eb67af4b8cccf9008a8cb44f9b

--- drawee/src/test/java/com/facebook/drawee/view/AspectRatioMeasureTest.java
+++ drawee/src/test/java/com/facebook/drawee/view/AspectRatioMeasureTest.java
@@ -10,7 +10,7 @@ package com.facebook.drawee.view;
 import static android.view.View.MeasureSpec.*;
 import static android.view.ViewGroup.LayoutParams;
 import static android.view.ViewGroup.LayoutParams.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -32,16 +32,16 @@ public class AspectRatioMeasureTest {
     mSpec.height = makeMeasureSpec(310, EXACTLY);
     LayoutParams layoutParams1 = new LayoutParams(WRAP_CONTENT, MATCH_PARENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 1.5f, layoutParams1, 10, 10);
-    assertEquals(makeMeasureSpec(460, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(310, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(460, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, EXACTLY));
 
     // height
     mSpec.width = makeMeasureSpec(410, EXACTLY);
     mSpec.height = makeMeasureSpec(310, UNSPECIFIED);
     LayoutParams layoutParams2 = new LayoutParams(MATCH_PARENT, WRAP_CONTENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 2f, layoutParams2, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(210, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(210, EXACTLY));
   }
 
   @Test
@@ -51,16 +51,16 @@ public class AspectRatioMeasureTest {
     mSpec.height = makeMeasureSpec(310, EXACTLY);
     LayoutParams layoutParams1 = new LayoutParams(WRAP_CONTENT, MATCH_PARENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 0, layoutParams1, 10, 10);
-    assertEquals(makeMeasureSpec(410, UNSPECIFIED), mSpec.width);
-    assertEquals(makeMeasureSpec(310, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, UNSPECIFIED));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, EXACTLY));
 
     // height
     mSpec.width = makeMeasureSpec(410, EXACTLY);
     mSpec.height = makeMeasureSpec(310, UNSPECIFIED);
     LayoutParams layoutParams2 = new LayoutParams(MATCH_PARENT, WRAP_CONTENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 0, layoutParams2, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(310, UNSPECIFIED), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, UNSPECIFIED));
   }
 
   @Test
@@ -70,32 +70,32 @@ public class AspectRatioMeasureTest {
     mSpec.height = makeMeasureSpec(310, EXACTLY);
     LayoutParams layoutParams1 = new LayoutParams(WRAP_CONTENT, MATCH_PARENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 1.5f, layoutParams1, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(310, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, EXACTLY));
 
     // width within limits
     mSpec.width = makeMeasureSpec(510, AT_MOST);
     mSpec.height = makeMeasureSpec(310, EXACTLY);
     LayoutParams layoutParams2 = new LayoutParams(WRAP_CONTENT, MATCH_PARENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 1.5f, layoutParams2, 10, 10);
-    assertEquals(makeMeasureSpec(460, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(310, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(460, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, EXACTLY));
 
     // height exceeded
     mSpec.width = makeMeasureSpec(410, EXACTLY);
     mSpec.height = makeMeasureSpec(110, AT_MOST);
     LayoutParams layoutParams3 = new LayoutParams(MATCH_PARENT, WRAP_CONTENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 2f, layoutParams3, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(110, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(110, EXACTLY));
 
     // height within limits
     mSpec.width = makeMeasureSpec(410, EXACTLY);
     mSpec.height = makeMeasureSpec(310, AT_MOST);
     LayoutParams layoutParams4 = new LayoutParams(MATCH_PARENT, WRAP_CONTENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 2f, layoutParams4, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(210, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(210, EXACTLY));
   }
 
   @Test
@@ -105,15 +105,15 @@ public class AspectRatioMeasureTest {
     mSpec.height = makeMeasureSpec(310, EXACTLY);
     LayoutParams layoutParams1 = new LayoutParams(WRAP_CONTENT, MATCH_PARENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 1.5f, layoutParams1, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(310, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, EXACTLY));
 
     // height
     mSpec.width = makeMeasureSpec(410, EXACTLY);
     mSpec.height = makeMeasureSpec(310, EXACTLY);
     LayoutParams layoutParams2 = new LayoutParams(MATCH_PARENT, WRAP_CONTENT);
     AspectRatioMeasure.updateMeasureSpec(mSpec, 2f, layoutParams2, 10, 10);
-    assertEquals(makeMeasureSpec(410, EXACTLY), mSpec.width);
-    assertEquals(makeMeasureSpec(310, EXACTLY), mSpec.height);
+    assertThat(mSpec.width).isEqualTo(makeMeasureSpec(410, EXACTLY));
+    assertThat(mSpec.height).isEqualTo(makeMeasureSpec(310, EXACTLY));
   }
 }
