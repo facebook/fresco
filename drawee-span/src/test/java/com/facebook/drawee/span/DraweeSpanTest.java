322f67e6529361d74bd1d06d824612e0af100257
generatedunixname2010855189643664 <noreply+2010855189643664@fb.com>
2026-03-20 02:19:11 -0700

    fbandroid/libraries/fresco/drawee-span/src/main/java/com/facebook/drawee/span/DraweeSpan.java
    Reviewed By: oprisnik
    
    Differential Revision: D97090486
    
    fbshipit-source-id: 40b5143e4b2b94eaed98dd55d69e0412f47a4029

--- drawee-span/src/test/java/com/facebook/drawee/span/DraweeSpanTest.java
+++ drawee-span/src/test/java/com/facebook/drawee/span/DraweeSpanTest.java
@@ -8,6 +8,8 @@
 package com.facebook.drawee.span;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,7 +28,7 @@ import org.robolectric.RobolectricTestRunner;
 @RunWith(RobolectricTestRunner.class)
 public class DraweeSpanTest {
 
-  @Mock public DraweeHolder mDraweeHierarchy;
+  private DraweeHolder mDraweeHierarchy;
   @Mock public Rect mBounds;
   @Mock public Drawable mDrawable;
 
@@ -35,18 +37,26 @@ public class DraweeSpanTest {
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
-    when(mDraweeHierarchy.getTopLevelDrawable()).thenReturn(mDrawable);
+    mDraweeHierarchy = spy(new DraweeHolder(null));
+    doReturn(mDrawable).when(mDraweeHierarchy).getTopLevelDrawable();
     when(mDrawable.getBounds()).thenReturn(mBounds);
     mDraweeSpan = new DraweeSpan(mDraweeHierarchy, BetterImageSpan.ALIGN_CENTER);
   }
 
   @Test
   public void testLifecycle() {
+    DraweeHolder holder = mDraweeSpan.getDraweeHolder();
+    assertThat(mDraweeSpan.isAttached()).isFalse();
+
     mDraweeSpan.onAttach();
     verify(mDraweeHierarchy).onAttach();
+    assertThat(mDraweeSpan.isAttached()).isTrue();
+    assertThat(holder.isAttached()).isTrue();
 
     mDraweeSpan.onDetach();
     verify(mDraweeHierarchy).onDetach();
+    assertThat(mDraweeSpan.isAttached()).isFalse();
+    assertThat(holder.isAttached()).isFalse();
   }
 
   @Test
