d291ae30fb7e85fb0b16beb1f7ec2c77b1648272
generatedunixname970978688699417 <noreply+970978688699417@fb.com>
2026-05-18 15:15:35 -0700

    com.facebook.imagepipeline.animated.factory.AnimatedImageFactory
    Reviewed By: oprisnik
    
    Differential Revision: D105537785
    
    fbshipit-source-id: b1d6433f7d30326445633d49f58604b2098a55d5

--- animated-base/src/main/java/com/facebook/imagepipeline/animated/factory/AnimatedImageFactory.kt
+++ animated-base/src/main/java/com/facebook/imagepipeline/animated/factory/AnimatedImageFactory.kt
@@ -7,39 +7,5 @@
 
 package com.facebook.imagepipeline.animated.factory
 
-import android.graphics.Bitmap
-import com.facebook.imagepipeline.common.ImageDecodeOptions
-import com.facebook.imagepipeline.image.CloseableImage
-import com.facebook.imagepipeline.image.EncodedImage
-
 /** Decoder for animated images. */
-interface AnimatedImageFactory {
-
-  /**
-   * Decodes a GIF into a CloseableImage.
-   *
-   * @param encodedImage encoded image (native byte array holding the encoded bytes and meta data)
-   * @param options the options for the decode
-   * @param bitmapConfig the Bitmap.Config used to generate the output bitmaps
-   * @return a [CloseableImage] for the GIF image
-   */
-  fun decodeGif(
-      encodedImage: EncodedImage,
-      options: ImageDecodeOptions,
-      bitmapConfig: Bitmap.Config,
-  ): CloseableImage
-
-  /**
-   * Decode a WebP into a CloseableImage.
-   *
-   * @param encodedImage encoded image (native byte array holding the encoded bytes and meta data)
-   * @param options the options for the decode
-   * @param bitmapConfig the Bitmap.Config used to generate the output bitmaps
-   * @return a [CloseableImage] for the WebP image
-   */
-  fun decodeWebP(
-      encodedImage: EncodedImage,
-      options: ImageDecodeOptions,
-      bitmapConfig: Bitmap.Config,
-  ): CloseableImage
-}
+interface AnimatedImageFactory {}
