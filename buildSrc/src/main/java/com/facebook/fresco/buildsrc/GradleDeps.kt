6a14e114e5854f5a7dfb3e71e78ec8beb3311396
Joshua Selbo <jselbo@meta.com>
2026-06-08 10:59:10 -0700

    Bump Kotlin to 2.2.0
    Reviewed By: oprisnik, hick209
    
    Differential Revision: D107895174
    
    fbshipit-source-id: 7f10d3f1241cfa5a9a18c053e2665d0b56a8b86c

--- buildSrc/src/main/java/com/facebook/fresco/buildsrc/GradleDeps.kt
+++ buildSrc/src/main/java/com/facebook/fresco/buildsrc/GradleDeps.kt
@@ -16,7 +16,7 @@ object GradleDeps {
   }
 
   object Kotlin {
-    const val version = "2.0.0"
+    const val version = "2.2.0"
     const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
     const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:$version"
   }
