5d02904be4c04722383992306843d575cb0c4383
Oleh Svirshchevsky <oleh@meta.com>
2026-06-09 06:05:15 -0700

    Convert ByteConstants from Java to Kotlin
    Differential Revision: D107980050
    
    fbshipit-source-id: d453c3a9a8b92a857df351e2604e0c58ee8cab62

---
+++ fbcore/src/main/java/com/facebook/common/util/ByteConstants.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.common.util
+
+object ByteConstants {
+  const val KB: Int = 1024
+  const val MB: Int = 1024 * KB
+}
