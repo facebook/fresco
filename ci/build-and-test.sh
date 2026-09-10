9d79abd68977c7b939b5e9ab041029bf1b1591c8
Peter Abbondanzo <abbo@meta.com>
2026-09-10 10:42:28 -0700

    Use internal Maven mirrors in Sandcastle
    Differential Revision: D119223727
    
    fbshipit-source-id: 025211afcea85d282ffaf67f1983ae19d876d058

--- ci/build-and-test.sh
+++ ci/build-and-test.sh
@@ -4,10 +4,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-if [[ $1 == "--local" || $1 == "-l" ]]; then
+# --local/-l is a script mode and must be the first argument; remaining arguments go to Gradle.
+if [[ ${1:-} == "--local" || ${1:-} == "-l" ]]; then
+  shift
   # Local build with less verbose output
-  ./gradlew test assembleDebug assembleDebugAndroidTest
+  ./gradlew test assembleDebug assembleDebugAndroidTest "$@"
 else
   # Standard CI build command with maximum verbose output
-  ./gradlew test assembleDebug assembleDebugAndroidTest --info
+  ./gradlew test assembleDebug assembleDebugAndroidTest --info "$@"
 fi
